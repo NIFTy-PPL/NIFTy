a648ea490222f0dfe857760bc4b9e3fb928f2ee6
theos <theo.steininger@ultimanet.de>
2016-10-20 03:27:47 +0200

    Finished VL-BFGS minimizer. Still untested.

--- nifty/minimization/__init__.py
+++ nifty/minimization/__init__.py
@@ -4,3 +4,4 @@ from line_searching import *
 from conjugate_gradient import ConjugateGradient
 from quasi_newton_minimizer import QuasiNewtonMinimizer
 from steepest_descent import SteepestDescent
+from vl_bfgs import VL_BFGS
