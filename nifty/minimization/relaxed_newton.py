73dabd0ba723d052e1cef614453f0cb830af7084
Theo Steininger <theos@mpa-garching.mpg.de>
2017-02-06 22:01:33 +0100

    Refactored relaxed_newton.py such that there is `one expression per line`.

--- nifty/minimization/relaxed_newton.py
+++ nifty/minimization/relaxed_newton.py
@@ -5,7 +5,9 @@ from .quasi_newton_minimizer import QuasiNewtonMinimizer
 
 class RelaxedNewton(QuasiNewtonMinimizer):
     def _get_descend_direction(self, energy):
-        descend_direction = energy.curvature.inverse_times(energy.gradient)
+        gradient = energy.gradient
+        curvature = energy.curvature
+        descend_direction = curvature.inverse_times(gradient)
         norm = descend_direction.norm()
         if norm != 1:
             return descend_direction / -norm
