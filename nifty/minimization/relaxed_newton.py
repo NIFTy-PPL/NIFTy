9f1c0f3b7139e12f9573fdb478f748ff5bc8ecd3
Theo Steininger <theos@mpa-garching.mpg.de>
2017-02-07 16:23:18 +0100

    Added prefered_initial_step_size to LineSearch class.

--- nifty/minimization/relaxed_newton.py
+++ nifty/minimization/relaxed_newton.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from .quasi_newton_minimizer import QuasiNewtonMinimizer
+from .line_searching import LineSearchStrongWolfe
 
 
 class RelaxedNewton(QuasiNewtonMinimizer):
+    def __init__(self, line_searcher=LineSearchStrongWolfe(), callback=None,
+                 convergence_tolerance=1E-4, convergence_level=3,
+                 iteration_limit=None):
+        super(RelaxedNewton, self).__init__(
+                                line_searcher=line_searcher,
+                                callback=callback,
+                                convergence_tolerance=convergence_tolerance,
+                                convergence_level=convergence_level,
+                                iteration_limit=iteration_limit)
+
+        self.line_searcher.prefered_initial_step_size = 1.
+
     def _get_descend_direction(self, energy):
         gradient = energy.gradient
         curvature = energy.curvature
