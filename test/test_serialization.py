1e8ce667a16c9dd6dc2e24496d4e9d2cfc66e4ee
Theo Steininger <theos@mpa-garching.mpg.de>
2017-03-22 13:24:50 +0100

    Inserted check in test/test_serialization.py if h5py is available.

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -1,21 +1,30 @@
 import unittest
 
+from nifty import dependency_injector as di
+
 from numpy.testing import assert_equal
 from keepers import Repository
 from test.common import expand, generate_spaces
 
+try:
+    import h5py
+except ImportError:
+    h5py_available = False
+else:
+    h5py_available = True
 
-class SpaceSerializationTests(unittest.TestCase):
-    # variable to hold the repository
-    _repo = None
+if h5py_available:
+    class SpaceSerializationTests(unittest.TestCase):
+        # variable to hold the repository
+        _repo = None
 
-    @classmethod
-    def setUpClass(cls):
-        cls._repo = Repository('test.h5')
+        @classmethod
+        def setUpClass(cls):
+            cls._repo = Repository('test.h5')
 
-    @expand([[space] for space in generate_spaces()])
-    def test_serialization(self, space):
-        self._repo.add(space, 'space')
-        self._repo.commit()
+        @expand([[space] for space in generate_spaces()])
+        def test_serialization(self, space):
+            self._repo.add(space, 'space')
+            self._repo.commit()
 
-        assert_equal(space, self._repo.get('space'))
+            assert_equal(space, self._repo.get('space'))
