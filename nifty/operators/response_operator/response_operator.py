bab313a1528877909eaa5a980d50295bebfef32d
dpumpe <dpumpe@mpa-garching.mpg.de>
2017-04-11 10:23:05 +0200

    correct import of modules and update of wiener filter unit test case

--- nifty/operators/response_operator/response_operator.py
+++ nifty/operators/response_operator/response_operator.py
@@ -1,5 +1,5 @@
 from nifty import Field,\
-                  FieldArray, \
+                  FieldArray
 from nifty.operators.linear_operator import LinearOperator
 from nifty.operators.smoothing_operator import SmoothingOperator
 
