6496fcef1c5e89727d787ef4ec22ef2ac1bed937
Mihai Baltac <ga95joh@mytum.de>
2017-05-06 21:17:49 +0200

    added showlegend attribute in cartesians

--- nifty/plotting/plots/scatter_plots/cartesian_2D.py
+++ nifty/plotting/plots/scatter_plots/cartesian_2D.py
@@ -5,12 +5,12 @@ from cartesian import Cartesian
 
 class Cartesian2D(Cartesian):
     def __init__(self, x=None, y=None, x_start=0, x_step=1,
-                 label='', line=None, marker=None, webgl=True):
+                 label='', line=None, marker=None, showlegend=True, webgl=True):
         if y is None:
             raise Exception('Error: no y data to plot')
         if x is None:
             x = range(x_start, len(y) * x_step, x_step)
-        super(Cartesian2D, self).__init__(x, y, label, line, marker)
+        super(Cartesian2D, self).__init__(x, y, label, line, marker, showlegend)
         self.webgl = webgl
 
     def to_plotly(self):
--- nifty/plotting/plots/scatter_plots/cartesian_3D.py
+++ nifty/plotting/plots/scatter_plots/cartesian_3D.py
@@ -4,8 +4,8 @@ from cartesian import Cartesian
 
 
 class Cartesian3D(Cartesian):
-    def __init__(self, x, y, z, label='', line=None, marker=None):
-        super(Cartesian3D, self).__init__(x, y, label, line, marker)
+    def __init__(self, x, y, z, label='', line=None, marker=None, showlegend=True):
+        super(Cartesian3D, self).__init__(x, y, label, line, marker, showlegend)
         self.z = z
 
     def to_plotly(self):
