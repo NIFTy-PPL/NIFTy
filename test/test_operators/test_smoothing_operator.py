3b3c0f34f9c839ad579d3c90493578233168642c
dpumpe <dpumpe@mpa-garching.mpg.de>
2017-05-14 14:08:00 +0200

    tests for SmoothingOperator

---
+++ test/test_operators/test_smoothing_operator.py
@@ -0,0 +1,69 @@
+import unittest
+
+from numpy.testing import assert_equal,\
+    assert_allclose,\
+    assert_approx_equal
+
+from nifty import Field,\
+    SmoothingOperator
+
+from test.common import generate_spaces
+
+from itertools import product
+from test.common import expand
+
+class SmoothingOperator_Tests(unittest.TestCase):
+    spaces = generate_spaces()
+
+    @expand(product(spaces, [0., .5, 5.], [True, False]))
+    def test_property(self, space, sigma, log_distances):
+        op = SmoothingOperator(space, sigma=sigma,
+                              log_distances=log_distances)
+        if op.domain[0] != space:
+            raise TypeError
+        if op.unitary != False:
+            raise ValueError
+        if op.self_adjoint != True:
+            raise ValueError
+        if op.sigma != sigma:
+            raise ValueError
+        if op.log_distances != log_distances:
+            raise ValueError
+
+    @expand(product(spaces, [0., .5, 5.], [True, False]))
+    def test_adjoint_times(self, space, sigma, log_distances):
+        op = SmoothingOperator(space, sigma=sigma,
+                              log_distances=log_distances)
+        rand1 = Field.from_random('normal', domain=space)
+        rand2 = Field.from_random('normal', domain=space)
+        tt1 = rand1.dot(op.times(rand2))
+        tt2 = rand2.dot(op.adjoint_times(rand1))
+        assert_approx_equal(tt1, tt2)
+
+    @expand(product(spaces, [0., .5, 5.], [True, False]))
+    def test_times(self, space, sigma, log_distances):
+        op = SmoothingOperator(space, sigma=sigma,
+                              log_distances=log_distances)
+        rand1 = Field(space, val=0.)
+        rand1.val[0] = 1.
+        tt1 = op.times(rand1)
+        assert_approx_equal(1, tt1.sum())
+
+    @expand(product(spaces, [0., .5, 5.], [True, False]))
+    def test_inverse_adjoint_times(self, space, sigma, log_distances):
+        op = SmoothingOperator(space, sigma=sigma,
+                              log_distances=log_distances)
+        rand1 = Field.from_random('normal', domain=space)
+        rand2 = Field.from_random('normal', domain=space)
+        tt1 = rand1.dot(op.inverse_times(rand2))
+        tt2 = rand2.dot(op.inverse_adjoint_times(rand1))
+        assert_approx_equal(tt1, tt2)
+
+    @expand(product(spaces, [0., .5, 5.], [True, False]))
+    def test_inverse_adjoint_times(self, space, sigma, log_distances):
+        op = SmoothingOperator(space, sigma=sigma,
+                              log_distances=log_distances)
+        rand1 = Field.from_random('normal', domain=space)
+        tt1 = op.times(op.inverse_times(rand1))
+        assert_allclose(rand1.val.get_full_data(), tt1.val.get_full_data(),
+                        rtol=1e-4)
