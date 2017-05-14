4979c08674a81499f5169b716ec46eb8f8842394
dpumpe <dpumpe@mpa-garching.mpg.de>
2017-05-14 15:53:27 +0200

    tests for ComposedOperator

--- test/test_operators/test_composed_operator.py
+++ test/test_operators/test_composed_operator.py
@@ -16,19 +16,19 @@ from test.common import expand
 class ComposedOperator_Tests(unittest.TestCase):
     spaces = generate_spaces()
 
-    @expand(product([spaces], [spaces]))
+    @expand(product(spaces, spaces))
     def test_property(self, space1, space2):
         rand1 = Field.from_random('normal', domain=space1)
         rand2 = Field.from_random('normal', domain=space2)
         op1 = DiagonalOperator(space1, diagonal=rand1)
         op2 = DiagonalOperator(space2, diagonal=rand2)
         op = ComposedOperator((op1, op2))
-        if op.domain != (op1.domain, op2.domain):
+        if op.domain != (op1.domain[0], op2.domain[0]):
             raise TypeError
         if op.unitary != False:
             raise ValueError
 
-    @expand(product([spaces],[spaces]))
+    @expand(product(spaces,spaces))
     def test_times_adjoint_times(self, space1, space2):
         diag1 = Field.from_random('normal', domain=space1)
         diag2 = Field.from_random('normal', domain=space2)
@@ -44,7 +44,7 @@ class ComposedOperator_Tests(unittest.TestCase):
         tt2 = rand1.dot(op.adjoint_times(rand2))
         assert_approx_equal(tt1, tt2)
 
-    @expand(product([spaces], [spaces]))
+    @expand(product(spaces, spaces))
     def test_times_inverse_times(self, space1, space2):
         diag1 = Field.from_random('normal', domain=space1)
         diag2 = Field.from_random('normal', domain=space2)
