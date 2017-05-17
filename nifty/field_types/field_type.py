26dcdd262a958c110401c7259b9955c1b77f8e16
Theo Steininger <theo.steininger@ultimanet.de>
2017-05-17 02:02:51 +0200

    Removed FieldType.process

--- nifty/field_types/field_type.py
+++ nifty/field_types/field_type.py
@@ -27,16 +27,3 @@ class FieldType(DomainObject):
         else:
             result = x.copy()
         return result
-
-    def process(self, method_name, array, inplace=True, **kwargs):
-        try:
-            result_array = self.__getattr__(method_name)(array,
-                                                         inplace,
-                                                         **kwargs)
-        except AttributeError:
-            if inplace:
-                result_array = array
-            else:
-                result_array = array.copy()
-
-        return result_array
