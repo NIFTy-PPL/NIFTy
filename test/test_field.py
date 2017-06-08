19066ee66a1fba6cd07eea0147200a2364e44c15
hutsch <hutsch@mpa-garching.mpg.de>
2017-06-08 12:13:31 +0200

    field test update

--- test/test_field.py
+++ test/test_field.py
@@ -26,7 +26,8 @@ from itertools import product
 
 from nifty import Field,\
                   RGSpace,\
-                  FieldArray
+                  HPSpace,\
+                  GLSpace
 
 from d2o import distributed_data_object,\
                 STRATEGIES
@@ -35,11 +36,110 @@ from test.common import expand
 
 np.random.seed(123)
 
-SPACES = [RGSpace((4,)), RGSpace((5))]
-SPACE_COMBINATIONS = [(), SPACES[0], SPACES[1], SPACES]
+#  Exploring the parameter space
+SPACES = [HPSpace(nside=1), RGSpace((4,)), GLSpace(nlat=2, nlon=3)]
+SPACE_COMBINATIONS = [(), SPACES[0], SPACES[1], SPACES[2], SPACES[0:2], SPACES[1:]]
+DTYPE_COMBINATIONS = [np.float64, np.complex128]
+DIST_COMBINATIONS = STRATEGIES['global']
+COPY_COMBINATIONS = [True, False]
+VAL_COMBINATIONS = [np.empty(0), np.empty(0, dtype=np.complex128),
+                    np.random.rand(12),  np.random.rand(12) + np.random.rand(12)*1j,
+                    np.random.rand(4),  np.random.rand(4) + np.random.rand(4)*1j,
+                    np.random.rand(6),  np.random.rand(6) + np.random.rand(6)*1j,
+                    np.random.rand(48).reshape((12, 4)),
+                    np.random.rand(48).reshape((12, 4)) + np.random.rand(48).reshape((12, 4))*1j,
+                    np.random.rand(24).reshape((4, 6)),
+                    np.random.rand(24).reshape((4, 6)) + np.random.rand(24).reshape((4, 6)) * 1j
+                    ]
+
+# The expected properties
+EXPECTED_PROPERTIES = [{'shape': (),  # empty, real
+                        'domain_axes': (),
+                        'dof': 0,
+                        'dim': 0,
+                        'total_volume': 0.0,
+                        },
+                       {'shape': (),  # empty, complex
+                        'domain_axes': (),
+                        'dof': 0,
+                        'dim': 0,
+                        'total_volume': 0.0,
+                        },
+                       {'shape': (12,),  # hp, real
+                        'domain_axes': ((0,),),
+                        'dof': 12,
+                        'dim': 12,
+                        'total_volume': 4*np.pi,
+                        },
+                       {'shape': (12,),  # hp, complex
+                        'domain_axes': ((0,),),
+                        'dof': 24,
+                        'dim': 12,
+                        'total_volume': 4*np.pi,
+                        },
+                       {'shape': (4,),  # rg, real
+                        'domain_axes': ((0,),),
+                        'dof': 4,
+                        'dim': 4,
+                        'total_volume': 1.0,
+                        },
+                       {'shape': (4,),   # rg, complex
+                        'domain_axes': ((0,),),
+                        'dof': 8,
+                        'dim': 4,
+                        'total_volume': 1.0,
+                        },
+                       {'shape': (6,),  # gl, real
+                        'domain_axes': ((0,),),
+                        'dof': 6,
+                        'dim': 6,
+                        'total_volume': 4 * np.pi,
+                        },
+                       {'shape': (6,),  # gl, complex
+                        'domain_axes': ((0,),),
+                        'dof': 12,
+                        'dim': 6,
+                        'total_volume': 4 * np.pi,
+                        },
+                       {'shape': (12, 4,),  # prod(hp,rg), real
+                        'domain_axes': ((0,), (1,)),
+                        'dof': 48,
+                        'dim': 48,
+                        'total_volume':  4 * np.pi,
+                        },
+                       {'shape': (12, 4,),  # prod(hp,rg), complex
+                        'domain_axes': ((0,), (1,)),
+                        'dof': 96,
+                        'dim': 48,
+                        'total_volume': 4 * np.pi,
+                        },
+                       {'shape': (4, 6,),  # prod(rg,gl), real
+                        'domain_axes': ((0,), (1,)),
+                        'dof': 24,
+                        'dim': 24,
+                        'total_volume': 4 * np.pi,
+                        },
+                       {'shape': (4, 6,),  # prod(rg,gl), complex
+                        'domain_axes': ((0,), (1,)),
+                        'dof': 48,
+                        'dim': 24,
+                        'total_volume': 4 * np.pi,
+                        },
+                       ]
+
+# The expected output of the methods
 
 
-class Test_Interface(unittest.TestCase):
+def get_cast_configs():
+    keys = ('__class__', 'shape', 'distribution_strategy', 'dtype')
+    values = product([distributed_data_object], [(), (12,), (4,), (6,), (12, 4), (4, 6)], ['not'],
+                     [np.float, np.complex])
+    output = [dict(zip(keys, l)) for l in values]
+    return output
+
+
+# The tests
+class FieldInterfaceTest(unittest.TestCase):
     @expand(product(SPACE_COMBINATIONS,
                     [['distribution_strategy', str],
                      ['domain', tuple],
@@ -55,10 +155,62 @@ class Test_Interface(unittest.TestCase):
         f = Field(domain=domain)
         assert_(isinstance(getattr(f, attribute), desired_type))
 
-#class Test_Initialization(unittest.TestCase):
-#
-#    @parameterized.expand(
-#        itertools.product(SPACE_COMBINATIONS,
-#                          []
-#                          )
-#    def test_
+
+class FieldPropertyCheck(unittest.TestCase):
+    @expand(zip(product(SPACE_COMBINATIONS, DTYPE_COMBINATIONS), EXPECTED_PROPERTIES))
+    def test_consistency(self, setters, expected_attributes):
+        f = Field(domain=setters[0], dtype=setters[1])
+        for key, value in expected_attributes.iteritems():
+            assert_equal(getattr(f, key), value)
+
+
+class FieldFunctionalityTest(unittest.TestCase):
+    @expand(zip(zip(product(SPACE_COMBINATIONS, DTYPE_COMBINATIONS), VAL_COMBINATIONS), 12*['not'], get_cast_configs()))
+    def test_cast(self, setters, dist, expected_functionality):
+        field = Field(domain=setters[0][0], val=setters[1], distribution_strategy=dist, dtype=setters[0][1])
+        cast = field.cast()
+        for key, value in expected_functionality.iteritems():
+            assert_equal(getattr(cast, key), value)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+'''
+    @expand(product(FIELD_COMBINATIONS, EXPECTED_FUNCTIONALITY['power_synthesize']))
+    def test_power_synthesize(self, field, expected_functionality):
+        pow_syn = field.power_synthesize()
+        for key, value in expected_functionality:
+            assert_equal(getattr(pow_syn, key), value)
+
+    @expand(product(SPACE_COMBINATIONS, EXPECTED_FUNCTIONALITY['power_analyze']))
+    def test_power_analyze(self, field, expected_functionality):
+        pow_an = field.power_analyze()
+        for key, value in expected_functionality:
+            assert_equal(getattr(pow_an, key), value)
+
+    @expand(product(SPACE_COMBINATIONS, EXPECTED_FUNCTIONALITY['weight']))
+    def test_weight(self, field, expected_functionality):
+        weight = field.power_synthesize()
+        for key, value in expected_functionality:
+            assert_equal(getattr(weight, key), value)
+
+    @expand(product(SPACE_COMBINATIONS, EXPECTED_FUNCTIONALITY['dot']))
+    def test_dot(self, field, expected_functionality):
+        dot = field.dot()
+        for key, value in expected_functionality:
+            assert_equal(getattr(dot, key), value)
+
+    @expand(product(SPACE_COMBINATIONS, EXPECTED_FUNCTIONALITY['norm']))
+    def test_norm(self, field, expected_functionality):
+        norm = field.norm()e
+        for key, value in expected_functionality:
+            assert_equal(getattr(norm, key), value)
+
+    @expand(product(SPACE_COMBINATIONS, EXPECTED_FUNCTIONALITY['conjugate']))
+    def test_conjugate(self, field, expected_functionality):
+        conjugate = field.conjugate()
+        for key, value in expected_functionality:
+            assert_equal(getattr(conjugate, key), value)
+
+'''
