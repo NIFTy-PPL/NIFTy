baa71d880ec63b299015d1f2537c3c0dd02fa069
Theo Steininger <theo.steininger@ipt.ai>
2017-08-26 02:35:40 +0200

    Added config for loglevel

--- nifty/config/nifty_config.py
+++ nifty/config/nifty_config.py
@@ -82,13 +82,20 @@ variable_threads = keepers.Variable(
                                 lambda z: np.int(abs(z)) == z,
                                 genus='int')
 
+variable_loglevel = keepers.Variable(
+                                'loglevel',
+                                [10],
+                                lambda z: np.int(z) == z and 0 <= z <= 50,
+                                genus='int')
+
 nifty_configuration = keepers.get_Configuration(
                  name='NIFTy',
                  variables=[variable_fft_module,
                             variable_default_field_dtype,
                             variable_default_distribution_strategy,
                             variable_harmonic_rg_base,
-                            variable_threads],
+                            variable_threads,
+                            variable_loglevel],
                  file_name='NIFTy.conf',
                  search_paths=[os.path.expanduser('~') + "/.config/nifty/",
                                os.path.expanduser('~') + "/.config/",
