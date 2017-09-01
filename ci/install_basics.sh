d89c7e507dca65d3c38d91e257e45996d27a811a
Martin Reinecke <martin@mpa-garching.mpg.de>
2017-09-01 13:25:20 +0200

    experiment

--- ci/install_basics.sh
+++ ci/install_basics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install -y build-essential python python-pip python-dev git autoconf libtool gsl-bin libgsl-dev wget python3 python3-pip python3-dev python3-nose libatlas-base-dev libfftw3-bin libfftw3-dev libfftw3-double3 libfftw3-long3 libfftw3-mpi-dev libfftw3-mpi3 libfftw3-quad3 libfftw3-single3 python-numpy python3-numpy
+apt-get install -y build-essential python python-pip python-dev git autoconf libtool python3 python3-pip python3-dev python3-nose libfftw3-bin libfftw3-dev libfftw3-double3 libfftw3-long3 libfftw3-quad3 libfftw3-single3 python-numpy python3-numpy
