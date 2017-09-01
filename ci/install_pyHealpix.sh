9088442de4ddcb7b617cf5621cfa8daa8fbfc9c1
Martin Reinecke <martin@mpa-garching.mpg.de>
2017-09-01 13:19:36 +0200

    experiment

--- ci/install_pyHealpix.sh
+++ ci/install_pyHealpix.sh
@@ -2,5 +2,7 @@
 
 git clone https://gitlab.mpcdf.mpg.de/ift/pyHealpix.git
 (cd pyHealpix && autoreconf -i && ./configure --enable-openmp --prefix=${HOME}/.local && make -j4 install)
+rm -rf pyHealpix
+git clone https://gitlab.mpcdf.mpg.de/ift/pyHealpix.git
 (cd pyHealpix && autoreconf -i && PYTHON=python3 PYTHON_CONFIG=python3-config ./configure --enable-openmp --prefix=${HOME}/.local && make -j4 install)
 rm -rf pyHealpix
