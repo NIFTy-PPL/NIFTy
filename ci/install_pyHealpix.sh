edd55d749fae7aaba2467baa561242d624e4c78c
Martin Reinecke <martin@mpa-garching.mpg.de>
2017-09-01 13:09:52 +0200

    Revert "tweak CI"
    This reverts commit 7c42148f5394d433ac9b898e9dac4c4ebc4ee8b5.

--- ci/install_pyHealpix.sh
+++ ci/install_pyHealpix.sh
@@ -2,5 +2,7 @@
 
 git clone https://gitlab.mpcdf.mpg.de/ift/pyHealpix.git
 (cd pyHealpix && autoreconf -i && ./configure --enable-openmp && make -j4 install)
-(cd pyHealpix && autoreconf -i && PYTHON=python3 ./configure --enable-openmp && make -j4 install)
+rm -rf pyHealpix
+git clone https://gitlab.mpcdf.mpg.de/ift/pyHealpix.git
+(cd pyHealpix && autoreconf -i && PYTHON=python3 PYTHON_CONFIG=python3-config ./configure --enable-openmp && make -j4 install)
 rm -rf pyHealpix
