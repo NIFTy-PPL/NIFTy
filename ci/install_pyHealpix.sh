577b25f439d5817cb5cd9d16a430c7a77561e5e5
Martin Reinecke <martin@mpa-garching.mpg.de>
2017-09-10 12:10:19 +0200

    remove unused file

--- ci/install_pyHealpix.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git clone https://gitlab.mpcdf.mpg.de/ift/pyHealpix.git
-(cd pyHealpix && autoreconf -i && ./configure --enable-openmp --prefix=${HOME}/.local && make -j4 install)
-rm -rf pyHealpix
-git clone https://gitlab.mpcdf.mpg.de/ift/pyHealpix.git
-(cd pyHealpix && autoreconf -i && PYTHON=python3 PYTHON_CONFIG=python3-config ./configure --enable-openmp --prefix=${HOME}/.local && make -j4 install)
-rm -rf pyHealpix
