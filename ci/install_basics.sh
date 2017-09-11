3d8ccbbeac331beb41392563849c292e6187f331
Martin Reinecke <martin@mpa-garching.mpg.de>
2017-09-11 18:48:55 +0200

    try to use pip to full extent

--- ci/install_basics.sh
+++ ci/install_basics.sh
@@ -2,5 +2,5 @@
 
 apt-get install -y build-essential  git autoconf libtool \
   libfftw3-bin libfftw3-dev libfftw3-double3 libfftw3-long3 libfftw3-quad3 libfftw3-single3 \
-  python python-pip python-dev python-nose python-numpy \
-  python3 python3-pip python3-dev python3-nose python3-numpy
+  python python-pip python-dev python-nose \
+  python3 python3-pip python3-dev python3-nose
