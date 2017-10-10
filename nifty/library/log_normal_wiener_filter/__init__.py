208e34065b2d71afd2cec7e20138149a30ef3e0a
Martin Reinecke <martin@mpa-garching.mpg.de>
2017-10-10 09:23:05 +0200

    remove unnecessary copyright notices

--- nifty/library/log_normal_wiener_filter/__init__.py
+++ nifty/library/log_normal_wiener_filter/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from .log_normal_wiener_filter_curvature import LogNormalWienerFilterCurvature
 from .log_normal_wiener_filter_energy import LogNormalWienerFilterEnergy
--- nifty/library/wiener_filter/__init__.py
+++ nifty/library/wiener_filter/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from .wiener_filter_curvature import WienerFilterCurvature
 from .wiener_filter_energy import WienerFilterEnergy
