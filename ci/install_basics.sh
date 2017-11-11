f160dafd9354fec4e2561456564b463008fea4ca
Martin Reinecke <martin@mpa-garching.mpg.de>
2017-11-11 17:13:46 +0100

    install MPI

--- ci/install_basics.sh
+++ ci/install_basics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get install -y build-essential  git autoconf libtool pkg-config \
-  libfftw3-bin libfftw3-dev libfftw3-double3 libfftw3-long3 libfftw3-quad3 libfftw3-single3 \
+  libfftw3-bin libfftw3-dev libfftw3-double3 libfftw3-long3 libfftw3-quad3 libfftw3-single3 openmpi-bin libopenmpi-dev \
   python  python-pip  python-dev  python-nose  python-numpy  python-matplotlib  python-future \
   python3 python3-pip python3-dev python3-nose python3-numpy python3-matplotlib python3-future
