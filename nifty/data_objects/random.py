7c69cf194f89f0cdcf41509653c7802b72045cf5
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-01-11 15:14:51 +0100

    more fixes

--- nifty/data_objects/random.py
+++ nifty/data_objects/random.py
@@ -32,12 +32,15 @@ class Random(object):
 
     @staticmethod
     def normal(dtype, shape, mean=0., std=1.):
+        if not (np.issubdtype(dtype, np.floating) or
+                np.issubdtype(dtype, np.complexfloating)):
+            raise TypeError("dtype must be float or complex")
         if not np.isscalar(mean) or not np.isscalar(std):
             raise TypeError("mean and std must be scalars")
         if np.issubdtype(type(std), np.complexfloating):
             raise TypeError("std must not be complex")
-        if ((not np.issubdtype(dtype, np.complexfloating)) and 
-               np.issubdtype(type(mean), np.complexfloating)):
+        if ((not np.issubdtype(dtype, np.complexfloating)) and
+                np.issubdtype(type(mean), np.complexfloating)):
             raise TypeError("mean must not be complex for a real result field")
         if np.issubdtype(dtype, np.complexfloating):
             x = np.empty(shape, dtype=dtype)
@@ -51,7 +54,7 @@ class Random(object):
     def uniform(dtype, shape, low=0., high=1.):
         if not np.isscalar(low) or not np.isscalar(high):
             raise TypeError("low and high must be scalars")
-        if (np.issubdtype(type(low), np.complexfloating) or 
+        if (np.issubdtype(type(low), np.complexfloating) or
                 np.issubdtype(type(high), np.complexfloating)):
             raise TypeError("low and high must not be complex")
         if np.issubdtype(dtype, np.complexfloating):
@@ -59,7 +62,7 @@ class Random(object):
             x.real = np.random.uniform(low, high, shape)
             x.imag = np.random.uniform(low, high, shape)
         elif np.issubdtype(dtype, np.integer):
-            if not (np.issubdtype(type(low), np.integer) and 
+            if not (np.issubdtype(type(low), np.integer) and
                     np.issubdtype(type(high), np.integer)):
                 raise TypeError("low and high must be integer")
             x = np.random.randint(low, high+1, shape)
