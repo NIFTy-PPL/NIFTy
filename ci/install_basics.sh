9658128949be7967ed53cc336d692b7164f55505
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-02-01 16:01:59 +0100

    more tweaking

--- ci/install_basics.sh
+++ ci/install_basics.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install -y build-essential python python-pip python-dev git autoconf libtool gsl-bin libgsl-dev wget python3 python3-pip python3-dev
+apt-get install -y build-essential python python-pip python-dev git autoconf libtool gsl-bin libgsl-dev wget python3 python3-pip python3-dev python3-nose
