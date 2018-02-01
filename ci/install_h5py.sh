aef529d25f5f3337961d38e5c9e2c917cce5ece8
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-02-01 13:31:53 +0100

    try to fix CI

--- ci/install_h5py.sh
+++ ci/install_h5py.sh
@@ -2,3 +2,4 @@
 
 apt-get install -y libhdf5-10 libhdf5-dev libhdf5-openmpi-10 libhdf5-openmpi-dev hdf5-tools
 CC=mpicc HDF5_DIR=/usr/lib/x86_64-linux-gnu/hdf5/openmpi HDF5_MPI="ON" pip install --no-binary=h5py h5py
+CC=mpicc HDF5_DIR=/usr/lib/x86_64-linux-gnu/hdf5/openmpi HDF5_MPI="ON" pip3 install --no-binary=h5py h5py
