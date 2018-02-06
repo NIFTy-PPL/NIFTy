af4e6a3f392c9aa321e9421d899dedb3cce65419
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-02-06 14:31:33 +0100

    make test work with MPI

--- test/test_operators/test_laplace_operator.py
+++ test/test_operators/test_laplace_operator.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright(C) 2013-2017 Max-Planck-Society
+# Copyright(C) 2013-2018 Max-Planck-Society
 #
 # NIFTy is being developed at the Max-Planck-Institut fuer Astrophysik
 # and financially supported by the Studienstiftung des deutschen Volkes.
@@ -43,9 +43,7 @@ class LaplaceOperatorTests(unittest.TestCase):
         foo = ift.PS_field(p, lambda k: 2*k**2)
         L = ift.LaplaceOperator(p, logarithmic=False)
 
-        result = ift.Field(p, val=2*2.)
-        result.val[0] = 0.
-        result.val[1] = 0.
-        result.val[-1] = 0.
+        result = np.full(p.shape, 2*2.)
+        result[0] = result[1] = result[-1] = 0.
 
-        assert_allclose(L(foo).val, result.val)
+        assert_allclose(ift.dobj.to_global_data(L(foo).val), result)
