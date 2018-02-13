1597f6664815aef62a6f2a02d4e600e3ea22adba
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-02-13 09:36:38 +0100

    rework imports

--- nifty4/__init__.py
+++ nifty4/__init__.py
@@ -2,60 +2,22 @@ from .version import __version__
 
 from . import dobj
 
-from .spaces.domain import Domain
-from .spaces.unstructured_domain import UnstructuredDomain
-from .spaces.structured_domain import StructuredDomain
-from .spaces.rg_space import RGSpace
-from .spaces.lm_space import LMSpace
-from .spaces.hp_space import HPSpace
-from .spaces.gl_space import GLSpace
-from .spaces.dof_space import DOFSpace
-from .spaces.power_space import PowerSpace
+from .spaces import *
 
 from .domain_tuple import DomainTuple
 
-from .operators.linear_operator import LinearOperator
-from .operators.endomorphic_operator import EndomorphicOperator
-from .operators.scaling_operator import ScalingOperator
-from .operators.diagonal_operator import DiagonalOperator
-from .operators.harmonic_transform_operator import HarmonicTransformOperator
-from .operators.fft_operator import FFTOperator
-from .operators.fft_smoothing_operator import FFTSmoothingOperator
-from .operators.geometry_remover import GeometryRemover
-from .operators.laplace_operator import LaplaceOperator
-from .operators.power_projection_operator import PowerProjectionOperator
-from .operators.inversion_enabler import InversionEnabler
+from .operators import *
 
 from .field import Field, sqrt, exp, log
 
 from .probing.utils import probe_with_posterior_samples, probe_diagonal
 
-from .minimization.line_search import LineSearch
-from .minimization.line_search_strong_wolfe import LineSearchStrongWolfe
-from .minimization.iteration_controller import IterationController
-from .minimization.gradient_norm_controller import GradientNormController
-from .minimization.minimizer import Minimizer
-from .minimization.conjugate_gradient import ConjugateGradient
-from .minimization.nonlinear_cg import NonlinearCG
-from .minimization.descent_minimizer import DescentMinimizer
-from .minimization.steepest_descent import SteepestDescent
-from .minimization.vl_bfgs import VL_BFGS
-from .minimization.relaxed_newton import RelaxedNewton
-from .minimization.scipy_minimizer import NewtonCG, L_BFGS_B
-from .minimization.energy import Energy
-from .minimization.quadratic_energy import QuadraticEnergy
-from .minimization.line_energy import LineEnergy
+from .minimization import *
 
 from .sugar import *
 from .plotting.plot import plot
 from . import library
 from . import extra
 
-__all__ = ["Domain", "UnstructuredDomain", "StructuredDomain",
-           "RGSpace", "LMSpace", "HPSpace", "GLSpace", "DOFSpace",
-           "PowerSpace", "DomainTuple",
-           "LinearOperator", "EndomorphicOperator", "ScalingOperator",
-           "DiagonalOperator", "FFTOperator", "FFTSmoothingOperator",
-           "LaplaceOperator",
-           "PowerProjectionOperator", "InversionEnabler",
-           "Field", "sqrt", "exp", "log"]
+__all__ = spaces.__all__ + operators.__all__ +minimization.__all__ + \
+          ["DomainTuple", "Field", "sqrt", "exp", "log"]
---
+++ nifty4/operators/__init__.py
@@ -0,0 +1,16 @@
+from .linear_operator import LinearOperator
+from .endomorphic_operator import EndomorphicOperator
+from .scaling_operator import ScalingOperator
+from .diagonal_operator import DiagonalOperator
+from .harmonic_transform_operator import HarmonicTransformOperator
+from .fft_operator import FFTOperator
+from .fft_smoothing_operator import FFTSmoothingOperator
+from .geometry_remover import GeometryRemover
+from .laplace_operator import LaplaceOperator
+from .power_projection_operator import PowerProjectionOperator
+from .inversion_enabler import InversionEnabler
+
+__all__ = ["LinearOperator", "EndomorphicOperator", "ScalingOperator",
+           "DiagonalOperator", "HarmonicTransformOperator", "FFTOperator",
+           "FFTSmoothingOperator", "GeometryRemover",
+           "LaplaceOperator", "PowerProjectionOperator", "InversionEnabler"]
