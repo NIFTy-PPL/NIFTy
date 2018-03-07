aa1e2272855fd9a299e7864b389685eb4b1ce79e
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-03-07 11:40:12 +0100

    fix

--- nifty4/version.py
+++ nifty4/version.py
@@ -2,4 +2,12 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
+
 __version__ = '3.9.0'
+
+def gitversion():
+    try:
+        from .git_version import get_gitversion
+    except ImportError:
+        return "unknown"
+    return get_gitversion()
