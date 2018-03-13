91287fb038e9fcbf0651306099ceebdf37f0dc56
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-03-13 14:24:54 +0100

    improve PNG logos; add SVG sources

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -161,7 +161,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'nifty_logo_white.png'
+html_logo = 'nifty_logo_black.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
