bf16eb35d00bc1f03c371dc9906e86ac39077be1
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-03-20 10:14:37 +0100

    remove superfluous files

--- cleanup.sh
+++
@@ -1,9 +0,0 @@
-find . -name "*.pyc" -delete
-find . -name "__pycache__" -exec rm -rf {} \; 2> /dev/null
-rm -rf build dist oprofile_data
-rm -rf *.egg-info .eggs
-rm -f log.log*
-rm -f *.pdf *.png
-rm -rf nifty2go
-find . -type d -empty -delete
-rm -rf docs/build docs/source/mod
