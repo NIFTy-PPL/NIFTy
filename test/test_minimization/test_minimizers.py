854c63cad66ab5485f8121a8f0c6552d4507bf39
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-04-20 10:02:38 +0200

    add Yango tests

--- test/test_minimization/test_minimizers.py
+++ test/test_minimization/test_minimizers.py
@@ -35,7 +35,8 @@ minimizers = ['ift.VL_BFGS(IC)',
               'ift.NonlinearCG(IC, "5.49")',
               'ift.NewtonCG(xtol=1e-5, maxiter=1000)',
               'ift.L_BFGS_B(ftol=1e-10, gtol=1e-5, maxiter=1000)',
-              'ift.L_BFGS(IC)']
+              'ift.L_BFGS(IC)',
+              'ift.Yango(IC)']
 
 newton_minimizers = ['ift.RelaxedNewton(IC)']
 quadratic_only_minimizers = ['ift.ConjugateGradient(IC)',
