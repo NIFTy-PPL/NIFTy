5cf3ed2d048476a84c85474d82e2b2b289f9659d
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-05-02 10:51:41 +0200

    cleanup

--- nifty4/multi/__init__.py
+++ nifty4/multi/__init__.py
@@ -1,12 +1,4 @@
 from .multi_domain import MultiDomain
 from .multi_field import MultiField
-#from .multi_linear_operator import MultiLinearOperator
-#from .multi_endomorphic_operator import MultiEndomorphicOperator
-#from .multi_chain_operator import MultiChainOperator
-#from .multi_sum_operator import MultiSumOperator
-#from .multi_scaling_operator import MultiScalingOperator
 
 __all__ = ["MultiDomain", "MultiField"]
-#, "MultiLinearOperator",
-#           "MultiEndomorphicOperator", "MultiChainOperator",
-#           "MultiSumOperator", "MultiScalingOperator"]
