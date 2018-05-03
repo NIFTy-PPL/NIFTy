b5b4457311c633d79559f8046ca09b6bab051728
Philipp Arras <parras@mpa-garching.mpg.de>
2018-05-03 18:32:58 +0200

    Add Test file

---
+++ temp/fun_with_nonlinear_ops.py
@@ -0,0 +1,40 @@
+import numpy as np
+import nifty4 as ift
+import nifty4.nonlinear.nonlinear_operator as nl
+
+space = ift.RGSpace(2)
+a = nl.NLOp_var(space)
+
+
+def takeOp(op, at, out):
+    print('E(x) = ', op.value(at))
+    print('dE/dx = ', op.derivative)
+    gradient = op.derivative.value(at)
+    print('dE/dx|(2,5) = ', gradient)
+    dom = gradient.domain
+    grad1 = gradient(ift.Field(dom, np.array([1, 0]))).val
+    grad2 = gradient(ift.Field(dom, np.array([0, 1]))).val
+    grad = np.array([grad1, grad2])
+    print('dE/dx|(2,5) (1,0) = ', grad[0])
+    print('dE/dx|(2,5) (0, 1) = ', grad[1])
+    print()
+    np.testing.assert_allclose(grad, out)
+
+
+# space -> space
+x = ift.Field(space, val=np.array([2, 5]))
+takeOp(a, x, np.array([[1, 0], [0, 1]]))
+takeOp(2*a, x, np.array([[2, 0], [0, 2]]))
+takeOp(a*a, x, np.diagflat(2*x.val))
+takeOp(nl.NLOp_const(4), x, np.zeros((2, 2)))
+
+# # space -> float
+# for E in [nl.NLOp_vdot(a, a), nl.NLOp_vdot(a, a) * nl.NLOp_vdot(a, a)]:
+#     x = ift.Field(space, val=np.array([2,5]))
+#     print('E(x) = ', E.value(x))
+#     print('dE/dx = ', E.derivative)
+#     print('dE/dx|(2,5) = ', E.derivative.value(x))
+#     dom = E.derivative.value(x).domain
+#     #print('dE/dx|(2,5) (1,1) = ', E.derivative.value(x)(ift.Field.full(dom, 1.)))
+#     print()
+# exit()
