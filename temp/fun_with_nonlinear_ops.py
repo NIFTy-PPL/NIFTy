e059d393127745dbd17da4d62a345ed2f4118fec
Philipp Arras <parras@mpa-garching.mpg.de>
2018-05-04 17:13:06 +0200

    First 2d test working

--- temp/fun_with_nonlinear_ops.py
+++ temp/fun_with_nonlinear_ops.py
@@ -66,3 +66,55 @@ def takeOp2D1D(op, at, out):
 
 takeOp2D1D(nl.NLOp_vdot(a, a), x, 2*x.val)
 takeOp2D1D(nl.NLOp_vdot(a, a) * nl.NLOp_vdot(a, a), x, 4*(x.vdot(x))*x.val)
+
+print()
+print()
+print('Start testing 2D examples.')
+print()
+print()
+
+space = ift.RGSpace((2, 2))
+a = nl.NLOp_var(space)
+
+
+# space -> space
+def takeOp(op, at, out):
+    print('E(x) = ', op.value(at))
+    print('dE/dx = ', op.derivative)
+    gradient = op.derivative.value(at)
+    print('dE/dx|(2,5) = ', gradient)
+    dom = gradient.domain
+    grad11 = gradient(ift.Field(dom, np.array([[1., 0.], [0., 0.]]))).val
+    grad12 = gradient(ift.Field(dom, np.array([[0., 1.], [0., 0.]]))).val
+    grad21 = gradient(ift.Field(dom, np.array([[0., 0.], [1., 0.]]))).val
+    grad22 = gradient(ift.Field(dom, np.array([[0., 0.], [0., 1.]]))).val
+    grad = np.array([[grad11, grad12], [grad21, grad22]])
+    print('dE/dx|((2,5),(1,3)) ((2,5),(1,3)) = \n', grad)
+    print()
+    np.testing.assert_allclose(grad, out)
+
+
+x = ift.Field(space, val=np.array([[2., 5.], [1., 3.]]))
+grad = np.zeros((2, 2, 2, 2))
+grad[0,0,0,0] = 1.
+grad[0,1,0,1] = 1.
+grad[1,0,1,0] = 1.
+grad[1,1,1,1] = 1.
+takeOp(a, x, grad)
+exit()
+takeOp(2*a, x, np.array([[2., 0.], [0., 2.]]))
+takeOp(a*a, x, np.diagflat(2*x.val))
+takeOp(nl.NLOp_const(4) + 0*a, x, np.zeros((2, 2)))
+takeOp(nl.NLOp_Exp(a), x, np.diagflat(np.exp(x.val)))
+takeOp(nl.NLOp_Exp(a*a), x, np.diagflat(2*x.val*np.exp((x**2).val)))
+takeOp(nl.NLOp_neg(a), x, np.diagflat(-np.ones_like(x.val)))
+# takeOp(nl.NLOp_Tanh(a), x, np.diagflat(1. - np.tanh(x.val))**2)
+exit()
+
+a11, a12, a21, a22 = x.val.flatten()
+grad = np.zeros((2, 2))
+grad[0, 0] = 3*a1**2+a2**2
+grad[0, 1] = 2*a1*a2
+grad[1, 0] = 2*a1*a2
+grad[1, 1] = a1**2+3*a2**2
+takeOp(nl.NLOp_mul(a, nl.NLOp_vdot(a, a), False, True), x, grad)
