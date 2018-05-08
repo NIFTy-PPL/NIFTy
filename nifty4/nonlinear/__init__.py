8f5fd861d15b20635abcaef40bcf998af94bedf2
Philipp Arras <parras@mpa-garching.mpg.de>
2018-05-08 17:43:54 +0200

    Add Chain skeleton

--- nifty4/nonlinear/__init__.py
+++ nifty4/nonlinear/__init__.py
@@ -1,5 +1,6 @@
 from .nonlinear_tensor import NLTensor
 from .add import TensorAdd
 from .variable import Variable
+from .chain import Chain
 
-__all__ = ['NLTensor', 'TensorAdd', 'Variable']
+__all__ = ['NLTensor', 'TensorAdd', 'Variable', 'Chain']
---
+++ nifty4/nonlinear/chain.py
@@ -0,0 +1,16 @@
+from . import NLTensor
+
+
+class Chain(NLTensor):
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        pass
+
+    def eval(self, x):
+        pass
+
+    @property
+    def derivative(self):
+        pass
