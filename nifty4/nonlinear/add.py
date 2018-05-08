eef86f77cf00ccb0454a92cc0538a54a1ae9b5e7
Philipp Arras <parras@mpa-garching.mpg.de>
2018-05-08 17:32:18 +0200

    fixup! Add Add operator

--- nifty4/nonlinear/add.py
+++ nifty4/nonlinear/add.py
@@ -7,8 +7,8 @@ class Tensor_Add(NLTensor):
         self._fst = fst
         self._snd = snd
 
-    def __call__(x):
-        pass
+    def __call__(self, x):
+        return self._fst(x) + self._snd(x)
 
     def eval(self, x):
         return self._fst.eval(x) + self._snd.eval(x)
