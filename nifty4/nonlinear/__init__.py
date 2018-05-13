63d53b19cbf9770d709f40ce928fdaa087efa15b
Philipp Arras <parras@mpa-garching.mpg.de>
2018-05-13 20:11:35 +0200

    One more test working

--- nifty4/nonlinear/__init__.py
+++ nifty4/nonlinear/__init__.py
@@ -1,7 +1,7 @@
-from .nonlinear_tensor import NLTensor, NLChain, NLCABF, NLVdot, NLScalarMul, NLApplyForm
+from .nonlinear_tensor import NLTensor, NLChain, NLCABF, NLVdot, NLScalarMul, NLApplyForm, NLChainLinOps
 from .add import NLTensorAdd
 from .variable import NLVariable
 from .constant import NLConstant, NLZero
 from .pointwise_nonlinearity import NLExp
 
-__all__ = ['NLTensor', 'NLTensorAdd', 'NLVariable', 'NLChain', 'NLConstant', 'NLCABF', 'NLExp', 'NLZero', 'NLVdot', 'NLScalarMul', 'NLApplyForm']
+__all__ = ['NLTensor', 'NLTensorAdd', 'NLVariable', 'NLChain', 'NLConstant', 'NLCABF', 'NLExp', 'NLZero', 'NLVdot', 'NLScalarMul', 'NLApplyForm', 'NLChainLinOps']
--- nifty4/nonlinear/pointwise_nonlinearity.py
+++ nifty4/nonlinear/pointwise_nonlinearity.py
@@ -1,5 +1,6 @@
-from . import NLTensor, NLCABF
+from . import NLTensor, NLChainLinOps
 from ..field import exp
+from ..operators import DiagonalOperator
 
 
 class NLExp(NLTensor):
@@ -19,5 +20,24 @@ class NLExp(NLTensor):
 
     @property
     def derivative(self):
+        return NLChainLinOps(NLDiag(NLExp(self._inner)), self._inner.derivative)
+
+
+class NLDiag(NLTensor):
+    def __init__(self, diag):
+        assert diag.rank == 1
+        self._diag = diag
+        self._indices = (diag.indices[0], -diag.indices[0])
+
+    def __call__(self, x):
         raise NotImplementedError
-        # return NLCABF(NLDiag(NLExp(self._inner), -1), self._inner.derivative)
+
+    def __str__(self):
+        return 'diag({})'.format(self._diag)
+
+    def eval(self, x):
+        return DiagonalOperator(self._diag.eval(x))
+
+    @property
+    def derivative(self):
+        raise NotImplementedError
