bb00af76a623803d3998b63cdb1e4cecd3318c80
Philipp Arras <parras@mpa-garching.mpg.de>
2018-05-13 22:27:10 +0200

    Clean up

--- nifty4/nonlinear/variable.py
+++ nifty4/nonlinear/variable.py
@@ -1,5 +1,5 @@
 from . import NLTensor
-from ..operators import Tensor, RowOperator
+from ..operators import Tensor
 from .constant import NLConstant
 
 
@@ -16,33 +16,6 @@ class NLVariable(NLTensor):
 
     def eval(self, x):
         return x
-
-    @property
-    def adjoint(self):
-        return NLAdjointVariable(self._domain)
-
-    @property
-    def derivative(self):
-        return NLConstant(Tensor(1., 2, self._domain), self._indices + (-1, ))
-
-
-class NLAdjointVariable(NLTensor):
-    def __init__(self, domain):
-        self._domain = domain
-        self._indices = (-1,)
-
-    def __call__(self, x):
-        raise ValueError
-
-    def __str__(self):
-        return 'var^dagger_{}'.format(self._indices)
-
-    def eval(self, x):
-        return RowOperator(x)
-
-    @property
-    def adjoint(self):
-        return NLVariable(self._domain)
 
     @property
     def derivative(self):
