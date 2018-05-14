bcedc25eaa99e482c413ca80eee30029b0261745
Philipp Arras <parras@mpa-garching.mpg.de>
2018-05-14 21:21:12 +0200

    Cleanup

--- nifty4/nonlinear/__init__.py
+++ nifty4/nonlinear/__init__.py
@@ -1,12 +1,16 @@
-from .tensor import NLTensor
 from .add import NLAdd
 from .adjoint import NLAdjoint
 from .constant import NLConstant
-from .contractions import NLChain, NLChainLinOps, NLChainLinOps11, NLCABF, NLCABL, NLVdot, NLOuterProd, NLApplyForm
+from .contractions import (NLCABF, NLCABL, NLApplyForm, NLChain, NLChainLinOps,
+                           NLChainLinOps11, NLOuterProd, NLVdot)
 from .diag import NLDiag
 from .pointwise_nonlinearity import NLExp
 from .scalar_mul import NLScalarMul
+from .tensor import NLTensor
 from .variable import NLVariable
 from .zero import NLZero
 
-__all__ = ['NLTensor', 'NLAdd', 'NLVariable', 'NLChain', 'NLConstant', 'NLCABF', 'NLExp', 'NLZero', 'NLVdot', 'NLScalarMul', 'NLApplyForm', 'NLChainLinOps']
+__all__ = ['NLTensor', 'NLAdd', 'NLAdjoint', 'NLConstant', 'NLChain',
+           'NLChainLinOps', 'NLChainLinOps11', 'NLCABF', 'NLCABL', 'NLVdot',
+           'NLOuterProd', 'NLApplyForm', 'NLDiag', 'NLExp', 'NLScalarMul',
+           'NLVariable', 'NLZero']
