55112079b81527a7e79324d381166b260b7a1042
Philipp Arras <parras@mpa-garching.mpg.de>
2018-06-18 17:34:03 +0200

    Addition for energies

--- nifty4/library/__init__.py
+++ nifty4/library/__init__.py
@@ -1,8 +1,9 @@
-from .wiener_filter_energy import WienerFilterEnergy
-from .wiener_filter_curvature import WienerFilterCurvature
+from .los_response import LOSResponse
 from .noise_energy import NoiseEnergy
 from .nonlinear_power_energy import NonlinearPowerEnergy
 from .nonlinear_wiener_filter_energy import NonlinearWienerFilterEnergy
+from .nonlinearities import Exponential, Linear, PositiveTanh, Tanh
 from .poisson_energy import PoissonEnergy
-from .nonlinearities import Exponential, Linear, Tanh, PositiveTanh
-from .los_response import LOSResponse
+from .unit_log_gauss import UnitLogGauss
+from .wiener_filter_curvature import WienerFilterCurvature
+from .wiener_filter_energy import WienerFilterEnergy
