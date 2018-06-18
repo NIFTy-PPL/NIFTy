208763a9cf4aee95ae7eac282220c0e57c18e1ad
Philipp Arras <parras@mpa-garching.mpg.de>
2018-06-18 12:29:48 +0200

    Remove Krylov sampling

--- nifty4/library/__init__.py
+++ nifty4/library/__init__.py
@@ -5,5 +5,4 @@ from .nonlinear_power_energy import NonlinearPowerEnergy
 from .nonlinear_wiener_filter_energy import NonlinearWienerFilterEnergy
 from .poisson_energy import PoissonEnergy
 from .nonlinearities import Exponential, Linear, Tanh, PositiveTanh
-from .krylov_sampling import generate_krylov_samples
 from .los_response import LOSResponse
