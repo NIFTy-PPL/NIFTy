41d128bb2e457047946f0609b23890d96bdbaa71
Reimar Leike <reimar@leike.name>
2018-06-27 16:14:59 +0200

    deleted old PoissonEnergy

--- nifty5/library/__init__.py
+++ nifty5/library/__init__.py
@@ -1,7 +1,6 @@
 from .los_response import LOSResponse
 from .nonlinear_wiener_filter_energy import NonlinearWienerFilterEnergy
 from .nonlinearities import Exponential, Linear, PositiveTanh, Tanh
-from .poisson_energy import PoissonEnergy
 from .unit_log_gauss import UnitLogGauss
 from .poisson_log_likelihood import PoissonLogLikelihood
 from .wiener_filter_curvature import WienerFilterCurvature
