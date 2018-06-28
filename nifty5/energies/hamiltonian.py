91572b1b765b57dd4153e04c414287ca7c74ea8b
Philipp Arras <parras@mpa-garching.mpg.de>
2018-06-28 21:17:20 +0200

    Cosmetics

--- nifty5/energies/hamiltonian.py
+++ nifty5/energies/hamiltonian.py
@@ -1,8 +1,26 @@
-from ..minimization.energy import Energy
-from ..operators import InversionEnabler, SamplingEnabler
-from ..models.variable import Variable
-from ..utilities import memo
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright(C) 2013-2018 Max-Planck-Society
+#
+# NIFTy is being developed at the Max-Planck-Institut fuer Astrophysik
+# and financially supported by the Studienstiftung des deutschen Volkes.
+
 from ..library.gaussian_energy import GaussianEnergy
+from ..minimization.energy import Energy
+from ..models.variable import Variable
+from ..operators import InversionEnabler, SamplingEnabler
+from ..utilities import memo
 
 
 class Hamiltonian(Energy):
