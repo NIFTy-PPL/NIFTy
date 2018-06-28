5a09d1ae336a16bcb9e50cee67512ef176acd2d6
Philipp Arras <parras@mpa-garching.mpg.de>
2018-06-28 21:21:12 +0200

    Comment

--- nifty5/library/apply_data.py
+++ nifty5/library/apply_data.py
@@ -1,4 +1,5 @@
 def ApplyData(data, var, model_data):
+    # TODO This is rather confusing. Delete that eventually.
     from .. import DiagonalOperator, Constant, sqrt
     sqrt_n = DiagonalOperator(sqrt(var))
     data = Constant(model_data.position, data)
