5862027f04af7938d9ba54afb49510253b1f9ee7
Philipp Arras <parras@mpa-garching.mpg.de>
2018-06-28 20:48:31 +0200

    Delete NonlinearWienerFilterEnergy

--- nifty5/library/__init__.py
+++ nifty5/library/__init__.py
@@ -2,7 +2,6 @@ from .amplitude_model import make_amplitude_model
 from .apply_data import ApplyData
 from .gaussian_energy import GaussianEnergy
 from .los_response import LOSResponse
-from .nonlinear_wiener_filter_energy import NonlinearWienerFilterEnergy
 from .point_sources import PointSources
 from .poissonian_energy import PoissonianEnergy
 from .smooth_sky import make_smooth_mf_sky_model, make_smooth_sky_model
