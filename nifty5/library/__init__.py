a91efaa59be47653bfa803b350d8840d6c01fbf8
Philipp Arras <parras@mpa-garching.mpg.de>
2018-06-28 20:46:11 +0200

    Rename PoissonLogLikelihood -> PoissonianEnergy

--- nifty5/library/__init__.py
+++ nifty5/library/__init__.py
@@ -1,10 +1,10 @@
 from .amplitude_model import make_amplitude_model
 from .apply_data import ApplyData
+from .gaussian_energy import GaussianEnergy
 from .los_response import LOSResponse
 from .nonlinear_wiener_filter_energy import NonlinearWienerFilterEnergy
-from .gaussian_energy import GaussianEnergy
 from .point_sources import PointSources
-from .poisson_log_likelihood import PoissonLogLikelihood
+from .poissonian_energy import PoissonianEnergy
 from .smooth_sky import make_smooth_mf_sky_model, make_smooth_sky_model
 from .wiener_filter_curvature import WienerFilterCurvature
 from .wiener_filter_energy import WienerFilterEnergy
