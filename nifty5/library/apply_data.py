2dfc44946211f8fa979f684d7d5fd1e1f3e485b9
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-06-29 11:46:15 +0200

    tweak imports

--- nifty5/library/apply_data.py
+++ nifty5/library/apply_data.py
@@ -1,6 +1,8 @@
 def ApplyData(data, var, model_data):
     # TODO This is rather confusing. Delete that eventually.
-    from .. import DiagonalOperator, Constant, sqrt
+    from ..operators.diagonal_operator import DiagonalOperator
+    from ..models.constant import Constant
+    from ..sugar import sqrt
     sqrt_n = DiagonalOperator(sqrt(var))
     data = Constant(model_data.position, data)
     return sqrt_n.inverse(model_data - data)
