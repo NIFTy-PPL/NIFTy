3d6b510e522abba97bbcf5fd444a617d05c58666
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-07-01 13:37:34 +0200

    try to simplify energies, stage 1

--- nifty5/energies/kl.py
+++ nifty5/energies/kl.py
@@ -1,11 +1,10 @@
+from builtins import *
 from ..minimization.energy import Energy
-from ..operators.inversion_enabler import InversionEnabler
-from ..operators.scaling_operator import ScalingOperator
 from ..utilities import memo
 
 
 class SampledKullbachLeiblerDivergence(Energy):
-    def __init__(self, h, res_samples, iteration_controller):
+    def __init__(self, h, res_samples):
         """
         h: Hamiltonian
         N: Number of samples to be used
@@ -13,42 +12,28 @@ class SampledKullbachLeiblerDivergence(Energy):
         super(SampledKullbachLeiblerDivergence, self).__init__(h.position)
         self._h = h
         self._res_samples = res_samples
-        self._iteration_controller = iteration_controller
 
-        self._energy_list = []
-        for ss in res_samples:
-            e = h.at(self.position+ss)
-            self._energy_list.append(e)
+        self._energy_list = tuple(h.at(self.position+ss)
+                                  for ss in res_samples)
 
     def at(self, position):
-        return self.__class__(self._h.at(position), self._res_samples,
-                              self._iteration_controller)
+        return self.__class__(self._h.at(position), self._res_samples)
 
 
     @property
     @memo
     def value(self):
-        v = self._energy_list[0].value
-        for energy in self._energy_list[1:]:
-            v += energy.value
-        return v / len(self._energy_list)
+        return (my_sum(map(lambda v: v.value, self._energy_list)) /
+                len(self._energy_list))
 
     @property
     @memo
     def gradient(self):
-        g = self._energy_list[0].gradient
-        for energy in self._energy_list[1:]:
-            g += energy.gradient
-        return g / len(self._energy_list)
+        return (my_sum(map(lambda v: v.gradient, self._energy_list)) /
+                len(self._energy_list))
 
     @property
     @memo
     def curvature(self):
-        # MR FIXME: This looks a bit strange...
-        approx = self._energy_list[-1]._prior.curvature
-        curvature_list = [e.curvature for e in self._energy_list]
-        op = curvature_list[0]
-        for curv in curvature_list[1:]:
-            op = op + curv
-        op = op * ScalingOperator(1./len(curvature_list), op.domain)
-        return InversionEnabler(op, self._iteration_controller, approx)
+       return (my_sum(map(lambda v: v.curvature, self._energy_list)) *
+               (1./len(self._energy_list)))
