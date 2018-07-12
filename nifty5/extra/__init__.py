0f33fd7ab63c5d3974980e9d8cda4219015fb632
Philipp Arras <parras@mpa-garching.mpg.de>
2018-07-12 15:39:30 +0200

    Add join MultiFields

--- nifty5/extra/__init__.py
+++ nifty5/extra/__init__.py
@@ -1,2 +1,3 @@
 from .operator_tests import consistency_check
 from .energy_and_model_tests import *
+from .join_multifields import join_multifields
---
+++ nifty5/extra/join_multifields.py
@@ -0,0 +1,31 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright(C) 2013-2018 Max-Planck-Society
+#
+# NIFTy is being developed at the Max-Planck-Institut fuer Astrophysik
+# and financially supported by the Studienstiftung des deutschen Volkes.
+
+from __future__ import absolute_import, division, print_function
+
+from ..compat import *
+from ..multi.multi_field import MultiField
+
+__all__ = ['join_multifields']
+
+
+def join_multifields(f1, f2):
+    f1 = f1.to_dict()
+    f2 = f2.to_dict()
+    f2.update(f1)
+    return MultiField.from_dict(f2)
