cb997558239e1a4fa3cf23e8c950695fa655c4a3
Philipp Arras <parras@mpa-garching.mpg.de>
2018-07-13 22:21:57 +0200

    Merge! Simplify make_correlated_field

--- nifty5/library/correlated_fields.py
+++ nifty5/library/correlated_fields.py
@@ -44,13 +44,10 @@ def make_correlated_field(s_space, amplitude_model):
     ht = FFTOperator(h_space, s_space)
     p_space = amplitude_model.value.domain[0]
     power_distributor = PowerDistributor(h_space, p_space)
-    # FIXME Remove tau and phi stuff from here. Should not be necessary
-    position = MultiField.from_dict({
-        'xi': Field.from_random('normal', h_space),
-        'tau': amplitude_model.position['tau'],
-        'phi': amplitude_model.position['phi']})
 
+    position = MultiField.from_dict({'xi': Field.full(h_space, 0.)})
     xi = Variable(position)['xi']
+
     A = power_distributor(amplitude_model)
     correlated_field_h = A * xi
     correlated_field = ht(correlated_field_h)
