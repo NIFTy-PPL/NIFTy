99b34c67f90e2759f2027c91d89b5afcc5c5350b
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-07-31 19:21:17 +0200

    tweak slicing to be compatible with future numpy versions

--- nifty5/utilities.py
+++ nifty5/utilities.py
@@ -86,10 +86,10 @@ def get_slice_list(shape, axes):
             [list(range(y)) for x, y in enumerate(shape) if x not in axes]
         for index in product(*axes_iterables):
             it_iter = iter(index)
-            slice_list = [
+            slice_list = tuple(
                 next(it_iter)
                 if axis else slice(None, None) for axis in axes_select
-            ]
+            )
             yield slice_list
     else:
         yield [slice(None, None)]
@@ -223,7 +223,7 @@ def hartley(a, axes=None):
             axes = tuple(range(tmp.ndim))
         lastaxis = axes[-1]
         ntmplast = tmp.shape[lastaxis]
-        slice1 = [slice(None)]*lastaxis + [slice(0, ntmplast)]
+        slice1 = (slice(None),)*lastaxis + (slice(0, ntmplast),)
         np.add(tmp.real, tmp.imag, out=res[slice1])
 
         def _fill_upper_half(tmp, res, axes):
@@ -236,9 +236,11 @@ def hartley(a, axes=None):
             for i in axes[:-1]:
                 slice1[i] = slice(1, None)
                 slice2[i] = slice(None, 0, -1)
+            slice1 = tuple(slice1)
+            slice2 = tuple(slice2)
             np.subtract(tmp[slice2].real, tmp[slice2].imag, out=res[slice1])
             for i, ax in enumerate(axes[:-1]):
-                dim1 = [slice(None)]*ax + [slice(0, 1)]
+                dim1 = (slice(None),)*ax + (slice(0, 1),)
                 axes2 = axes[:i] + axes[i+1:]
                 _fill_upper_half(tmp[dim1], res[dim1], axes2)
 
