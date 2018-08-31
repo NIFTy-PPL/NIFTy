1255a77996054b2e279eb457e5efc442447f0652
Philipp Arras <parras@mpa-garching.mpg.de>
2018-08-31 15:37:32 +0200

    Add type check to MultiField.from_dict()

--- nifty5/multi_field.py
+++ nifty5/multi_field.py
@@ -24,6 +24,7 @@ from . import utilities
 from .compat import *
 from .field import Field
 from .multi_domain import MultiDomain
+from .domain_tuple import DomainTuple
 
 
 class MultiField(object):
@@ -52,6 +53,9 @@ class MultiField(object):
     @staticmethod
     def from_dict(dict, domain=None):
         if domain is None:
+            for dd in dict.values():
+                if not isinstance(dd.domain, DomainTuple):
+                    raise TypeError('Values of dictionary need to be Fields defined on DomainTuples.')
             domain = MultiDomain.make({key: v._domain
                                        for key, v in dict.items()})
         res = tuple(dict[key] if key in dict else Field(dom, 0)
