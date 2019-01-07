b8bfb322e4f6dbcf6cf4f1f3764ed6bf1e215500
Martin Reinecke <martin@mpa-garching.mpg.de>
2019-01-07 14:25:31 +0100

    fix

--- demos/getting_started_1.py
+++ demos/getting_started_1.py
@@ -101,15 +101,12 @@ if __name__ == '__main__':
     # Masking operator to model that parts of the field have not been observed
     mask = ift.Field.from_global_data(position_space, mask)
     Mask = ift.DiagonalOperator(mask)
-<<<<<<< HEAD
 
-    # The response operator consists out of
+    # The response operator consists of
     # - an harmonic transform (to get to image space)
     # - the application of the mask
     # - the removal of geometric information
-=======
-    # Operators can be composed either with paranthesis
->>>>>>> NIFTy_5
+    # Operators can be composed either with parenthesis
     R = GR(Mask(HT))
     # or with @
     R = GR @ Mask @ HT
@@ -125,17 +122,10 @@ if __name__ == '__main__':
     MOCK_NOISE = N.draw_sample()
     data = R(MOCK_SIGNAL) + MOCK_NOISE
 
-<<<<<<< HEAD
     # Build inverse propagator D and information source j
-    D_inv = R.adjoint(N.inverse(R)) + S.inverse
+    D_inv = R.adjoint @ N.inverse @ R + S.inverse
     j = R.adjoint_times(N.inverse_times(data))
     # Make D_inv invertible (via Conjugate Gradient)
-=======
-    # Build propagator D and information source j
-    j = R.adjoint_times(N.inverse_times(data))
-    D_inv = R.adjoint @ N.inverse @ R + S.inverse
-    # Make it invertible
->>>>>>> NIFTy_5
     IC = ift.GradientNormController(iteration_limit=500, tol_abs_gradnorm=1e-3)
     D = ift.InversionEnabler(D_inv, IC, approximation=S.inverse).inverse
 
