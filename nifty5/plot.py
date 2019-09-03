0a97a9ae5f751983ac663608cde01053c224a4fe
Philipp Arras <parras@mpa-garching.mpg.de>
2019-09-03 12:09:17 +0200

    Multifrequency plotting with only one frequency

--- nifty5/plot.py
+++ nifty5/plot.py
@@ -348,8 +348,12 @@ def _plot2D(f, ax, **kwargs):
     if len(dom) == 2:
         if (not isinstance(dom[1], RGSpace)) or len(dom[1].shape) != 1:
             raise TypeError("need 1D RGSpace as second domain")
-        rgb = _rgb_data(f.to_global_data())
-        have_rgb = True
+        if dom[1].shape[0] == 1:
+            from .sugar import from_global_data
+            f = from_global_data(f.domain[0], f.to_global_data()[..., 0])
+        else:
+            rgb = _rgb_data(f.to_global_data())
+            have_rgb = True
 
     foo = kwargs.pop("norm", None)
     norm = {} if foo is None else {'norm': foo}
