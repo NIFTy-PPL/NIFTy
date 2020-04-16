de7936c9c27ddb28f217183bbadf38642081da64
pfrank <philipp@mpa-garching.mpg.de>
2020-04-16 23:57:06 +0200

    attempt fix GenLeibniz

--- nifty6/diff_tensor.py
+++ nifty6/diff_tensor.py
@@ -215,6 +215,8 @@ class DiagonalTensor(_DiffTensorImpl):
         self._vec = vec
 
     def _helper(self, x, rnk):
+        print(rnk)
+        print(self._rank-len(x))
         if self._rank-len(x) != rnk:
             raise ValueError
         res = self._vec
@@ -272,6 +274,7 @@ class GenLeibnizTensor(_DiffTensorImpl):
         lst = tuple(np.array(i) for i in product([0, 1], repeat=t1.maxorder))
         self._id1 = [np.where(inds == 1)[0] for inds in lst]
         self._id2 = [np.where(inds == 0)[0] for inds in lst]
+        
 
     def getVec(self, x=()):
         if self._rank-len(x) != 1:
@@ -294,9 +297,14 @@ class GenLeibnizTensor(_DiffTensorImpl):
         res = None
         for id1, id2 in zip(self._id1, self._id2):
 # FIXME this is completely wrong, but I don't understand the algorithm in the original _contract_to_one()
-            xx1 = [x1[inp] for inp in id1]
-            xx2 = [x2[inp] for inp in id2]
-            tmp = self._t1[len(xx1)].getLinop(xx1)@self._t2[len(xx2)].getLinop(xx2)
+            xx1 = [x1[inp] for inp in id1 if inp != self._t1.maxorder-1]
+            xx2 = [x2[inp] for inp in id2 if inp != self._t1.maxorder-1]
+            if self._t1.maxorder-1 in id1:
+                tmp = (makeOp(self._t2[len(xx2)].getVec(xx2))@
+                       self._t1[len(xx1)+1].getLinop(xx1))
+            else:
+                tmp = (makeOp(self._t1[len(xx1)].getVec(xx1))@
+                       self._t2[len(xx2)+1].getLinop(xx2))
             res = tmp if res is None else res+tmp
         return res
 
