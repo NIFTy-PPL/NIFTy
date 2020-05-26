0cba5f1fef98888e3d32aa20966ea0c91a46cad4
Vincent Eberle <VE@Vincents-MBP-2.fritz.box>
2020-05-26 14:43:49 +0200

    numpy tests added

--- test/test_operators/test_vdot_operator.py
+++ test/test_operators/test_vdot_operator.py
@@ -28,8 +28,12 @@ def test_vdot_operator():
     fa_2 = ift.FieldAdapter(dom, 'f2')
     op1 = fa_1.vdot(fa_2)
     f = ift.from_random(op1.domain, dtype=np.float64)
+    arr1 = f['f1'].val
+    arr2 = f['f2'].val
     res1 = f['f1'].vdot(f['f2'])
     res2 = op1(f)
+    res3 = np.vdot(arr1, arr2)
     assert_allclose(res1.val, res2.val)
+    assert_allclose(res1.val, res3)
     ift.extra.check_jacobian_consistency(op1, f)
     #another Test for linearization?
