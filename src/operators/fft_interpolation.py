3d7e89946ab3dfaee9fb15fd863016092e98c4e9
Vincent Eberle <VE@Vincents-MBP-2.fritz.box>
2021-01-28 16:12:12 +0100

    authors

--- src/operators/fft_interpolation.py
+++ src/operators/fft_interpolation.py
@@ -12,6 +12,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Copyright(C) 2013-2021 Max-Planck-Society
+# Authors: Vincent Eberle und Philipp Arras
 #
 # NIFTy is being developed at the Max-Planck-Institut fuer Astrophysik.
 
