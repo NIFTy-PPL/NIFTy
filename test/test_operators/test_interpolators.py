5b39cab6b999221f0082692f670684031080551d
Philipp Arras <parras@mpa-garching.mpg.de>
2021-02-01 14:48:56 +0100

    Make tests harder

--- test/test_operators/test_interpolators.py
+++ test/test_operators/test_interpolators.py
@@ -5,7 +5,9 @@ import numpy as np
 """THIS IS A DRAFT VERSION OF A TEST FOR INTERPOLATORS"""
 
 res = 64
-sp = ift.RGSpace([res, res])
+vol = 2
+sp = ift.RGSpace([res, res], [vol/res, vol/res])
+# sp = ift.RGSpace([res, res])
 
 mg = np.mgrid[(slice(0,res),)*2]
 mg = np.array(list(map(np.ravel,mg)))
@@ -25,8 +27,18 @@ out = R(inp).val
 out1 = linInp(inp).val
 
 np.testing.assert_allclose(out, inp.val.reshape(-1))
-# np.testing.assert_allclose(out, inp.val[n, 0])
-# np.testing.assert_allclose(out, out1)
+np.testing.assert_allclose(out1, inp.val.reshape(-1))
+np.testing.assert_allclose(out, out1)
+
+sampling_points = np.array([[0.25], [0.]])
+R = ift.FFTInterpolator(sp, sampling_points)
+R1 = ift.LinearInterpolator(sp, sampling_points)
+
+p = ift.Plot()
+p.add(R.adjoint(ift.full(R.target, 1)), title="FFT")
+p.add(R1.adjoint(ift.full(R.target, 1)), title="Linear")
+p.output(name="debug.png", ny=1, xsize=12)
+
 
 
 #TODO Generate one Fourriermode, read out between gridpoints, check if right value
