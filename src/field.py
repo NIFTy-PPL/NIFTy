2ddea1f2ab6f8c3b7df8ea11d2055d8df85a8df3
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2021-03-16 18:35:38 +0100

    field.py: Format

--- src/field.py
+++ src/field.py
@@ -2,6 +2,7 @@ from jax.numpy import sum, isscalar, array, abs
 from jax.numpy.linalg import norm
 from jax.tree_util import tree_unflatten
 
+
 class Field():
     def __init__(self, domain, val):
         self.val, self.domain = val, domain
@@ -17,7 +18,7 @@ class Field():
             raise TypeError("Can only perform dot product between fields")
         if other.domain != self.domain:
             raise ValueError("domains are incompatible.")
-        res = [sum(v*w) for v,w in zip(self.val, other.val)]
+        res = [sum(v * w) for v, w in zip(self.val, other.val)]
         return sum(array(res))
 
     def squared_norm(self):
@@ -25,7 +26,8 @@ class Field():
         return sum(array(res))
 
     def norm(self, ord):
-        my_norm = lambda x: abs(x) if (isscalar(x) or len(x.shape)==0) else norm(x, ord=ord)
+        my_norm = lambda x: abs(x) if (isscalar(x) or len(x.shape) == 0
+                                      ) else norm(x, ord=ord)
         res = [my_norm(v) for v in self.val]
         return norm(array(res), ord=ord)
 
@@ -48,21 +50,22 @@ class Field():
             from itertools import repeat
             it = repeat(other, len(self.val))
         else:
-            raise ValueError("Invalid binary op for Field and {}".format(type(other)))
-        return self.new([getattr(v,op)(o) for v,o in zip(self.val, it)])
+            raise ValueError(
+                "Invalid binary op for Field and {}".format(type(other))
+            )
+        return self.new([getattr(v, op)(o) for v, o in zip(self.val, it)])
 
 
-for op in ["__add__", "__radd__",
-           "__sub__", "__rsub__",
-           "__mul__", "__rmul__",
-           "__truediv__", "__rtruediv__",
-           "__floordiv__", "__rfloordiv__",
-           "__pow__", "__rpow__",
-           "__lt__", "__le__", "__gt__", "__ge__", "__eq__", "__ne__"]:
+for op in [
+    "__add__", "__radd__", "__sub__", "__rsub__", "__mul__", "__rmul__",
+    "__truediv__", "__rtruediv__", "__floordiv__", "__rfloordiv__", "__pow__",
+    "__rpow__", "__lt__", "__le__", "__gt__", "__ge__", "__eq__", "__ne__"
+]:
+
     def func(op):
         def func2(self, other):
             return self._binary_op(other, op)
+
         return func2
+
     setattr(Field, op, func(op))
-
-
