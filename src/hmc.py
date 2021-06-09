883d21a3223f60bbe735a80cf7b7641a38b94578
Massin Guerdi <massinguerdi@web.de>
2021-06-09 15:55:28 +0200

    fix test run, add arrow plotting, add diagnostics

--- src/hmc.py
+++ src/hmc.py
@@ -78,6 +78,7 @@ def leapfrog_step(
     # return the last two paramters unchanged for iteration in lax.fori_loop
     # TODO: apply last two args partially?
     #return potential_energy, momentum_fullstep, position_fullstep, step_length
+    # TODO: delete step_length
     return qp_fullstep, step_length
 
 
@@ -352,34 +353,48 @@ def test_run_build_tree_rec():
     from jax import grad
     import matplotlib.pyplot as plt
     dims = (2,)
-    potential_energy = lambda q: 0.5 * np.sum(q**2) + 0.5 * np.sum(((q - np.ones(shape=dims)) / np.array([0.3, 3]))**2)
-    kinetic_energy = potential_energy
+    potential_energy = lambda q: 0 * 0.5 * np.sum((q - np.ones(shape=dims))**2) + 0.5 * np.sum(((q - np.ones(shape=dims)) / np.array([0.3, 3]))**2)
+    kinetic_energy = lambda p: 0.5 * np.sum(p**2)
     potential_energy_gradient = grad(potential_energy)
     stepper = lambda qp, eps, direction: leapfrog_step(potential_energy_gradient, qp, eps*direction)[0]
     key = random.PRNGKey(42)
     key, subkey1, subkey2 = random.split(key, 3)
-    initial_qp = QP(position=random.uniform(subkey1, dims), momentum=random.uniform(subkey2, dims))
+    initial_qp = QP(position=random.normal(subkey1, dims), momentum=random.normal(subkey2, dims))
     current_qp = initial_qp
-    for loop_idx in range(10):
+    chosen_states = []
+    proposed_states = []
+    acceptance_probabilities = []
+    acceptance_bools = []
+    plt.plot(current_qp.position[0], current_qp.position[1], 'rx', label='initial position')
+    plt.arrow(current_qp.position[0], current_qp.position[1], 0.1*current_qp.momentum[0], 0.1*current_qp.momentum[1])
+    for loop_idx in range(50):
+        plt.arrow(current_qp.position[0], current_qp.position[1], 0.1*current_qp.momentum[0], 0.1*current_qp.momentum[1])
         key, subkey = random.split(key)
         left_endpoint, right_endpoint, chosen = build_tree_iterative(current_qp, subkey, 0.01194, 6, stepper)
         key, subkey = random.split(key)
         proposed_qp = chosen[random.choice(subkey, np.array(len(chosen)))]
+        proposed_states.append(proposed_qp)
         chosen = np.array(chosen)
+        chosen_states.append(chosen)
         acceptance_probability = np.exp(
             potential_energy(current_qp.position) + kinetic_energy(current_qp.momentum)
             - potential_energy(proposed_qp.position) - kinetic_energy(proposed_qp.momentum)
         )
+        acceptance_probabilities.append(acceptance_probability)
         print("acceptance probability:", acceptance_probability)
         key, subkey = random.split(key)
         acceptance_threshold = random.uniform(subkey)
         if acceptance_threshold < acceptance_probability:
             current_qp = proposed_qp
+            acceptance_bools.append(True)
             print("accepted")
         else:
             print("rejected")
+            acceptance_bools.append(False)
         if True or loop_idx == 2:
-            plt.plot(current_qp.position[0], current_qp.position[1], 'rx', label='initial position')
-            plt.scatter(chosen[:,0,0], chosen[:,0,1], label='chosen states', alpha=0.5)
+            plt.plot(current_qp.position[0], current_qp.position[1], 'rx')
+            plt.arrow(current_qp.position[0], current_qp.position[1], 0.1*current_qp.momentum[0], 0.1*current_qp.momentum[1], alpha=0.2)
+            plt.scatter(chosen[:,0,0], chosen[:,0,1], s=0.2, label='chosen states', alpha=0.9)
         key, subkey = random.split(key)
-        current_qp = QP(position=current_qp.position, momentum=random.uniform(subkey, shape=dims))
+        current_qp = QP(position=current_qp.position, momentum=random.normal(subkey, shape=dims))
+    return initial_qp, chosen_states, proposed_states, acceptance_probabilities, acceptance_bools
