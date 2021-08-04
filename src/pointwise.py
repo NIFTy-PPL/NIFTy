fix gradients of clip and unitstep, add softclip and leakyclip

src/pointwise.py
@@ -77,10 +77,60 @@ def _clip_helper(v, a_min, a_max):
     tmp = np.clip(v, a_min, a_max)
     tmp2 = np.ones(v.shape)
     if a_min is not None:
-        tmp2 = np.where(tmp == a_min, 0., tmp2)
+        tmp2 = np.where(tmp == a_min, np.nan, tmp2)
     if a_max is not None:
-        tmp2 = np.where(tmp == a_max, 0., tmp2)
+        tmp2 = np.where(tmp == a_max, np.nan, tmp2)
     return (tmp, tmp2)
+
+
+def _leakyclip_field(v, a_min, a_max, lower_slope=0.01, upper_slope=0.01):
+    if np.issubdtype(v.dtype, np.complexfloating):
+        raise TypeError("Argument must not be complex")
+    if a_min is not None:
+        v = np.where(v > a_min, v, a_min + (v - a_min) * lower_slope)
+    if a_max is not None:
+        v = np.where(v < a_min, v, a_max + (v - a_max) * upper_slope)
+    return v
+
+
+def _leakyclip_linearization(v,
+                              a_min,
+                              a_max,
+                              lower_slope=0.01,
+                              upper_slope=0.01):
+    if np.issubdtype(v.dtype, np.complexfloating):
+        raise TypeError("Argument must not be complex")
+    grad = np.ones(v.shape)
+    if a_min is not None:
+        cond = v > a_min
+        v = np.where(cond, v, a_min + (v - a_min) * lower_slope)
+        grad = np.where(cond, grad, lower_slope)
+    if a_max is not None:
+        cond = v < a_max
+        v = np.where(cond, v, a_max + (v - a_max) * upper_slope)
+        grad = np.where(cond, grad, upper_slope)
+    return (v, grad)
+
+
+def softclip(v, a_min, a_max):
+    if np.issubdtype(v.dtype, np.complexfloating):
+        raise TypeError("Argument must not be complex")
+    delta_a = np.array(a_max - a_min)
+    c = delta_a / softplus(delta_a)
+    return a_max - c * softplus(delta_a - softplus(v - a_min))
+
+
+def _softclip_helper(v, a_min, a_max):
+    if np.issubdtype(v.dtype, np.complexfloating):
+        raise TypeError("Argument must not be complex")
+    delta_a = np.array(a_max - a_min)
+    c = delta_a / softplus(delta_a)
+    f1, df1 = _softplus_helper(v - a_min)
+    f2, df2 = _softplus_helper(delta_a - f1)
+    val = a_max - c * f2
+    grad = c * df2 * df1  # minus signs cancel
+    return val, grad
+
 
 def _step_helper(v, grad):
     if np.issubdtype(v.dtype, np.complexfloating):
@@ -88,7 +138,7 @@ def _step_helper(v, grad):
     r = np.zeros(v.shape)
     r[v>=0.] = 1.
     if grad:
-        return (r, np.zeros(v.shape))
+        return (r, np.where(v == 0., np.nan, np.zeros(v.shape)))
     return r
 
 def softplus(v):
@@ -148,6 +198,8 @@ ptw_dict = {
     "sign": (np.sign, _sign_helper),
     "power": (np.power, _power_helper),
     "clip": (np.clip, _clip_helper),
+    "softclip": (softclip, _softclip_helper),
+    "leakyclip": (_leakyclip_field, _leakyclip_linearization),
     "softplus": (softplus, _softplus_helper),
     "exponentiate": (exponentiate, _exponentiate_helper),
     "arctan": (np.arctan, lambda v: (np.arctan(v), 1./(1.+v**2))),

test/test_linearization.py
@@ -24,6 +24,7 @@ import nifty8 as ift
 from .common import setup_function, teardown_function
 
 pmp = pytest.mark.parametrize
+no_complex_vals = ['absolute', 'softclip', 'leakyclip']
 
 
 def _lin2grad(lin):
@@ -41,11 +42,10 @@ def test_special_gradients():
     s = f.val
 
     jt(var.clip(0, 10), np.ones_like(s))
-    jt(var.clip(-1, 0), np.zeros_like(s))
+    jt(var.clip(-1, 0), np.full(s.shape, np.nan))
 
     assert_allclose(
         _lin2grad(ift.Linearization.make_var(0*f).ptw("sinc")), np.zeros(s.shape))
-    ift.myassert(np.isnan(_lin2grad(ift.Linearization.make_var(0*f).ptw("abs"))))
     assert_allclose(
         _lin2grad(ift.Linearization.make_var(0*f + 10).ptw("abs")),
         np.ones(s.shape))
@@ -54,18 +54,28 @@ def test_special_gradients():
         -np.ones(s.shape))
 
 
+@pmp('f', ['abs', 'sign', 'unitstep'])
+def test_nan_gradients(f):
+    dom = ift.UnstructuredDomain((1,))
+    lin = ift.Linearization.make_var(ift.full(dom, 0.)).ptw(f)
+    ift.myassert(np.isnan(_lin2grad(lin)))
+
+
 @pmp('f', [
     'log', 'exp', 'sqrt', 'sin', 'cos', 'tan', 'sinc', 'sinh', 'cosh', 'tanh',
     'absolute', 'reciprocal', 'sigmoid', 'log10', 'log1p', 'expm1', 'softplus',
-    ('power', 2.), ('exponentiate', 1.1)
+    ('power', 2.), ('exponentiate', 1.1), ('softclip', -0.5, 0.5),
+    ('leakyclip', -0.5, 0.5)
 ])
 @pmp('cplxpos', [True, False])
 @pmp('cplxdir', [True, False])
 @pmp('holomorphic', [True, False])
 def test_actual_gradients(f, cplxpos, cplxdir, holomorphic):
-    if (cplxpos or cplxdir) and f in ['absolute']:
+    if not isinstance(f, tuple):
+        f = (f,)
+    if (cplxpos or cplxdir) and f[0] in no_complex_vals:
         return
-    if holomorphic and f in ['absolute']:
+    if holomorphic and f[0] in no_complex_vals:
         # These function are not holomorphic
         return
     dom = ift.UnstructuredDomain((1,))
@@ -79,8 +89,6 @@ def test_actual_gradients(f, cplxpos, cplxdir, holomorphic):
         eps *= (1+0.78j)
     var0 = ift.Linearization.make_var(fld)
     var1 = ift.Linearization.make_var(fld + eps)
-    if not isinstance(f, tuple):
-        f = (f,)
     f0 = var0.ptw(*f).val.val
     f1 = var1.ptw(*f).val.val
     df1 = _lin2grad(var0.ptw(*f))