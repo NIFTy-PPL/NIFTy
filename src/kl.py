636f404ffe309cad456b5c08385939e169cfa532
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2021-08-18 13:50:49 +0200

    Always sample from inverse metric
    Hide the option to sample the metric and the inverse metric of the
    `StandardHamiltonian` from the public API.

--- src/kl.py
+++ src/kl.py
@@ -15,17 +15,50 @@ def sample_likelihood(likelihood: Likelihood, primals, key):
     return likelihood.left_sqrt_metric(primals, white_sample)
 
 
-def sample_standard_hamiltonian(
+def _sample_standard_hamiltonian(
     hamiltonian: StandardHamiltonian,
     primals,
     key,
-    from_inverse: bool = False,
+    from_inverse: bool,
     cg: Callable = cg,
-    **cg_kwargs
+    cg_kwargs: Optional[dict] = None
 ):
-    r"""Draws a sample of which the covariance is the metric
-    (`from_inverse=False`) or the inverse metric (`from_inverse=True`) of the
-    Hamiltonian.
+    if not isinstance(hamiltonian, StandardHamiltonian):
+        te = f"`hamiltonian` of invalid type; got '{type(hamiltonian)}'"
+        raise TypeError(te)
+    cg_kwargs = cg_kwargs if cg_kwargs is not None else {}
+
+    subkey_nll, subkey_prr = random.split(key, 2)
+    nll_smpl = sample_likelihood(
+        hamiltonian.likelihood, primals, key=subkey_nll
+    )
+    prr_inv_metric_smpl = random_like(primals, key=subkey_prr)
+    # One may transform any metric sample to a sample of the inverse
+    # metric by simply applying the inverse metric to it
+    prr_smpl = prr_inv_metric_smpl
+    # Note, we can sample antithetically by swapping the global sign of
+    # the metric sample below (which corresponds to mirroring the final
+    # sample) and additionally by swapping the relative sign between
+    # the prior and the likelihood sample. The first technique is
+    # computationally cheap and empirically known to improve stability.
+    # The latter technique requires an additional inversion and its
+    # impact on stability is still unknown.
+    # TODO: investigate the impact of sampling the prior and likelihood
+    # antithetically.
+    met_smpl = nll_smpl + prr_smpl
+    if from_inverse:
+        # TODO: Set sensible convergence criteria
+        signal_smpl = hamiltonian.inv_metric(
+            primals, met_smpl, cg=cg, x0=prr_inv_metric_smpl, **cg_kwargs
+        )
+        return signal_smpl, met_smpl
+    else:
+        return None, met_smpl
+
+
+def sample_standard_hamiltonian(*args, **kwargs):
+    r"""Draws a sample of which the covariance is the metric or the inverse
+    metric of the Hamiltonian.
 
     To sample from the inverse metric, we need to be able to draw samples
     which have the metric as covariance structure and we need to be able to
@@ -67,57 +100,21 @@ def sample_standard_hamiltonian(
         Position at which to draw samples.
     key : tuple, list or np.ndarray of uint32 of length two
         Random key with which to generate random variables in data domain.
-    from_inverse : bool
-        Whether to draw samples from the metric or the inverse metric.
-    cg : callable
+    cg : callable, optional
         Implementation of the conjugate gradient algorithm and used to
         apply the inverse of the metric.
-    cg_kwargs : dict
+    cg_kwargs : dict, optional
         Additional keyword arguments passed on to `cg`.
 
     Returns
     -------
     sample : tree-like structure
-        Sample of which the covariance is the metric (`from_inverse=False`)
-        or the inverse metric (`from_inverse=True`).
+        Sample of which the covariance is the inverse metric.
     """
-    if not isinstance(hamiltonian, StandardHamiltonian):
-        te = f"`hamiltonian` of invalid type; got '{type(hamiltonian)}'"
-        raise TypeError(te)
-
-    subkey_nll, subkey_prr = random.split(key, 2)
-    nll_smpl = sample_likelihood(
-        hamiltonian.likelihood, primals, key=subkey_nll
+    inv_met_smpl, _ = _sample_standard_hamiltonian(
+        *args, from_inverse=True, **kwargs
     )
-    prr_inv_metric_smpl = random_like(primals, key=subkey_prr)
-    # One may transform any metric sample to a sample of the inverse
-    # metric by simply applying the inverse metric to it
-    prr_smpl = prr_inv_metric_smpl
-    if from_inverse:
-        # Note, we can sample antithetically by swapping the global sign of
-        # the metric sample below (which corresponds to mirroring the final
-        # sample) and additionally by swapping the relative sign between
-        # the prior and the likelihood sample. The first technique is
-        # computationally cheap and empirically known to improve stability.
-        # The latter technique requires an additional inversion and its
-        # impact on stability is still unknown.
-        # TODO: investigate the impact of sampling the prior and likelihood
-        # antithetically.
-        met_smpl = nll_smpl + prr_smpl
-        # TODO: Set sensible convergence criteria
-        """
-        lambda x: met(pos, x),
-        absdelta=1. / 100,
-        resnorm=np.linalg.norm(met_smpl, ord=1) / 2,
-        norm_ord=1
-        """
-        signal_smpl = hamiltonian.inv_metric(
-            primals, met_smpl, cg=cg, x0=prr_inv_metric_smpl, **cg_kwargs
-        )
-        return signal_smpl
-    else:
-        return nll_smpl + prr_smpl
-
+    return inv_met_smpl
 
 class MetricKL():
     def __init__(
@@ -139,16 +136,14 @@ class MetricKL():
         self._pos = primals
         self._n_samples = n_samples
 
-        cg_kwargs = {} if cg_kwargs is None else cg_kwargs
         if _samples is None:
             _samples = []
             draw = Partial(
                 sample_standard_hamiltonian,
                 hamiltonian=hamiltonian,
                 primals=primals,
-                from_inverse=True,
                 cg=cg,
-                **cg_kwargs
+                cg_kwargs=cg_kwargs
             )
             subkeys = random.split(key, n_samples)
             _samples = [draw(key=k) for k in subkeys]
