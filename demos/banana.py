2a6fbc6b32408434c34e0238982c8c91a8e1eb96
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2021-08-26 19:26:28 +0200

    banana.py: Compare apples to apples
    Compare the regularized potential fitted with MGVI and geoVI against
    each other and do not drop the regularization for MGVI.
    
    Furthermore, simplify the code where possible.

--- demos/banana.py
+++ demos/banana.py
@@ -34,37 +34,6 @@ def banana_helper_phi_b(b, x):
     return np.array([x[0], x[1] + b * x[0]**2 - 100 * b])
 
 
-def make_kinetic_energy_fn_from_diag_mass_matrix(mass_matrix):
-    from jax import tree_util
-
-    def _kin_energy(momentum):
-        # calculate kinetic energies for every array (leaf) in the pytree
-        kin_energies = tree_util.tree_map(
-            lambda p, m: np.sum(p**2 / (2 * m)), momentum, mass_matrix
-        )
-        # sum everything up
-        total_kin_energy = tree_util.tree_reduce(
-            lambda acc, leaf_kin_e: acc + leaf_kin_e, kin_energies, 0.
-        )
-        return total_kin_energy
-
-    return _kin_energy
-
-
-def make_banana_density(b):
-    # https://link.springer.com/article/10.1007/s001800050022
-    # b = 0.03 (moderately twisted)
-    # b = 0.1 (strongly twisted)
-    # kinetic energy is abuse of notation, but gives me a gaussian
-    f = make_kinetic_energy_fn_from_diag_mass_matrix(np.array([100, 1]))
-
-    def banana_potential(x):
-        return f(banana_helper_phi_b(b, x))
-
-    return banana_potential
-
-
-# %%
 def sample_nonstandard_hamiltonian(
     likelihood, primals, key, cg=jft.cg, cg_kwargs=None
 ):
@@ -133,19 +102,21 @@ signal_response = partial(banana_helper_phi_b, b)
 nll = jft.Gaussian(
     np.zeros(2), lambda x: x / np.sqrt(np.array([100., 1.]))
 ) @ signal_response
-nll = nll.jit()
-nll_vg = jit(value_and_grad(nll))
+
+ham = jft.StandardHamiltonian(nll)
+ham = ham.jit()
+ham_vg = jit(value_and_grad(ham))
 
 # %%
 n_pix_sqrt = 1000
 x = np.linspace(-5, 5, n_pix_sqrt)
-y = np.linspace(5, 15, n_pix_sqrt)
+y = np.linspace(2, 7, n_pix_sqrt)
 xx = cartesian_product((x, y))
-nll_everywhere = np.vectorize(nll, signature="(2)->()")(xx).reshape(
+ham_everywhere = np.vectorize(ham, signature="(2)->()")(xx).reshape(
     n_pix_sqrt, n_pix_sqrt
 )
 plt.imshow(
-    np.exp(-nll_everywhere.T),
+    np.exp(-ham_everywhere.T),
     extent=(x.min(), x.max(), y.min(), y.max()),
     origin="lower"
 )
@@ -159,29 +130,28 @@ n_newton_iterations = [7] * (n_mgvi_iterations - 10) + [10] * 6 + 4 * [25]
 absdelta = 1e-10
 
 initial_position = np.array([1., 1.])
-pos = 1e-2 * jft.Field(initial_position)
+mkl_pos = 1e-2 * jft.Field(initial_position)
 
-# %%
 # Minimize the potential
 for i in range(n_mgvi_iterations):
     print(f"MGVI Iteration {i}", file=sys.stderr)
     print("Sampling...", file=sys.stderr)
     key, subkey = random.split(key, 2)
-    mkl = NonStandardMetricKL(
-        nll,
-        pos,
-        n_samples=n_samples[i],
-        key=subkey,
+    mkl = jft.MetricKL(
+        ham,
+        mkl_pos,
+        n_samples[i],
+        key,
         mirror_samples=True,
-        hamiltonian_and_gradient=nll_vg,
-        cg_kwargs={"absdelta": absdelta / 10.}
+        linear_sampling_kwargs={"absdelta": absdelta / 10.},
+        hamiltonian_and_gradient=ham_vg
     )
 
     print("Minimizing...", file=sys.stderr)
     # TODO: Re-introduce a simplified version that works without fields
     opt_state = jft.minimize(
         None,
-        x0=pos,
+        x0=mkl_pos,
         method="newton-cg",
         options={
             "fun_and_grad": mkl.energy_and_gradient,
@@ -191,16 +161,13 @@ for i in range(n_mgvi_iterations):
             "name": "N",
         }
     )
-    pos = opt_state.x
+    mkl_pos = opt_state.x
     print(
         (
-            f"Post MGVI Iteration {i}: Energy {nll(pos):2.4e}"
-            f"; #NaNs {np.isnan(pos.val).sum()}"
+            f"Post MGVI Iteration {i}: Energy {ham(mkl_pos):2.4e}"
         ),
         file=sys.stderr
     )
-
-mkl_pos = pos
 
 # %%
 n_geovi_iterations = 15
@@ -209,12 +176,7 @@ n_newton_iterations = [7] * (n_geovi_iterations - 10) + [10] * 6 + [25] * 4
 absdelta = 1e-10
 
 initial_position = np.array([1., 1.])
-pos = 1e-2 * jft.Field(initial_position)
-
-# %%
-ham = jft.StandardHamiltonian(nll)
-ham = ham.jit()
-ham_vg = jit(value_and_grad(ham))
+gkl_pos = 1e-2 * jft.Field(initial_position)
 
 for i in range(n_geovi_iterations):
     print(f"GeoVI Iteration {i}", file=sys.stderr)
@@ -222,7 +184,7 @@ for i in range(n_geovi_iterations):
     key, subkey = random.split(key, 2)
     gkl = jft.GeoMetricKL(
         ham,
-        pos,
+        gkl_pos,
         n_samples[i],
         key,
         mirror_samples=True,
@@ -234,7 +196,7 @@ for i in range(n_geovi_iterations):
     print("Minimizing...", file=sys.stderr)
     opt_state = jft.minimize(
         None,
-        x0=pos,
+        x0=gkl_pos,
         method="newton-cg",
         options={
             "fun_and_grad": gkl.energy_and_gradient,
@@ -244,20 +206,22 @@ for i in range(n_geovi_iterations):
             "name": "N",
         }
     )
-    pos = opt_state.x
-
-gkl_pos = pos
+    gkl_pos = opt_state.x
+    print(
+        f"Post geoVI Iteration {i}: Energy {ham(gkl_pos):2.4e}",
+        file=sys.stderr
+    )
 
 # %%
 b_space_smpls = np.array([(mkl_pos + smpl).val for smpl in mkl.samples])
 
-delta = .1
-x = np.arange(-15.0, 15.0, delta)
-y = np.arange(0.0, 15.0, delta)
+n_pix_sqrt = 200
+x = np.linspace(-5.0, 5.0, n_pix_sqrt, endpoint=True)
+y = np.linspace(2.0, 7.0, n_pix_sqrt, endpoint=True)
 X, Y = np.meshgrid(x, y)
 XY = np.array([X, Y]).T
 xy = XY.reshape((XY.shape[0] * XY.shape[1], 2))
-es = np.exp(-lax.map(nll, xy)).reshape(XY.shape[:2]).T
+es = np.exp(-lax.map(ham, xy)).reshape(XY.shape[:2]).T
 
 fig, ax = plt.subplots()
 contour = ax.contour(X, Y, es)
@@ -268,14 +232,6 @@ plt.show()
 
 # %%
 b_space_smpls = np.array([(gkl_pos + smpl).val for smpl in gkl.samples])
-
-delta = .1
-x = np.arange(-15.0, 15.0, delta)
-y = np.arange(0.0, 15.0, delta)
-X, Y = np.meshgrid(x, y)
-XY = np.array([X, Y]).T
-xy = XY.reshape((XY.shape[0] * XY.shape[1], 2))
-es = np.exp(-lax.map(ham, xy)).reshape(XY.shape[:2]).T
 
 fig, ax = plt.subplots()
 contour = ax.contour(X, Y, es)
