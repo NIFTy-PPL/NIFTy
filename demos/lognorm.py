26287117bc7dda043fd18428b0f8fb87a77a0fa6
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2021-08-26 19:25:46 +0200

    lognorm.py: Simplify and format

--- demos/lognorm.py
+++ demos/lognorm.py
@@ -31,35 +31,14 @@ def cartesian_product(arrays, out=None):
 
 
 def helper_phi_b(b, x):
-    return np.array([b*x[0]*np.exp(b*x[1])])
-
-
-def make_kinetic_energy_fn_from_diag_mass_matrix(mass_matrix):
-    from jax import tree_util
-
-    def _kin_energy(momentum):
-        # calculate kinetic energies for every array (leaf) in the pytree
-        kin_energies = tree_util.tree_map(
-            lambda p, m: np.sum(p**2 / (2 * m)), momentum, mass_matrix
-        )
-        # sum everything up
-        total_kin_energy = tree_util.tree_reduce(
-            lambda acc, leaf_kin_e: acc + leaf_kin_e, kin_energies, 0.
-        )
-        return total_kin_energy
-
-    return _kin_energy
+    return b * x[0] * np.exp(b * x[1])
 
 
 # %%
 b = 2.
 
 signal_response = partial(helper_phi_b, b)
-nll = jft.Gaussian(
-    np.zeros(1), lambda x: x / np.sqrt(np.array([1.,]))
-) @ signal_response
-nll = nll.jit()
-nll_vg = jit(value_and_grad(nll))
+nll = jft.Gaussian(0., lambda x: x / np.sqrt(1.)) @ signal_response
 
 ham = jft.StandardHamiltonian(nll)
 ham = ham.jit()
@@ -70,11 +49,11 @@ n_pix_sqrt = 1000
 x = np.linspace(-4, 4, n_pix_sqrt)
 y = np.linspace(-4, 4, n_pix_sqrt)
 xx = cartesian_product((x, y))
-nll_everywhere = np.vectorize(ham, signature="(2)->()")(xx).reshape(
+ham_everywhere = np.vectorize(ham, signature="(2)->()")(xx).reshape(
     n_pix_sqrt, n_pix_sqrt
 )
 plt.imshow(
-    np.exp(-nll_everywhere.T),
+    np.exp(-ham_everywhere.T),
     extent=(x.min(), x.max(), y.min(), y.max()),
     origin="lower"
 )
@@ -88,13 +67,8 @@ n_newton_iterations = [7] * (n_mgvi_iterations - 10) + [10] * 6 + 4 * [25]
 absdelta = 1e-10
 
 initial_position = np.array([1., 1.])
-pos = 1e-2 * jft.Field(initial_position)
+mkl_pos = 1e-2 * jft.Field(initial_position)
 
-ham = jft.StandardHamiltonian(nll)
-ham = ham.jit()
-ham_vg = jit(value_and_grad(ham))
-
-# %%
 # Minimize the potential
 for i in range(n_mgvi_iterations):
     print(f"MGVI Iteration {i}", file=sys.stderr)
@@ -102,7 +76,7 @@ for i in range(n_mgvi_iterations):
     key, subkey = random.split(key, 2)
     mkl = jft.MetricKL(
         ham,
-        pos,
+        mkl_pos,
         n_samples[i],
         key,
         mirror_samples=True,
@@ -114,7 +88,7 @@ for i in range(n_mgvi_iterations):
     # TODO: Re-introduce a simplified version that works without fields
     opt_state = jft.minimize(
         None,
-        x0=pos,
+        x0=mkl_pos,
         method="newton-cg",
         options={
             "fun_and_grad": mkl.energy_and_gradient,
@@ -124,16 +98,9 @@ for i in range(n_mgvi_iterations):
             "name": "N",
         }
     )
-    pos = opt_state.x
-    print(
-        (
-            f"Post MGVI Iteration {i}: Energy {nll(pos):2.4e}"
-            f"; #NaNs {np.isnan(pos.val).sum()}"
-        ),
-        file=sys.stderr
-    )
-
-mkl_pos = pos
+    mkl_pos = opt_state.x
+    msg = f"Post MGVI Iteration {i}: Energy {ham(mkl_pos):2.4e}"
+    print(msg, file=sys.stderr)
 
 # %%
 n_geovi_iterations = 15
@@ -142,19 +109,15 @@ n_newton_iterations = [7] * (n_geovi_iterations - 10) + [10] * 6 + [25] * 4
 absdelta = 1e-10
 
 initial_position = np.array([1., 1.])
-pos = 1e-2 * jft.Field(initial_position)
-
-ham = jft.StandardHamiltonian(nll)
-ham = ham.jit()
-ham_vg = jit(value_and_grad(ham))
+gkl_pos = 1e-2 * jft.Field(initial_position)
 
 for i in range(n_geovi_iterations):
-    print(f"GeoVI Iteration {i}", file=sys.stderr)
+    print(f"geoVI Iteration {i}", file=sys.stderr)
     print("Sampling...", file=sys.stderr)
     key, subkey = random.split(key, 2)
     gkl = jft.GeoMetricKL(
         ham,
-        pos,
+        gkl_pos,
         n_samples[i],
         key,
         mirror_samples=True,
@@ -166,7 +129,7 @@ for i in range(n_geovi_iterations):
     print("Minimizing...", file=sys.stderr)
     opt_state = jft.minimize(
         None,
-        x0=pos,
+        x0=gkl_pos,
         method="newton-cg",
         options={
             "fun_and_grad": gkl.energy_and_gradient,
@@ -176,16 +139,16 @@ for i in range(n_geovi_iterations):
             "name": "N",
         }
     )
-    pos = opt_state.x
-
-gkl_pos = pos
+    gkl_pos = opt_state.x
+    msg = f"Post geoVI Iteration {i}: Energy {ham(gkl_pos):2.4e}"
+    print(msg, file=sys.stderr)
 
 # %%
 b_space_smpls = np.array([(mkl_pos + smpl).val for smpl in mkl.samples])
 
-delta = .1
-x = np.arange(-4.0, 4.0, delta)
-y = np.arange(-4.0, 4.0, delta)
+n_pix_sqrt = 200
+x = np.linspace(-4.0, 4.0, n_pix_sqrt, endpoint=True)
+y = np.linspace(-4.0, 4.0, n_pix_sqrt, endpoint=True)
 X, Y = np.meshgrid(x, y)
 XY = np.array([X, Y]).T
 xy = XY.reshape((XY.shape[0] * XY.shape[1], 2))
