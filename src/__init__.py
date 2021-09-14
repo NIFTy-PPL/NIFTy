6c5a795ea86040dc544161ae22241a6d63432bbb
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2021-09-14 22:55:21 +0200

    Introduce an invgamma prior
    For the purpose of interpolating the inverse transform sampling step,
    introduce an `interpolator` helper function.

--- src/__init__.py
+++ src/__init__.py
@@ -12,7 +12,9 @@ from .field import Field
 from .forest_util import norm, ShapeWithDtype, vmap_forest, vmap_forest_mean
 from .optimize import minimize, newton_cg, trust_ncg
 from .correlated_field import CorrelatedFieldMaker, non_parametric_amplitude
-from .stats_distributions import laplace_prior, normal_prior, lognormal_prior
+from .stats_distributions import (
+    laplace_prior, normal_prior, lognormal_prior, invgamma_prior
+)
 from .sugar import (
     ducktape, mean, mean_and_std, random_like, random_like_shapewdtype,
     sum_of_squares, interpolate
--- src/stats_distributions.py
+++ src/stats_distributions.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Optional
 
 from jax import numpy as np
 
@@ -28,14 +28,14 @@ def normal_prior(mean, std) -> Callable:
 
 
 def lognormal_moments(mean, std):
-    """Compute the cumulants a log-normal process would have to comply with the
+    """Compute the cumulants a log-normal process would need to comply with the
     provided mean and standard-deviation `std`
     """
-
     if np.any(mean <= 0.):
         raise ValueError(f"`mean` must be greater zero; got {mean!r}")
     if np.any(std <= 0.):
         raise ValueError(f"`std` must be greater zero; got {std!r}")
+
     logstd = np.sqrt(np.log1p((std / mean)**2))
     logmean = np.log(mean) - 0.5 * logstd**2
     return logmean, logstd
@@ -58,3 +58,112 @@ def lognormal_prior(mean, std) -> Callable:
         return np.exp(standard_to_normal(xi))
 
     return standard_to_lognormal
+
+
+def interpolator(
+    func: Callable,
+    xmin: float,
+    xmax: float,
+    *,
+    step: Optional[float] = None,
+    num: Optional[int] = None,
+    table_func: Optional[Callable] = None,
+    inv_table_func: Optional[Callable] = None
+):  # Adapted from NIFTy
+    """
+    Evaluate a function point-wise by interpolation.  Can be supplied with a
+    table_func to increase the interpolation accuracy, Best results are
+    achieved when `lambda x: table_func(func(x))` is roughly linear.
+
+    Parameters
+    ----------
+    func : function
+        Function to interpolate.
+    xmin : float
+        The smallest value for which `func` will be evaluated.
+    xmax : float
+        The largest value for which `func` will be evaluated.
+    step : float
+        Distance between sampling points for linear interpolation. Either of
+        `step` or `num` must be specified.
+    num : int
+        The number of interpolation points. Either of `step` of `num` must be
+        specified.
+    table_func : function
+        Non-linear function applied to the tabulated function in order to
+        transform the table to a more linear space.
+    inv_table_func : function
+        Inverse of `table_func`.
+    """
+    # from scipy.interpolate import CubicSpline
+
+    if step is not None and num is not None:
+        ve = "either but not both of `step` and `num` must be specified"
+        raise ValueError(ve)
+    if step is not None:
+        xs = np.arange(xmin, xmax + step, step)
+    elif num is not None:
+        xs = np.linspace(xmin, xmax, num)
+    else:
+        ve = "either of `step` or `num` must be specified"
+        raise ValueError(ve)
+
+    ys = func(xs)
+    if table_func is not None:
+        if inv_table_func is None:
+            raise ValueError("no `inv_table_func` specified")
+        ys = table_func(ys)
+
+    # interpolator = CubicSpline(xs, ys)
+    # deriv = interpolator.derivative()
+
+    def interp(x):
+        # res = interpolator(x)
+        res = np.interp(x, xs, ys)
+        if inv_table_func is not None:
+            res = inv_table_func(res)
+        return res
+
+    return interp
+
+
+def invgamma_prior(a, scale, loc=0., step=1e-2) -> Callable:
+    """Transform a standard normal into an inverse gamma distribution.
+
+    The pdf of the inverse gamma distribution is defined as follows using
+    :math:`q` to denote the scale:
+
+    .. math::
+        \\frac{q^\\a}{\\Gamma(\\a)}x^{-\\a -1}
+        \\exp \\left(-\\frac{q}{x}\\right)
+
+    That means that for large x the pdf falls off like :math:`x^(-\\a -1)`.
+    The mean of the pdf is at :math:`q / (\\a - 1)` if :math:`\\a > 1`.
+    The mode is :math:`q / (\\a + 1)`.
+
+    This transformation is implemented as a linear interpolation which maps a
+    Gaussian onto an inverse gamma distribution.
+
+    Parameters
+    ----------
+    a : float
+        The shape-parameter of the inverse-gamma distribution.
+    scale : float
+        The scale-parameter of the inverse-gamma distribution.
+    loc : float
+        An option shift of the whole distribution.
+    step : float
+        Distance between sampling points for linear interpolation.
+    """
+    from scipy.stats import invgamma, norm
+
+    xmin, xmax = -8.2, 8.2  # (1. - norm.cdf(8.2)) * 2 < 1e-15
+    standard_to_invgamma = interpolator(
+        lambda x: invgamma.ppf(norm._cdf(x), a=a, loc=loc, scale=scale),
+        xmin,
+        xmax,
+        step=step,
+        table_func=np.log,
+        inv_table_func=np.exp
+    )
+    return standard_to_invgamma
