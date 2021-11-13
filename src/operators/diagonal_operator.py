NIFTy-PPL/NIFTy

commit 2db6c449d6d3d9b96c33473a05c00009c993a0c6

More informative errors for domain mismatches

src/operators/diagonal_operator.py
@@ -61,15 +61,13 @@ class DiagonalOperator(EndomorphicOperator):
             self._domain = DomainTuple.make(domain)
         if spaces is None:
             self._spaces = None
-            if diagonal.domain != self._domain:
-                raise ValueError("domain mismatch")
+            utilities.check_domain_equality(diagonal.domain, self._domain)
         else:
             self._spaces = utilities.parse_spaces(spaces, len(self._domain))
             if len(self._spaces) != len(diagonal.domain):
                 raise ValueError("spaces and domain must have the same length")
             for i, j in enumerate(self._spaces):
-                if diagonal.domain[i] != self._domain[j]:
-                    raise ValueError("domain mismatch")
+                utilities.check_domain_equality(diagonal.domain[i], self._domain[j])
             if self._spaces == tuple(range(len(self._domain))):
                 self._spaces = None  # shortcut
 

src/utilities.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright(C) 2013-2019 Max-Planck-Society
+# Copyright(C) 2013-2021 Max-Planck-Society
 #
 # NIFTy is being developed at the Max-Planck-Institut fuer Astrophysik.
 
@@ -25,7 +25,7 @@ __all__ = ["get_slice_list", "safe_cast", "parse_spaces", "infer_space",
            "memo", "NiftyMeta", "my_sum", "my_lincomb_simple",
            "my_lincomb", "indent",
            "my_product", "frozendict", "special_add_at", "iscomplextype",
-           "value_reshaper", "lognormal_moments"]
+           "value_reshaper", "lognormal_moments", "check_domain_equality"]
 
 
 def my_sum(iterable):
@@ -412,3 +412,19 @@ def myassert(val):
     `__debug__` is False."""
     if not val:
         raise AssertionError
+
+
+def check_domain_equality(domain0, domain1):
+    """Check if two domains are equal and throw ValueError if not. Throw a
+    TypeError if one of the inputs is neither a DomainTuple nor a
+    MultiDomain.
+    """
+    from .domain_tuple import DomainTuple
+    from .multi_domain import MultiDomain
+    from .domains.domain import Domain
+    for dom in [domain0, domain1]:
+        if not isinstance(dom, (MultiDomain, DomainTuple, Domain)):
+            raise TypeError("The following domain is neither an instance of "
+                            f"ift.MultiDomain nor of ift.DomainTuple.\n{dom}")
+    if domain0 != domain1:
+        raise ValueError(f"Domain mismatch:\n{domain0}\n{domain1}")

src/field.py
@@ -56,7 +56,7 @@ class Field(Operator):
             else:
                 raise TypeError("val must be of type numpy.ndarray")
         if domain.shape != val.shape:
-            raise ValueError("shape mismatch between val and domain")
+            raise ValueError(f"shape mismatch between val and domain\n{domain.shape}\n{val.shape}")
         self._domain = domain
         self._val = val
         self._val.flags.writeable = False
@@ -310,8 +310,7 @@ class Field(Operator):
             raise TypeError("The dot-partner must be an instance of " +
                             "the Field class")
 
-        if x._domain != self._domain:
-            raise ValueError("Domain mismatch")
+        utilities.check_domain_equality(x._domain, self._domain)
 
         ndom = len(self._domain)
         spaces = utilities.parse_spaces(spaces, ndom)
@@ -339,8 +338,7 @@ class Field(Operator):
             raise TypeError("The dot-partner must be an instance of " +
                             "the Field class")
 
-        if x._domain != self._domain:
-            raise ValueError("Domain mismatch")
+        utilities.check_domain_equality(x._domain, self._domain)
 
         return vdot(self._val, x._val)
 
@@ -671,13 +669,11 @@ class Field(Operator):
                "\n- val         = " + repr(self._val)
 
     def extract(self, dom):
-        if dom != self._domain:
-            raise ValueError("domain mismatch")
+        utilities.check_domain_equality(dom, self._domain)
         return self
 
     def extract_part(self, dom):
-        if dom != self._domain:
-            raise ValueError("domain mismatch")
+        utilities.check_domain_equality(dom, self._domain)
         return self
 
     def unite(self, other):
@@ -690,8 +686,7 @@ class Field(Operator):
         # if other is a field, make sure that the domains match
         f = getattr(self._val, op)
         if isinstance(other, Field):
-            if other._domain != self._domain:
-                raise ValueError("domains are incompatible.")
+            utilities.check_domain_equality(other._domain, self._domain)
             return Field(self._domain, f(other._val))
         if np.isscalar(other):
             return Field(self._domain, f(other))

src/linearization.py
@@ -19,6 +19,7 @@ import numpy as np
 
 from .operators.operator import Operator
 from .sugar import makeOp
+from .utilities import check_domain_equality
 
 
 class Linearization(Operator):
@@ -41,8 +42,7 @@ class Linearization(Operator):
     def __init__(self, val, jac, metric=None, want_metric=False):
         self._val = val
         self._jac = jac
-        if self._val.domain != self._jac.target:
-            raise ValueError("domain mismatch")
+        check_domain_equality(self._val.domain, self._jac.target)
         self._want_metric = want_metric
         self._metric = metric
 
@@ -179,13 +179,10 @@ class Linearization(Operator):
                 return self
             met = None if self._metric is None else self._metric.scale(other)
             return self.new(self._val*other, self._jac.scale(other), met)
-        from .sugar import makeOp
         if other.jac is None:
-            if self.target != other.domain:
-                raise ValueError("domain mismatch")
+            check_domain_equality(self.target, other.domain)
             return self.new(self._val*other, makeOp(other)(self._jac))
-        if self.target != other.target:
-            raise ValueError("domain mismatch")
+        check_domain_equality(self.target, other.target)
         return self.new(
             self.val*other.val,
             (makeOp(other.val)(self.jac))._myadd(

src/multi_domain.py
@@ -16,7 +16,7 @@
 # NIFTy is being developed at the Max-Planck-Institut fuer Astrophysik.
 
 from .domain_tuple import DomainTuple
-from .utilities import frozendict, indent
+from .utilities import check_domain_equality, frozendict, indent
 
 
 class MultiDomain:
@@ -128,8 +128,7 @@ class MultiDomain:
         for dom in inp:
             for key, subdom in zip(dom._keys, dom._domains):
                 if key in res:
-                    if res[key] != subdom:
-                        raise ValueError("domain mismatch")
+                    check_domain_equality(res[key], subdom)
                 else:
                     res[key] = subdom
         return MultiDomain.make(res)

src/multi_field.py
@@ -41,8 +41,7 @@ class MultiField(Operator):
             raise ValueError("length mismatch")
         for d, v in zip(domain._domains, val):
             if isinstance(v, Field):
-                if v._domain != d:
-                    raise ValueError("domain mismatch")
+                utilities.check_domain_equality(v._domain, d)
             else:
                 raise TypeError("bad entry in val (must be Field)")
         self._domain = domain
@@ -137,13 +136,9 @@ class MultiField(Operator):
                for kk in domain.keys()}
         return MultiField.from_dict(dct)
 
-    def _check_domain(self, other):
-        if other._domain != self._domain:
-            raise ValueError("domains are incompatible.")
-
     def s_vdot(self, x):
         result = 0.
-        self._check_domain(x)
+        utilities.check_domain_equality(x._domain, self._domain)
         for v1, v2 in zip(self._val, x._val):
             result += v1.s_vdot(v2)
         return result
@@ -369,8 +364,7 @@ class MultiField(Operator):
     def _binary_op(self, other, op):
         f = getattr(Field, op)
         if isinstance(other, MultiField):
-            if self._domain != other._domain:
-                raise ValueError("domain mismatch")
+            utilities.check_domain_equality(self._domain, other._domain)
             val = tuple(f(v1, v2)
                         for v1, v2 in zip(self._val, other._val))
         else:

src/operators/block_diagonal_operator.py
@@ -17,7 +17,7 @@
 
 from ..multi_domain import MultiDomain
 from ..multi_field import MultiField
-from ..utilities import indent
+from ..utilities import check_domain_equality, indent
 from .endomorphic_operator import EndomorphicOperator
 from .linear_operator import LinearOperator
 
@@ -75,16 +75,14 @@ class BlockDiagonalOperator(EndomorphicOperator):
         return MultiField(self._domain, val)
 
     def _combine_chain(self, op):
-        if self._domain != op._domain:
-            raise ValueError("domain mismatch")
+        check_domain_equality(self._domain, op._domain)
         res = {key: v1(v2)
                for key, v1, v2 in zip(self._domain.keys(), self._ops, op._ops)}
         return BlockDiagonalOperator(self._domain, res)
 
     def _combine_sum(self, op, selfneg, opneg):
         from ..operators.sum_operator import SumOperator
-        if self._domain != op._domain:
-            raise ValueError("domain mismatch")
+        check_domain_equality(self._domain, op._domain)
         res = {key: SumOperator.make([v1, v2], [selfneg, opneg])
                for key, v1, v2 in zip(self._domain.keys(), self._ops, op._ops)}
         return BlockDiagonalOperator(self._domain, res)

src/operators/chain_operator.py
@@ -44,8 +44,7 @@ class ChainOperator(LinearOperator):
     def simplify(ops):
         # verify domains
         for i in range(len(ops) - 1):
-            if ops[i + 1].target != ops[i].domain:
-                raise ValueError("domain mismatch")
+            utilities.check_domain_equality(ops[i + 1].target, ops[i].domain)
         # unpack ChainOperators
         opsnew = []
         for op in ops:

commit d20bd30b11428ef443754ede2a40ef81c4662028

Fix performance regression
introduced by 2db6c449d6d3d9b96c33473a05c00009c993a0c6

src/operators/diagonal_operator.py
@@ -74,7 +74,7 @@ class DiagonalOperator(EndomorphicOperator):
             if len(self._spaces) != len(diagonal.domain):
                 raise ValueError("spaces and domain must have the same length")
             for i, j in enumerate(self._spaces):
-                utilities.check_domain_equality(diagonal.domain[i], self._domain[j])
+                utilities.check_domain_equality(diagonal.domain[i], self._domain[j], eq=True)
             if self._spaces == tuple(range(len(self._domain))):
                 self._spaces = None  # shortcut
 

src/utilities.py
@@ -413,19 +413,15 @@ def myassert(val):
         raise AssertionError
 
 
-def check_domain_equality(domain0, domain1):
+def check_domain_equality(domain0, domain1, eq=False):
     """Check if two domains are equal and throw ValueError if not. Throw a
     TypeError if one of the inputs is neither a DomainTuple nor a
     MultiDomain.
+
+    The equality check is performed via object comparison (`is`) and is suited
+    for `DomainTuple` and `MultiField`s. To check via `==`, set `eq` to True.
     """
-    from .domain_tuple import DomainTuple
-    from .domains.domain import Domain
-    from .multi_domain import MultiDomain
-    for dom in [domain0, domain1]:
-        if not isinstance(dom, (MultiDomain, DomainTuple, Domain)):
-            raise TypeError("The following domain is neither an instance of "
-                            f"ift.MultiDomain nor of ift.DomainTuple.\n{dom}")
-    if domain0 != domain1:
+    if domain0 != domain1 if eq else domain0 is not domain1:
         raise ValueError(f"Domain mismatch:\n{domain0}\n{domain1}")
 
 