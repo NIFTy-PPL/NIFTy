d43ccad63c33932915cab6b9200ed7fcc6062ea7
Philipp Arras <c@philipp-arras.de>
2022-02-08 18:14:12 +0100

    fixup! Implement new likelihood operators

--- src/operators/jax_operator.py
+++ src/operators/jax_operator.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright(C) 2021 Max-Planck-Society
+# Copyright(C) 2021-2022 Max-Planck-Society
 # Author: Philipp Arras
 
 from types import SimpleNamespace
