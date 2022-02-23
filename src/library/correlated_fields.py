NIFTy-PPL/NIFTy

commit 62572ceca1db9e2fc81bcc8fadf95c0bdb67c0e6

CorrelatedFieldMaker: Add JAX expressions

src/library/correlated_fields.py
@@ -20,12 +20,14 @@
 
 from functools import reduce
 from operator import mul
+from warnings import warn
 
 import numpy as np
 
 from .. import utilities
 from ..domain_tuple import DomainTuple
 from ..domains.power_space import PowerSpace
+from ..domains.rg_space import RGSpace
 from ..domains.unstructured_domain import UnstructuredDomain
 from ..field import Field
 from ..logger import logger
@@ -435,6 +437,16 @@ class CorrelatedFieldMaker:
         self._prefix = prefix
         self._total_N = total_N
 
+        try:
+            import jifty1 as jft
+
+            if total_N != 0:
+                warn(f"unable to add JAX operator for {total_N=}")
+                raise ImportError("short-circuit JAX init")
+            self._jax_cfm = jft.CorrelatedFieldMaker(prefix=prefix)
+        except ImportError:
+            self._jax_cfm = None
+
     def add_fluctuations(self,
                          target_subdomain,
                          fluctuations,
@@ -546,6 +558,25 @@ class CorrelatedFieldMaker:
                          target_subdomain[-1].total_volume,
                          pre + 'spectrum', dofdex)
 
+        is_rg = all(isinstance(dom, RGSpace) for dom in target_subdomain)
+        if self._jax_cfm is not None and (len(dofdex) > 0 or index or not is_rg):
+            warn(f"unable to add JAX operator for {target_subdomain}")
+            self._jax_cfm = None
+        if self._jax_cfm is not None:
+            dists = tuple(e for di in target_subdomain for e in di.distances)
+            self._jax_cfm.add_fluctuations(
+                shape=target_subdomain.shape,
+                distances=dists,
+                fluctuations=fluctuations,
+                loglogavgslope=loglogavgslope,
+                flexibility=flexibility,
+                asperity=asperity,
+                prefix=str(prefix),
+                harmonic_domain_type="fourier",
+                non_parametric_kind="power",
+            )
+            amp._jax_expr = self._jax_cfm.fluctuations[-1]
+
         if index is not None:
             self._a.insert(index, amp)
             self._target_subdomains.insert(index, target_subdomain)
@@ -632,6 +663,10 @@ class CorrelatedFieldMaker:
         amp = _AmplitudeMatern(pow_spc, scale, cutoff, loglogslope,
                                totvol)
 
+        if self._jax_cfm is not None:
+            warn(f"unable to add JAX operator for Matern fluctuations")
+            self._jax_cfm = None
+
         self._a.append(amp)
         self._target_subdomains.append(target_subdomain)
 
@@ -664,10 +699,13 @@ class CorrelatedFieldMaker:
             logger.warning("Overwriting the previous mean offset and zero-mode")
 
         self._offset_mean = offset_mean
+        jax_offset_std = offset_std
         if offset_std is None or (np.isscalar(offset_std) and offset_std == 1.):
             self._azm = 1.
+            jax_offset_std = lambda _: 1.
         elif isinstance(offset_std, Operator):
             self._azm = offset_std
+            jax_offset_std = offset_std.jax_expr
         else:
             if dofdex is None:
                 dofdex = np.full(self._total_N, 0)
@@ -684,6 +722,16 @@ class CorrelatedFieldMaker:
             if self._total_N > 0:
                 zm = _Distributor(dofdex, zm.target, UnstructuredDomain(self._total_N)) @ zm
             self._azm = zm
+
+        if self._jax_cfm is not None and dofdex is not None and len(dofdex) > 0:
+            warn(f"unable to add JAX operator for {dofdex=}")
+            self._jax_cfm = None
+        if self._jax_cfm is not None:
+            self._jax_cfm.set_amplitude_total_offset(
+                offset_mean=offset_mean, offset_std=jax_offset_std
+            )
+            if not isinstance(self._azm, float):
+                self._azm._jax_expr = self._jax_cfm.azm
 
     def finalize(self, prior_info=100):
         """Finishes model construction process and returns the constructed
@@ -744,6 +792,10 @@ class CorrelatedFieldMaker:
                 offset = float(offset)
                 op = Adder(full(op.target, offset)) @ op
         self.statistics_summary(prior_info)
+
+        if self._jax_cfm is not None:
+            cf, _ = self._jax_cfm.finalize()
+            op._jax_expr = cf
         return op
 
     def statistics_summary(self, prior_info):
@@ -798,8 +850,13 @@ class CorrelatedFieldMaker:
         if np.isscalar(self.azm):
             return self.fluctuations
 
+        if self._jax_cfm:
+            normed_amps_jax = self._jax_cfm.get_normalized_amplitudes()
+        else:
+            normed_amps_jax = (None, ) * len(self._a)
+
         normal_amp = []
-        for amp in self._a:
+        for amp, na_jax in zip(self._a, normed_amps_jax):
             a_target = amp.target
             a_space = 0 if not hasattr(amp, "_space") else amp._space
             a_pp = amp.target[a_space]
@@ -821,7 +878,9 @@ class CorrelatedFieldMaker:
             zm_normalization = zm_unmask @ (
                 zm_mask @ azm_expander(self.azm.ptw("reciprocal"))
             )
-            normal_amp.append(zm_normalization * amp)
+            na = zm_normalization * amp
+            na._jax_expr = na_jax
+            normal_amp.append(na)
         return tuple(normal_amp)
 
     @property
@@ -834,12 +893,16 @@ class CorrelatedFieldMaker:
         normal_amp = self.get_normalized_amplitudes()[0]
 
         if np.isscalar(self.azm):
-            return normal_amp
+            na = normal_amp
         else:
             expand = ContractionOperator(
                 normal_amp.target, len(normal_amp.target) - 1
             ).adjoint
-            return normal_amp * (expand @ self.azm)
+            na = normal_amp * (expand @ self.azm)
+
+        if self._jax_cfm:
+            na._jax_expr = self._jax_cfm.amplitude
+        return na
 
     @property
     def power_spectrum(self):

commit 8a3715ec52012a3705900843a696d5968cc980ea

Restore py3.7 compatibility by adapting f-strings
This issue was introduced in 62572cec .

src/library/correlated_fields.py
@@ -441,7 +441,7 @@ class CorrelatedFieldMaker:
             from .. import re as jft
 
             if total_N != 0:
-                warn(f"unable to add JAX operator for {total_N=}")
+                warn(f"unable to add JAX operator for total_N={total_N}")
                 raise ImportError("short-circuit JAX init")
             self._jax_cfm = jft.CorrelatedFieldMaker(prefix=prefix)
         except ImportError:
@@ -726,7 +726,7 @@ class CorrelatedFieldMaker:
             self._azm = zm
 
         if self._jax_cfm is not None and dofdex is not None and len(dofdex) > 0:
-            warn(f"unable to add JAX operator for {dofdex=}")
+            warn(f"unable to add JAX operator for dofdex={dofdex}")
             self._jax_cfm = None
         if self._jax_cfm is not None:
             try: