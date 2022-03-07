ad95c67197117b183e22acc2d8c5453824510697
matteani <matteani@mpa-garching.mpg.de>
2022-03-07 15:13:29 +0100

    evidence_calculation: Refactored model_comparison demo into stand-alone demo. The prior term is now sampled (compatible with geovi).

--- src/evidence_lower_bound.py
+++ src/evidence_lower_bound.py
@@ -12,11 +12,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Copyright(C) 2013-2022 Max-Planck-Society
-
 import numpy as np
+import scipy.linalg as slg
 import scipy.sparse.linalg as ssl
 
-from .field import Field
+from .linearization import Linearization
 from .logger import logger
 from .minimization.kl_energies import SampledKLEnergyClass
 from .minimization.sample_list import SampleListBase
@@ -56,16 +56,17 @@ class _Projector(ssl.LinearOperator):
         return self._matvec(x)
 
 
-def _return_eigenspace(M, n_eigenvalues, tol):
-    eigenvalues, eigenvectors = ssl.eigsh(M, k=n_eigenvalues, tol=tol, return_eigenvectors=True, which='LM')
+def _return_eigenspace(M, n_eigenvalues, tol, exact):
+    eigenvalues, eigenvectors = ssl.eigsh(M, k=n_eigenvalues, tol=tol, return_eigenvectors=True,
+                                          which='LM') if not exact else slg.eigh(M)
     i = np.argsort(eigenvalues)
     eigenvalues, eigenvectors = np.flip(eigenvalues[i]), np.flip(eigenvectors[:, i], axis=1)
     return eigenvalues, eigenvectors
 
 
-def estimate_evidence_lower_bound(mean, samples, n_eigenvalues, hamiltonian, constants=[], invariants=None,
-                                  fudge_factor=0,
-                                  max_iteration=5, eps=1e-3, tol=0., verbose=True, data=None):
+def estimate_evidence_lower_bound(samples, n_eigenvalues, hamiltonian, constants=[], invariants=None,
+                                  fudge_factor=0, max_iteration=5, eps=1e-3, tol=0., verbose=True, data=None,
+                                  exact=False):
     """Provides an estimate for the Evidence Lower Bound (ELBO).
 
     Statistical inference deals with the problem of hypothesis testing, given the data and models that can describe it.
@@ -79,18 +80,17 @@ def estimate_evidence_lower_bound(mean, samples, n_eigenvalues, hamiltonian, con
     The evidence for an approximated-inference problem could be in principle calculated by considering
 
     .. math ::
-        \\log(p(d)) - D_\\text{KL} \\left[ Q(\\theta(\\xi)|d) || p(\\theta(\\xi) | d) \\right] = -\\langleH(
-        d|\\xi)\\rangle - \\frac{1}{2} \\left( \\Tr[\\Lambda - \\mathbb{1}] + \\xi^\\dagger \\xi - \\Tr\\log\\Lambda
-        \\right)
+        \\log(p(d)) - D_\\text{KL} \\left[ Q(\\theta(\\xi)|d) || p(\\theta(\\xi) | d) \\right] = -\\langle H(\\theta(
+        \\xi), d)\\rangle + \\frac1 2 \\left( N + \\text{Tr } \\log\\Lambda\\right)
 
     where :math:`D_\\text{KL} \\left[ Q || p \\right]` is the Kullback-Leibler (KL) divergence between the
     approximating posterior distribution :math:`Q` and the actual posterior :math:`p`.
-    But since the Kullback-Leibler divergence :math:`D_\\text{KL}[\\dot] \\geq 0` is positive definite, it is convenient
-    to consider the lower bound
+    But since the Kullback-Leibler divergence :math:`D_\\text{KL} [\\cdot, \\cdot] \\geq 0` is positive definite,
+    it is convenient to consider the lower bound
 
     .. math ::
-        \\log(p(d)) \\geq -\\langleH(d|\\xi)\\rangle - \\frac{1}{2} \\left( \\Tr[\\Lambda - \\mathbb{1}] +
-        \\xi^\\dagger \\xi - \\Tr\\log\\Lambda \\right),
+        \\log(p(d)) \\geq -\\langle H(\\theta(\\xi), d)\\rangle + \\frac1 2 \\left(N + \\text{Tr } \\log\\Lambda
+        \\right),
 
     which takes the name of Evidence Lower Bound (ELBO).
 
@@ -102,49 +102,52 @@ def estimate_evidence_lower_bound(mean, samples, n_eigenvalues, hamiltonian, con
     Parameters
     ----------
 
-    mean : Field
-        Position at which the metric is to be evaluated.
-
     samples : SampleListBase
         Collection of samples from the posterior distribution.
-
-    hamiltonian : StandardHamiltonian
-        Hamiltonian of the approximated probability distribution.
-
-    constants : list
-        Collection of parameter keys that are kept constant during optimization.
-        Default is no constants.
-
-    invariants : XXX
-        TODO: Explain
-        Default is None.
 
     n_eigenvalues : int
         The starting number of eigenvalues and eigenvectors to be calculated.
         `n_eigenvalues` must be smaller than N-1, where N is the metric dimension.
 
-    fudge_factor : signed int
+    hamiltonian : StandardHamiltonian
+        Hamiltonian of the approximated probability distribution.
+
+    constants : Optional[list]
+        Collection of parameter keys that are kept constant during optimization.
+        Default is empty list.
+
+    invariants : Optional[XXX]
+        TODO: Explain
+        Default is None.
+
+    fudge_factor : Optional[signed int]
         Constant number of eigenvalues that will always be subtracted (or
         computed) during each iteration of the algorithm.
 
-    max_iteration : int
+    max_iteration : Optional[int]
         Maximum iteration -> stopping criterion for the algorithm.
+        Default is 5 iterations.
 
-    eps : float
+    eps : Optional[float]
         Relative accuracy (with respect to `1.`) for the eigenvalues (stopping
-        criterion).
+        criterion). Default is 1e-3.
 
-    tol : float
+    tol : Optional[float]
         Tolerance on the eigenvalue calculation. Zero indicates machine precision.
+        Default is 0.
 
-    verbose : bool
+    verbose : Optional[bool]
         Annoy me with what you are doing. Or don't.
         Default is True.
 
-    data :
+    data : Optional[Field]
         Required to evaluate evidence contributions deriving from
         parameter-independent likelihood terms that might have been ignored
-        during minimization.
+        during minimization. Default is None.
+
+    exact : Optional[bool]
+        If true calculates all the metric eigenvalues and returns non-approximated ELBO.
+        Default is False.
 
 
     Returns
@@ -155,6 +158,15 @@ def estimate_evidence_lower_bound(mean, samples, n_eigenvalues, hamiltonian, con
 
     Notes
     -----
+    **IMPORTANT**: The provided estimate is missing the constant term :math:`-\\frac1 2 \\log \\det |2 \\pi N|`,
+    where :math:`N` is the noise covariance matrix. This term is not considered in (most of) the NIFTy
+    implementations of the energy operators, since it is constant throughout minimization. To obtain the actual ELBO
+    this term should be added. Since calculating this term can in principle be expensive and in most cases it is
+    not needed for model comparison (the noise statistics should be independent of the model at hand) it has here
+    been neglected.
+    Only when comparing models with different noise statistics (or when the ELBO is needed to approximate the true
+    evidence) this contribution should be added.
+
     It is advisable to start with a higher number of initial eigenvalues to ensure better convergence.
 
     See also
@@ -167,72 +179,78 @@ def estimate_evidence_lower_bound(mean, samples, n_eigenvalues, hamiltonian, con
     if not isinstance(samples, SampleListBase):
         raise TypeError("samples attribute should be of type SampleListBase.")
 
-    if verbose:
-        logger.info(f"Number of eigenvalues to compute: {n_eigenvalues}")
-
-    metric = SampledKLEnergyClass(samples, hamiltonian, constants, invariants,
-                                  False).metric  # FIXME: Check parameters of SKLEC
+    KL = SampledKLEnergyClass(samples, hamiltonian, constants, invariants, False)  # FIXME: Check parameters of SKLEC
+    # metric = KL.metric
+    mean = KL.position
+    lin = Linearization.make_var(mean, want_metric=True)
+    metric = hamiltonian(lin).metric
 
     Ar = SandwichOperator.make(_DomRemover(metric.domain).adjoint, metric)
-    M = ssl.LinearOperator(shape=2 * (metric.domain.size,), matvec=lambda x: Ar(makeField(Ar.domain, x)).val)
+    metric_size = metric.domain.size
+    M = ssl.LinearOperator(shape=2 * (metric_size,), matvec=lambda x: Ar(makeField(Ar.domain, x)).val)
 
-    eigenvalues, eigenvectors = _return_eigenspace(M, n_eigenvalues, tol)
+    if exact:
+        n_eigenvalues = metric_size
+        identity = np.identity(n_eigenvalues, dtype=np.float64)
+        m = []
+        for v in identity:
+            m.append(M.matvec(v))
+        M = np.vstack(m).T
+
+    if verbose:
+        logger.info(f"Number of eigenvalues to compute: {n_eigenvalues}")
+    eigenvalues, eigenvectors = _return_eigenspace(M, n_eigenvalues, tol, exact)
 
     eigenvalue_error = abs(1.0 - eigenvalues[-1])
     count = 0
 
-    while (eigenvalue_error > eps) and (count < max_iteration):
-        projected_metric = _Projector(eigenvectors) @ M if count == 0 else _Projector(eigenvectors) @ projected_metric
-        n_eigenvalues -= n_eigenvalues // 4 + fudge_factor  # FIXME: Not sure if it makes sense. Open to suggestions
-        if verbose:
-            logger.info(f"Number of additional eigenvalues being computed: {n_eigenvalues}")
+    if not exact:
+        while (eigenvalue_error > eps) and (count < max_iteration):
+            projected_metric = _Projector(eigenvectors) @ M if count == 0 else _Projector(
+                eigenvectors) @ projected_metric
+            n_eigenvalues -= n_eigenvalues // 4 + fudge_factor  # FIXME: Not sure if it makes sense. Open to suggestions
+            if verbose:
+                logger.info(f"Number of additional eigenvalues being computed: {n_eigenvalues}")
 
-        eigvals, eigenvectors = _return_eigenspace(projected_metric, n_eigenvalues, tol)
-        eigenvalues = np.concatenate((eigenvalues, eigvals))
-        eigenvalue_error = abs(1.0 - eigenvalues[-1])
+            eigvals, eigenvectors = _return_eigenspace(projected_metric, n_eigenvalues, tol, False)
+            eigenvalues = np.concatenate((eigenvalues, eigvals))
+            eigenvalue_error = abs(1.0 - eigenvalues[-1])
 
-        if eigenvalue_error == 0.:
-            break
-        count += 1
+            if eigenvalue_error == 0.:
+                break
+            count += 1
 
     if verbose:
-        logger.info(f"{eigenvalues.size} largest eigenvalues\n{eigenvalues}")
+        logger.info(f"{eigenvalues.size} largest eigenvalues (out of {metric_size})\n{eigenvalues}")
 
-    # Calculate the \Tr \ln term and \Tr terms
-    tr_reduced_diagonal_lat_cov = 0.
+    # Calculate the \Tr term and \Tr \log terms
+    tr_identity = metric_size
     tr_log_diagonal_lat_cov = 0.
+
     for ev in eigenvalues:
-        # The eigenvalues are of Theta^{-1} and we need the 1/ev for the ELBO calculation
-        ev_lat_cov = 1. / ev
-        # Error assessment made here is very conservative
-        if abs(ev_lat_cov - 1.) > eps:
-            tr_reduced_diagonal_lat_cov += ev_lat_cov - 1.
-        if abs(np.log(ev_lat_cov)) > eps:
-            tr_log_diagonal_lat_cov += np.log(ev_lat_cov)
+        if abs(np.log(ev)) > eps:
+            tr_log_diagonal_lat_cov -= np.log(ev)
 
     # Asses maximal error: propagate the error on the eigenvalues
-    unexplored_dimensions = metric.domain.size - n_eigenvalues
-    tr_reduced_diagonal_lat_cov_error = unexplored_dimensions * (min(eigenvalues) - 1.)
+    unexplored_dimensions = metric_size - n_eigenvalues
     tr_log_diagonal_lat_cov_error = unexplored_dimensions * np.log(min(eigenvalues))
-
-    # Calculate the contribution from the prior
-    squared_mean = mean.vdot(mean).val
-    prior_evidence = 0.5 * (squared_mean + tr_reduced_diagonal_lat_cov)
 
     hamiltonian_mean, hamiltonian_var = samples.sample_stat(hamiltonian)
     hamiltonian_mean_std = np.sqrt(hamiltonian_var.val / samples.n_samples)  # std of the estimate for the mean
 
     h0 = 0.
     # if isinstance(hamiltonian.likelihood_energy, ift.PoissonianEnergy): # FIXME: Try to simplify this expression
-    likelihood_type = str(hamiltonian.likelihood_energy.__dict__['_ops'][0])
+    likelihood_type = str(hamiltonian.likelihood_energy.__dict__['_op'])
 
-    if likelihood_type in {'PoissonianEnergy', 'PoissonianEnergy ()'}:
+    if 'PoissonianEnergy' in likelihood_type:
+        if data is None:
+            raise TypeError("For correct ELBO estimation data must be provided.")
         from scipy.special import factorial
         data = data.to_numpy() if hasattr(data, "to_numpy") else data.val if hasattr(data, "val") else data
         h0 = np.log(factorial(data)).sum()
 
     # elif not isinstance(hamiltonian.likelihood_energy, ift.GaussianEnergy): # FIXME: Try to simplify this expression
-    elif not likelihood_type in {'GaussianEnergy', 'GaussianEnergy ()'}:
+    elif not 'GaussianEnergy' in likelihood_type:
         tp_lh = type(hamiltonian.likelihood_energy)
         warn_msg = (f"Unknown likelihood of type {tp_lh!r};\n"
                     f"The (log) ELBO is potentially missing some constant term from the likelihood.")
@@ -240,28 +258,24 @@ def estimate_evidence_lower_bound(mean, samples, n_eigenvalues, hamiltonian, con
 
     # TODO: Implement these checks for all NIFTy-supported likelihoods
 
-    elbo_mean = - h0 - hamiltonian_mean - prior_evidence + 0.5 * tr_log_diagonal_lat_cov
-    elbo_var_upper = abs(
-        hamiltonian_mean_std - 0.5 * tr_reduced_diagonal_lat_cov_error + 0.5 * tr_log_diagonal_lat_cov_error)
-    elbo_var_lower = abs(
-        - hamiltonian_mean_std - 0.5 * tr_reduced_diagonal_lat_cov_error + 0.5 * tr_log_diagonal_lat_cov_error)
+    hamiltonian_mean = samples.sample_stat(hamiltonian)[0]
+    elbo_mean = - hamiltonian_mean + 0.5 * tr_identity + 0.5 * tr_log_diagonal_lat_cov - h0
+    elbo_var_upper = abs(hamiltonian_mean_std + 0.5 * tr_log_diagonal_lat_cov_error)
+    elbo_var_lower = abs(- hamiltonian_mean_std + 0.5 * tr_log_diagonal_lat_cov_error)
 
     stats = {"estimate": elbo_mean, "upper": elbo_mean + elbo_var_upper, "lower": elbo_mean - elbo_var_lower,
-             "ln_likelihood_mean": hamiltonian_mean, "ln_likelihood_mean_std": hamiltonian_mean_std,
-             "ln_likelihood_0": h0, "xi^2": squared_mean, "tr_reduced_diag_lat_cov": tr_reduced_diagonal_lat_cov,
-             "tr_reduced_diagonal_lat_cov_error": tr_reduced_diagonal_lat_cov_error,
-             "tr_ln_diag_lat_cov": tr_log_diagonal_lat_cov,
+             "hamiltonian_mean": hamiltonian_mean, "hamiltonian_mean_std": hamiltonian_mean_std,
+             "ln_likelihood_0": h0, "tr_log_diag_lat_cov": tr_log_diagonal_lat_cov,
              "tr_log_diagonal_lat_cov_error": tr_log_diagonal_lat_cov_error}
 
     if verbose:
         s = (f"\nELBO decomposition (in log units)"
-             f"\nELBO           : {stats['estimate'].val:.4e} (upper: {stats['upper'].val:.4e}, lower: "
+             f"\nELBO            : {stats['estimate'].val:.4e} (upper: {stats['upper'].val:.4e}, lower: "
              f"{stats['lower'].val:.4e})"
-             f"\nH_lh           : {stats['ln_likelihood_mean'].val:.4e} ± {stats['ln_likelihood_mean_std']:.5e}"
+             f"\n<H>             : {stats['hamiltonian_mean'].val:.4e} ± {stats['hamiltonian_mean_std']:.5e}"
              f"\nH_{{0, lh}}       : {stats['ln_likelihood_0']:.4e} "
-             f"\n\\xi^2         : {stats['xi^2']:.4e}"
-             f"\nTr \\Lambda     : {stats['tr_reduced_diag_lat_cov']:.5e} (+ "
-             f"{stats['tr_reduced_diagonal_lat_cov_error']:.5e})"
-             f"\nTr \\log \\Lambda : {stats['tr_ln_diag_lat_cov']:.5e} (+ {stats['tr_log_diagonal_lat_cov_error']:.5e})")
+             f"\nTr \\log \\Lambda : {stats['tr_log_diag_lat_cov']:.5e} (+ "
+             f"{stats['tr_log_diagonal_lat_cov_error']:.5e})"
+             )
         logger.info(s)
     return stats
