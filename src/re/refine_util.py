544a6a70b8c311dca7487e55071abf4e37d6eb06
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2022-04-06 22:18:49 +0200

    refine: Add helper method for error estimation
    To accommodate for the error estimation method of the refinement scheme,
    add various helper methods for translating shapes between coarse and
    fine grids.
    
    Introduce a method for computing the Kullback-Leibler (KL)
    divergence between a desired and an approximative covariance.
    
    Add tests for the various sub-routines.

---
+++ src/re/refine_util.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause
+
+from math import ceil
+import sys
+from typing import Iterable, Literal, Union
+from warnings import warn
+
+import jax
+from jax import numpy as jnp
+import numpy as np
+from scipy.spatial import distance_matrix
+
+from .refine import get_fixed_power_correlated_field
+
+
+def coarse2fine_shape(
+    shape0: Union[int, Iterable[int]],
+    depth: int,
+    *,
+    _coarse_size: int = 3,
+    _fine_size: int = 2,
+    _fine_strategy: Literal["jump", "extend"] = "jump",
+):
+    """Translates a coarse shape to its corresponding fine shape."""
+    shape0 = (shape0, ) if isinstance(shape0, int) else shape0
+    csz = int(_coarse_size)  # coarse size
+    fsz = int(_fine_size)  # fine size
+    if _fine_size % 2 != 0:
+        raise ValueError("only even numbers allowed for `_fine_size`")
+
+    shape = []
+    for shp in shape0:
+        sz_at = shp
+        for lvl in range(depth):
+            if _fine_strategy == "jump":
+                sz_at = fsz * (sz_at - (csz - 1))
+            elif _fine_strategy == "extend":
+                sz_at = fsz * ceil((sz_at - (csz - 1)) / (fsz // 2))
+            else:
+                ve = f"invalid `_fine_strategy`; got {_fine_strategy}"
+                raise ValueError(ve)
+            if sz_at <= 0:
+                ve = (
+                    f"`shape0` ({shape0}) with `depth` ({depth}) yield an"
+                    f" invalid shape ({sz_at}) at level {lvl}"
+                )
+                raise ValueError(ve)
+        shape.append(int(sz_at))
+    return tuple(shape)
+
+
+def fine2coarse_shape(
+    shape: Union[int, Iterable[int]],
+    depth: int,
+    *,
+    _coarse_size: int = 3,
+    _fine_size: int = 2,
+    _fine_strategy: Literal["jump", "extend"] = "jump",
+    ceil_sizes: bool = False,
+):
+    """Translates a fine shape to its corresponding coarse shape."""
+    shape = (shape, ) if isinstance(shape, int) else shape
+    csz = int(_coarse_size)  # coarse size
+    fsz = int(_fine_size)  # fine size
+    if _fine_size % 2 != 0:
+        raise ValueError("only even numbers allowed for `_fine_size`")
+
+    shape0 = []
+    for shp in shape:
+        sz_at = shp
+        for lvl in range(depth, 0, -1):
+            if _fine_strategy == "jump":
+                # solve for n: `fsz * (n - (csz - 1))`
+                sz_at = sz_at / fsz + (csz - 1)
+            elif _fine_strategy == "extend":
+                # solve for n: `fsz * ceil((n - (csz - 1)) / (fsz // 2))`
+                # NOTE, not unique because of `ceil`; use lower limit
+                sz_at_max = (sz_at / fsz) * (fsz // 2) + (csz - 1)
+                sz_at_min = ceil(sz_at_max - (fsz // 2 - 1))
+                for sz_at_cand in range(sz_at_min, int(sz_at_max) + 1):
+                    try:
+                        shp_w_min = coarse2fine_shape(
+                            (sz_at_cand, ),
+                            depth=depth - lvl + 1,
+                            _coarse_size=csz,
+                            _fine_size=fsz,
+                            _fine_strategy=_fine_strategy
+                        )[0]
+                    except ValueError as e:
+                        if "invalid shape" not in "".join(e.args):
+                            ve = "unexpected behavior of `coarse2fine_shape`"
+                            raise ValueError(ve) from e
+                        shp_w_min = -1
+                    if shp_w_min == shp:
+                        sz_at = sz_at_cand
+                        break
+                else:
+                    ve = "interval search within [{sz_at_min}, {sz_at_max}] failed"
+                    raise ValueError(ve)
+            else:
+                ve = f"invalid `_fine_strategy`; got {_fine_strategy}"
+                raise ValueError(ve)
+
+            sz_at = ceil(sz_at) if ceil_sizes else sz_at
+            if sz_at != int(sz_at):
+                raise ValueError(f"invalid shape at level {lvl}")
+        shape0.append(int(sz_at))
+    return tuple(shape0)
+
+
+def gauss_kl(cov_desired, cov_approx, *, m_desired=None, m_approx=None):
+    cov_t_ds, cov_t_dl = jnp.linalg.slogdet(cov_desired)
+    cov_a_ds, cov_a_dl = jnp.linalg.slogdet(cov_approx)
+    if (cov_t_ds * cov_a_ds) <= 0.:
+        raise ValueError("fraction of determinants must be positive")
+
+    cov_a_inv = jnp.linalg.inv(cov_approx)
+
+    kl = -cov_desired.shape[0]  # number of dimensions
+    kl += cov_a_dl - cov_t_dl + jnp.trace(cov_a_inv @ cov_desired)
+    if m_approx is not None and m_desired is not None:
+        m_diff = m_approx - m_desired
+        kl += m_diff @ cov_a_inv @ m_diff
+    elif not (m_approx is None and m_approx is None):
+        ve = "either both or neither of `m_approx` and `m_desired` must be `None`"
+        raise ValueError(ve)
+    return 0.5 * kl
+
+
+def refinement_approximation_error(
+    *,
+    shape,
+    distances,
+    depth,
+    kernel,
+    cutout=None,
+    _coarse_size=3,
+    _fine_size=2,
+    _fine_strategy="jump"
+):
+    """Computes the Kullback-Leibler (KL) divergence of the true covariance versus the
+    approximative one for a given kernel and shape of the fine grid.
+
+    If the desired shape can not be matched, the next larger one is used and
+    the field is subsequently cropped to the desired shape.
+    """
+    if _fine_strategy == "jump":
+        fpx_in_cpx = _fine_size**depth
+    elif _fine_strategy == "extend":
+        fpx_in_cpx = 2**depth
+    else:
+        ve = f"invalid `_fine_strategy`; got {_fine_strategy}"
+        raise ValueError(ve)
+    distances0 = tuple(d * fpx_in_cpx for d in distances)
+
+    if any(s <= 2 * fpx_in_cpx for s in shape):
+        msg = f"shape potentially too small (coarse pixel has {fpx_in_cpx} fine pixels)"
+        warn(msg)
+
+    shape0 = fine2coarse_shape(
+        shape,
+        depth,
+        ceil_sizes=True,
+        _coarse_size=_coarse_size,
+        _fine_size=_fine_size,
+        _fine_strategy=_fine_strategy
+    )
+    cf, dom = get_fixed_power_correlated_field(
+        shape0=shape0,
+        distances0=distances0,
+        depth=depth,
+        kernel=kernel,
+        _coarse_size=_coarse_size,
+        _fine_size=_fine_size,
+        _fine_strategy=_fine_strategy
+    )
+    tgt = jax.eval_shape(cf, dom)
+    cf_T = jax.linear_transpose(cf, dom)
+    cov_implicit = jax.jit(lambda x: cf(*cf_T(x)))
+
+    if _fine_strategy == "jump":
+        d_at = np.atleast_1d(distances0) / _fine_size**depth
+    elif _fine_strategy == "extend":
+        d_at = np.atleast_1d(distances0) / 2**depth
+    else:
+        raise AssertionError()
+    c0 = [d * jnp.arange(sz, dtype=float) for d, sz in zip(d_at, tgt.shape)]
+    pos = jnp.stack(jnp.meshgrid(*c0, indexing="ij"), axis=0)
+
+    probe = jnp.zeros(pos.shape[1:])
+    indices = np.indices(pos.shape[1:]).reshape(pos.ndim - 1, -1)
+    cov_empirical = jax.lax.map(
+        lambda idx: cov_implicit(probe.at[tuple(idx)].set(1.)).ravel(),
+        indices.T
+    ).T  # vmap over `indices` w/ `in_axes=1, out_axes=-1`
+
+    p = jnp.moveaxis(pos, 0, -1).reshape(-1, pos.shape[0])
+    dist_mat = distance_matrix(p, p)
+    del p
+    cov_truth = kernel(dist_mat)
+
+    if cutout is None and tgt.shape != shape:
+        print(
+            f"cropping enlarged field (w/ shape {tgt.shape}) to {shape}",
+            file=sys.stderr
+        )
+        cutout = tuple(slice(s) for s in shape)
+    if cutout is not None:
+        sz = np.prod(shape)
+        cov_empirical = cov_empirical.reshape(tgt.shape * 2
+                                             )[cutout * 2].reshape(sz, sz)
+        cov_truth = cov_truth.reshape(tgt.shape * 2)[cutout * 2].reshape(sz, sz)
+
+    aux = {
+        "cov_empirical": cov_empirical,
+        "cov_truth": cov_truth,
+        "shape0": shape0,
+        "distances0": distances0,
+        "correlated_field": cf,
+        "domain": dom
+    }
+    return gauss_kl(cov_truth, cov_empirical), aux
---
+++ test/test_re/test_refine_util.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause
+
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+from jax.tree_util import Partial
+import numpy as np
+import pytest
+
+from nifty8.re import refine, refine_util
+
+pmp = pytest.mark.parametrize
+
+
+def matern_kernel(distance, scale, cutoff, dof):
+    from jax.scipy.special import gammaln
+    from scipy.special import kv
+
+    reg_dist = jnp.sqrt(2 * dof) * distance / cutoff
+    return scale**2 * 2**(1 - dof) / jnp.exp(
+        gammaln(dof)
+    ) * (reg_dist)**dof * kv(dof, reg_dist)
+
+
+scale, cutoff, dof = 1., 80., 3 / 2
+
+x = jnp.logspace(-6, 11, base=jnp.e, num=int(1e+5))
+y = matern_kernel(x, scale, cutoff, dof)
+y = jnp.nan_to_num(y, nan=0.)
+kernel = Partial(jnp.interp, xp=x, fp=y)
+
+
+@pmp("shape0", ((16, ), (13, 15), (11, 12, 13)))
+@pmp("depth", (1, 2))
+@pmp("_coarse_size", (3, 5, 7))
+@pmp("_fine_size", (2, 4, 6))
+@pmp("_fine_strategy", ("jump", "extend"))
+def test_shape_translations(
+    shape0, depth, _coarse_size, _fine_size, _fine_strategy
+):
+    kwargs = {
+        "_coarse_size": _coarse_size,
+        "_fine_size": _fine_size,
+        "_fine_strategy": _fine_strategy
+    }
+
+    eval_cf_shape = partial(
+        jax.eval_shape,
+        partial(
+            refine.correlated_field,
+            distances0=(1., ) * len(shape0),
+            kernel=lambda x: x,
+            **kwargs
+        )
+    )
+    dom = refine.get_refinement_shapewithdtype(shape0, depth, **kwargs)
+    tgt = eval_cf_shape(dom)
+    tgt_pred_shp = refine_util.coarse2fine_shape(shape0, depth, **kwargs)
+    assert tgt_pred_shp == tgt.shape
+    assert dom[-1].size == tgt.size == np.prod(tgt_pred_shp)
+
+    shape0_pred = refine_util.fine2coarse_shape(tgt.shape, depth, **kwargs)
+    dom_pred = refine.get_refinement_shapewithdtype(
+        shape0_pred, depth, **kwargs
+    )
+    tgt_pred = eval_cf_shape(dom_pred)
+
+    assert tgt.shape == tgt_pred.shape
+    if _fine_strategy == "jump":
+        assert shape0_pred == shape0
+    else:
+        assert _fine_strategy == "extend"
+        assert all(s0_p <= s0 for s0_p, s0 in zip(shape0_pred, shape0))
+
+
+@pmp("seed", (42, 45))
+def test_gauss_kl(seed, n_resamples=100):
+    rng = np.random.default_rng(seed)
+    for _ in range(n_resamples):
+        d = max(rng.poisson(4), 1)
+        m_t = rng.normal(size=(d, d))
+        m_t = m_t @ m_t.T
+        scl = rng.lognormal(2., 3.)
+
+        np.testing.assert_allclose(
+            refine_util.gauss_kl(m_t, m_t), 0., atol=4e-12
+        )
+        kl_rhs_scl = 0.5 * d * (np.log(scl) + 1. / scl - 1.)
+        np.testing.assert_allclose(
+            kl_rhs_scl, refine_util.gauss_kl(m_t, scl * m_t), rtol=4e-12
+        )
+        kl_lhs_scl = 0.5 * d * (-np.log(scl) + scl - 1.)
+        np.testing.assert_allclose(
+            kl_lhs_scl, refine_util.gauss_kl(scl * m_t, m_t), rtol=4e-12
+        )
