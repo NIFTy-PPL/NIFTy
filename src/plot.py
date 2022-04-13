e6a407b4b8bb0ba9d512f2bfd26c3d116ff8290d
Martin Reinecke <martin@mpa-garching.mpg.de>
2022-04-13 18:37:07 +0200

    ugly hack, but perhaps correct?

--- src/plot.py
+++ src/plot.py
@@ -69,6 +69,8 @@ def _hammer_helper(xsize):
     u, v = 2*(u-xc)/(xc/1.02), (v-yc)/(yc/1.02)
 
     mask = np.where((u*u/4 + v*v) <= 1.)
+    u*=2
+    v*=2
     umask, vmask = u[mask], v[mask]
 
     zmask = np.sqrt(1-umask*umask/16 - vmask*vmask/4)
