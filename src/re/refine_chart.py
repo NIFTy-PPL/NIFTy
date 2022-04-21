f1f2574c5d02e23ac4327ab51792652319349327
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2022-04-21 17:11:00 +0200

    refine: Check for true positive definiteness
    While being computationally expensive, it is absolutely necessary as to
    not produce NaNs in the filter matrices with which to correlated the
    fine excitations.

--- src/re/refine_chart.py
+++ src/re/refine_chart.py
@@ -428,9 +428,14 @@ def _coordinate_pixel_refinement_matrices(
     # the diagonal as estimate for the magnitude of the variance.
     fine_kernel_fallback = jnp.diag(jnp.abs(jnp.diag(fine_kernel)))
     # Never produce NaNs (https://github.com/google/jax/issues/1052)
+    # This is expensive but necessary (worse but cheaper:
+    # `jnp.all(jnp.diag(fine_kernel) > 0.)`)
+    is_pos_def = jnp.all(jnp.linalg.eigvalsh(fine_kernel) > 0)
     fine_kernel = jnp.where(
-        jnp.all(jnp.diag(fine_kernel) > 0.), fine_kernel, fine_kernel_fallback
+        is_pos_def, fine_kernel, fine_kernel_fallback
     )
+    # NOTE, use the Cholesky decomposition, even though already having computed
+    # the eigenvalues, as to get consistent results across platforms
     fine_kernel_sqrt = jnp.linalg.cholesky(fine_kernel)
 
     return olf, fine_kernel_sqrt
