5805f85d1d4b742bf571d59e956df29de12c717b
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2022-07-13 23:02:19 +0200

    Fix demos for new nifty.re.Model class

--- demos/getting_started_6_nifty2jax.py
+++ demos/getting_started_6_nifty2jax.py
@@ -76,7 +76,7 @@ noise_cov = 0.5**2
 key = random.PRNGKey(42)
 
 key, sk = random.split(key)
-synth_pos = signal_response.init(sk)
+synth_pos = jft.Field(signal_response.init(sk))
 data = synth_signal_response = signal_response(synth_pos)
 data += jnp.sqrt(noise_cov) * random.normal(sk, shape=data.shape)
 
@@ -99,7 +99,7 @@ MetricKL = jit(
 )
 
 key, subkey = random.split(key)
-pos = pos_init = 1e-2 * signal_response.init(subkey)
+pos = pos_init = 1e-2 * jft.Field(signal_response.init(subkey))
 
 # %% [markdown]
 # Let's do a simple MGVI minimization. Note, while this might look very similar
@@ -186,7 +186,7 @@ def timeit(stmt, setup=lambda: None, number=None):
     return Timed(time=t, number=number)
 
 
-r = signal_response.init(random.PRNGKey(54))
+r = jft.Field(signal_response.init(random.PRNGKey(54)))
 
 r_nft = ift.makeField(signal_response_nft.domain, r.val)
 data_nft = ift.makeField(signal_response_nft.target, data)
@@ -252,7 +252,7 @@ def get_lognormal_model(shapes, cfm_kwargs, data_key, noise_cov=0.5**2):
     )
 
     sk_signal, sk_noise = random.split(data_key)
-    synth_pos = signal_response.init(sk_signal)
+    synth_pos = jft.Field(signal_response.init(sk_signal))
     data = signal_response(synth_pos)
     data += jnp.sqrt(noise_cov) * random.normal(sk_noise, shape=data.shape)
 
