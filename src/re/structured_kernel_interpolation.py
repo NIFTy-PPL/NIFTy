1adcf534a6e02ffedd7fa7c91a3357cbd58bf269
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2022-09-07 14:28:17 +0200

    Implement position space SKI (KISS-GP)
    Use an implicit Toeplitz matrix to apply the kernel in position space.

--- src/re/structured_kernel_interpolation.py
+++ src/re/structured_kernel_interpolation.py
@@ -11,6 +11,52 @@ from .correlated_field import get_fourier_mode_distributor, hartley
 NDArray = Union[jnp.ndarray, np.ndarray]
 
 
+def matmul_toeplitz(c, x):
+    """Efficiently compute the product of an implicit Toeplitz matrix with a
+    vector.
+
+    Parameters
+    ----------
+    c : ndarray
+        One-dimensional column of the Toeplitz matrix. It is assumed that the
+        row `r` is given by `r = conjugate(c)`.
+    x : (M,) or (M, K) ndarray
+        Vector or matrix which to multiply from the right to the Toeplitz
+        matrix.
+    Returns
+    -------
+    Toeplitz @ x : (M,) or (M, K) ndarray
+        Result of the matrix multiplication.
+    """
+    from jax.numpy.fft import fft, ifft, rfft, irfft
+
+    c = c.ravel()
+    r = c.conjugate()
+
+    toepl_shp = (len(c), len(c))
+    x_shp = x.shape
+    if x.shape[0] != r.shape[0] or x.ndim > 2:
+        raise ValueError('invalid matrix product dimensions')
+    x = x.reshape(-1, 1) if x.ndim == 1 else x.reshape(x.shape[0], -1)
+    m = x.shape[1]
+
+    dtp = np.find_common_type(tuple(i.dtype for i in (r, c, x)), ())
+    r, c, x = (jnp.asarray(i, dtype=dtp) for i in (r, c, x))
+    embedded_col = jnp.concatenate((c, r[-1:0:-1]))
+
+    # For real inputs use rfft
+    cmplx = jnp.iscomplexobj(embedded_col) or jnp.iscomplexobj(x)
+    ht, iht = (fft, ifft) if cmplx else (rfft, irfft)
+    p = toepl_shp[0] + toepl_shp[1] - 1
+    ht_mat = ht(embedded_col, axis=0).reshape(-1, 1)
+    ht_x = ht(x, n=p, axis=0)
+
+    toepl_times_x = iht(ht_mat * ht_x, axis=0, n=p)[:toepl_shp[0], :]
+
+    out_shp = (toepl_shp[0], ) if len(x_shp) == 1 else (toepl_shp[0], m)
+    return toepl_times_x.reshape(*out_shp)
+
+
 def interp_mat(grid_shape, grid_bounds, sampling_points, *, distances=None):
     from scipy.sparse import coo_matrix  # TODO: use only JAX w/o SciPy or NumPy
     from jax.experimental.sparse import BCOO
@@ -260,6 +306,116 @@ class HarmonicSKI():
         ]
         p = np.stack(np.meshgrid(*p, indexing="ij"),
                      axis=-1).reshape(-1, len(self.grid_unpadded_shape))
+        kernel_inducing = kernel(distance_matrix(p, p))
+
+        return self.w @ kernel_inducing @ self.w.T + jitter
+
+
+class ToeplitzSKI():
+    def __init__(
+        self,
+        grid_shape: Tuple[int],
+        grid_bounds: Tuple[Tuple[float, float]],
+        sampling_points: NDArray,
+        kernel: Optional[Callable] = None,
+        jitter: Union[bool, float, None] = True
+    ):
+        """Instantiate a KISS-GP model of the covariance using a Toeplitz matrix
+        representation of the kernel.
+
+        Parameters
+        ----------
+        grid_shape :
+            Number of pixels along each axes of the inducing points within
+            `grid_bounds`.
+        grid_bounds :
+            Tuple of boundaries of length of the number of dimensions. The
+            boundaries should denote the leftmost and rightmost edge of the
+            modeling space.
+        sampling_points :
+            Locations of the modeled points within the grid.
+        kernel :
+            Position space kernel.
+        jitter :
+            Strength of the diagonal jitter which to add to the covariance.
+        """
+        if jitter is True:
+            if sampling_points.dtype.type == np.float64:
+                self.jitter = 1e-8
+            elif sampling_points.dtype.type == np.float32:
+                self.jitter = 1e-6
+            else:
+                raise NotImplementedError()
+        elif jitter is False:
+            self.jitter = None
+        else:
+            self.jitter = jitter
+
+        self.grid_shape = np.asarray(grid_shape)
+        self.grid_bounds = np.asarray(grid_bounds)
+        self.grid_distances = jnp.diff(self.grid_bounds,
+                                       axis=1).ravel() / self.grid_shape
+        self.ndim = len(grid_shape)
+
+        d = jnp.mgrid[tuple(slice(s) for s in grid_shape)]
+        d *= self.grid_distances.reshape((-1, ) + (1, ) * self.ndim)
+        self.grid_distances_to_zero = jnp.linalg.norm(d, axis=0)
+
+        self.w = interp_mat(grid_shape, grid_bounds, sampling_points)
+
+        self._kernel = kernel
+
+    @property
+    def kernel(self) -> Callable:
+        """Yields the harmonic kernel specified during initialization or throw
+        a `TypeError`.
+        """
+        if self._kernel is None:
+            te = (
+                "either specify a fixed kernel during initialization of the"
+                f" {self.__class__.__name__} class or provide one here"
+            )
+            raise TypeError(te)
+        return self._kernel
+
+    def __call__(self, x, kernel=None) -> NDArray:
+        """Applies the Covariance matrix."""
+        kernel = self.kernel if kernel is None else kernel
+        x_shp = x.shape
+        jitter = 0. if self.jitter is None else self.jitter * x
+
+        x = (self.w.T @ x.ravel()).reshape(tuple(self.grid_shape))
+        cov_row = kernel(self.grid_distances_to_zero)
+        x = matmul_toeplitz(cov_row, x)
+        x = (self.w @ x.ravel()).reshape(x_shp)
+        return x + jitter
+
+    def evaluate(self, kernel=None):
+        """Instantiate the full covariance matrix."""
+        probe = jnp.zeros(self.w.shape[0])
+        indices = jnp.arange(self.w.shape[0]).reshape(1, -1)
+
+        return jax.lax.map(
+            lambda idx: self(probe.at[tuple(idx)].set(1.), kernel=kernel).ravel(
+            ), indices.T
+        ).T  # vmap over `indices` w/ `in_axes=1, out_axes=-1`
+
+    def evaluate_(self, kernel=None) -> NDArray:
+        from scipy.spatial import distance_matrix
+
+        kernel = self.kernel if kernel is None else kernel
+
+        if self.jitter is None:
+            jitter = 0.
+        else:
+            jitter = self.jitter * jnp.eye(self.w.shape[0])
+
+        p = [
+            np.linspace(*b, num=sz, endpoint=True)
+            for b, sz in zip(self.grid_bounds, self.grid_shape)
+        ]
+        p = np.stack(np.meshgrid(*p, indexing="ij"),
+                     axis=-1).reshape(-1, len(self.grid_shape))
         kernel_inducing = kernel(distance_matrix(p, p))
 
         return self.w @ kernel_inducing @ self.w.T + jitter
