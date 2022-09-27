e9bb8199ddafedb97ef547eb4e74b15bbf819e55
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2022-09-27 18:12:45 +0200

    Implement and expose wrapper for ICR on HEALPix

--- src/re/__init__.py
+++ src/re/__init__.py
@@ -49,7 +49,7 @@ from .lanczos import stochastic_lq_logdet
 from .likelihood import Likelihood, StandardHamiltonian
 from .model import Model
 from .optimize import minimize, newton_cg, trust_ncg
-from .refine_chart import CoordinateChart, RefinementField
+from .refine_chart import CoordinateChart, RefinementField, RefinementHPField
 from .stats_distributions import (
     invgamma_invprior,
     invgamma_prior,
