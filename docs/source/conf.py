65be342ce35fb7c36f4d92b36b64d7fb1fb426ea
Vincent Eberle <veberle@mpa-garching.mpg.de>
2022-11-17 16:17:21 +0100

    fixing eq. in the doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,7 @@ needs_sphinx = '3.2.0'
 
 extensions = [
     'sphinx.ext.napoleon',   # Support for NumPy and Google style docstrings
-    'sphinx.ext.imgmath',    # Render math as images
+    # 'sphinx.ext.imgmath',    # Render math as images
     'sphinx.ext.viewcode',   # Add links to highlighted source code
     'sphinx.ext.intersphinx' # Links to other sphinx docs (mostly numpy)
 ]
