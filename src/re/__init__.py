5be65d38698fcf68f76c14fdde28a4e0a89bbfcf
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2023-03-03 21:24:21 -0500

    Implement smap that compiles only once

--- src/re/__init__.py
+++ src/re/__init__.py
@@ -1,8 +1,7 @@
 # Copyright(C) 2013-2021 Max-Planck-Society
 # SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause
 
-from . import lanczos
-from . import structured_kernel_interpolation
+from . import lanczos, structured_kernel_interpolation
 from .conjugate_gradient import cg, static_cg
 from .correlated_field import CorrelatedFieldMaker, non_parametric_amplitude
 from .energy_operators import (
@@ -38,19 +37,16 @@ from .kl import (
     geometrically_sample_standard_hamiltonian,
     sample_standard_hamiltonian,
 )
-from .kl_aux import (
-    mean_hessp,
-    mean_metric,
-    mean_value_and_grad,
-)
+from .kl_aux import mean_hessp, mean_metric, mean_value_and_grad
 from .lanczos import stochastic_lq_logdet
 from .likelihood import Likelihood, StandardHamiltonian
 from .logger import logger
-from .model import Model, Initializer
+from .model import Initializer, Model
 from .optimize import minimize, newton_cg, trust_ncg
 from .refine.chart import CoordinateChart, HEALPixChart
 from .refine.charted_field import RefinementField
 from .refine.healpix_field import RefinementHPField
+from .smap import smap
 from .stats_distributions import (
     invgamma_invprior,
     invgamma_prior,
