7c33a51edd66395363f412dadf5e62aad63ac9bc
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2023-09-07 16:32:10 +0200

    misc.py: Format

--- src/re/misc.py
+++ src/re/misc.py
@@ -1,10 +1,10 @@
 # Copyright(C) 2013-2021 Max-Planck-Society
 # SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause
 
-from typing import Any, Callable, Dict, Hashable, Mapping, TypeVar, Union
+from typing import Any, Callable, Dict, Hashable, Mapping, TypeVar
 
 import jax
-from jax import numpy as jnp, tree_map
+from jax import numpy as jnp
 
 O = TypeVar('O')
 I = TypeVar('I')
@@ -101,33 +101,35 @@ def interpolate(xmin=-7., xmax=7., N=14000) -> Callable:
 
     return decorator
 
+
 def _red_chisq(inp):
     #FIXME complex numbers (use nifty convention)
     return jnp.vdot(inp.conjugate(), inp).real / inp.size
 
-def reduced_chisq_stats(primals, samples = None, func = None):
+
+def reduced_chisq_stats(primals, samples=None, func=None):
     """Computes the reduced chi-squared summary statistics for given input.
 
     Parameters:
     -----------
     primals: tree-like
-        Input values to compute reduces chi-sq statistics. The statistics is 
+        Input values to compute reduces chi-sq statistics. The statistics is
         computed for each leaf of the pytree, i.E. only array-like leafs are
         square averaged. See `samples` and `func` for further infos.
     samples: Samples (optional)
         Posterior samples corresponding to primals. If provided, the chi-sq
-        statistics is computed for each sample, and the sample 
+        statistics is computed for each sample, and the sample
         mean and standard deviation of the statistics is returned.
     func: Callable (optional)
-        Function to compute the chi-sq statistics for instead of primals 
+        Function to compute the chi-sq statistics for instead of primals
         (samples). If provided, the statistics is computed for `func(x)` instead
         of `x` where x is either primals or a sample.
 
     Returns:
     --------
     reduced_chisq: tree-like
-        Pytree of Mean-Std pairs of the reduces chi-sq statistics at each leaf 
-        of the tree. Irregardless of samples being provided or not, the 
+        Pytree of Mean-Std pairs of the reduces chi-sq statistics at each leaf
+        of the tree. Irregardless of samples being provided or not, the
         resulting leafs are always Mean-Std pairs, with the Std always being
         zero if samples is None.
     """
