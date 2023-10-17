202c2ebca1c1a8f8fa5a557510fafe283674f734
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2023-10-17 11:54:57 +0200

    Revert "laplace_prior: Simplify"
    This reverts commit 93729999f10131d6b20211c79707bc1a16c5c0c3.

--- src/re/num/stats_distributions.py
+++ src/re/num/stats_distributions.py
@@ -14,10 +14,8 @@ def laplace_prior(alpha) -> Callable:
     from jax.scipy.stats import norm
 
     def standard_to_laplace(xi):
-        res = jnp.where(
-            xi < 0,
-            norm.logcdf(xi) + jnp.log(2), -1 * (norm.logcdf(-xi) + jnp.log(2))
-        )
+        res = (xi < 0) * (norm.logcdf(xi) + jnp.log(2))
+        res -= (xi > 0) * (norm.logcdf(-xi) + jnp.log(2))
         return res * alpha
 
     return standard_to_laplace
