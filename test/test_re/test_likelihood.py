83b9e6789f7cf52c046a7f67a4c6d00e647af3ac
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2023-11-30 16:25:28 +0100

    test_likelihood: Test adding nonvariable likelihoods

--- test/test_re/test_likelihood.py
+++ test/test_re/test_likelihood.py
@@ -20,6 +20,10 @@ def _identity(x):
     return x
 
 
+def tree_assert_allclose(a, b, **kwargs):
+    return jax.tree_map(partial(assert_allclose, **kwargs), a, b)
+
+
 def test_partial_insert_and_remove():
     # _identity takes exactly one argument, thus `insert_axes` and `flat_fill`
     # are length one tuples
@@ -87,5 +91,84 @@ def test_likelihood_partial(seed, forward):
     )
 
 
+def _Poissonian(x):
+    return jft.Poissonian(jax.tree_map(lambda y: jnp.exp(y).astype(int), x))
+
+
+@pmp("seed", (33, 42, 43))
+@pmp("likelihood", (jft.Gaussian, partial(jft.StudentT, dof=2.2), _Poissonian))
+@pmp("forward_a,forward_b", ((_identity, jnp.exp), (jnp.exp, jnp.reciprocal)))
+def test_nonvariable_likelihood_add(seed, likelihood, forward_a, forward_b):
+    key = random.PRNGKey(seed)
+    N_TRIES = 100
+    swd_a = jft.Vector((jft.ShapeWithDtype((3, 5)), ) * 2)
+    swd_b = jft.Vector((jft.ShapeWithDtype((12, 5)), ) * 3)
+    key_a, key_b = "a", "b"
+
+    def fwd_a(x):
+        x = jax.tree_map(forward_a, x[key_a])
+        everything_pos = likelihood is _Poissonian
+        return jax.tree_map(jnp.abs, x) if everything_pos else x
+
+    def fwd_b(x):
+        x = jax.tree_map(forward_b, x[key_b])
+        everything_pos = likelihood is _Poissonian
+        return jax.tree_map(jnp.abs, x) if everything_pos else x
+
+    def forward(x):
+        return jft.Vector({key_a: fwd_a(x), key_b: fwd_b(x)})
+
+    key, k_a, k_b = random.split(key, 3)
+    data_a = jft.random_like(k_a, swd_a)
+    data_b = jft.random_like(k_b, swd_b)
+    data_ab = jft.Vector({key_a: data_a, key_b: data_b})
+
+    lh_orig = likelihood(data_ab) @ forward
+    lh_a = likelihood(data_a).amend(fwd_a, domain={key_a: swd_a})
+    lh_b = likelihood(data_b).amend(fwd_b, domain={key_b: swd_b})
+    lh_ab = lh_a + lh_b
+
+    key, k_p, k_t, k_q = random.split(key, 4)
+    swd = jft.Vector({key_a: swd_a, key_b: swd_b})
+    rl = jax.vmap(jft.random_like, in_axes=(0, None))
+    p, t, q = tuple(rl(random.split(k, N_TRIES), swd) for k in (k_p, k_t, k_q))
+
+    assert_allclose(jax.vmap(lh_orig)(p), jax.vmap(lh_ab)(p), equal_nan=False)
+    rsm_orig = jax.vmap(lh_orig.right_sqrt_metric)(p, t)
+    rsm_ab = jax.vmap(lh_ab.right_sqrt_metric)(p, t)
+    tree_assert_allclose(
+        rsm_orig.tree[key_a], rsm_ab["lh_left"], equal_nan=False
+    )
+    tree_assert_allclose(
+        rsm_orig.tree[key_b], rsm_ab["lh_right"], equal_nan=False
+    )
+    tree_assert_allclose(
+        jax.vmap(
+            lambda p, t, q: lh_orig.
+            left_sqrt_metric(p, lh_orig.right_sqrt_metric(t, q))
+        )(p, t, q),
+        jax.vmap(
+            lambda p, t, q: lh_ab.
+            left_sqrt_metric(p, lh_ab.right_sqrt_metric(t, q))
+        )(p, t, q),
+        equal_nan=False
+    )
+    tree_assert_allclose(
+        jax.vmap(lh_orig.metric)(p, t),
+        jax.vmap(lh_ab.metric)(p, t),
+        equal_nan=False
+    )
+    trafo_orig = jax.vmap(lh_orig.transformation)(p)
+    trafo_ab = jax.vmap(lh_ab.transformation)(p)
+    tree_assert_allclose(
+        trafo_orig.tree[key_a], trafo_ab["lh_left"], equal_nan=False
+    )
+    tree_assert_allclose(
+        trafo_orig.tree[key_b], trafo_ab["lh_right"], equal_nan=False
+    )
+
+
 if __name__ == "__main__":
     test_likelihood_partial(33, jnp.exp)
+    test_nonvariable_likelihood_add(42, jft.Gaussian, jnp.exp, jnp.reciprocal)
+    test_nonvariable_likelihood_add(42, _Poissonian, jnp.exp, jnp.reciprocal)
