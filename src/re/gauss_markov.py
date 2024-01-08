6741c295f640f50f18587f00d9e4dfd468fe987e
Philipp Frank <philipp@mpa-garching.mpg.de>
2024-01-08 16:54:57 +0100

    re.gauss_markov.py: add gm processes

--- src/re/gauss_markov.py
+++ src/re/gauss_markov.py
@@ -5,28 +5,73 @@
 from functools import partial
 import numpy as np
 import jax.numpy as jnp
-from jax import Array
+from jax import Array, vmap
 from jax.lax import fori_loop
-from typing import Callable, Iterable, Optional, Union
+from typing import Union
 
 from .model import Model
 from .tree_math import ShapeWithDtype, random_like
 
+def _isscalar(x):
+    return jnp.ndim(x) == 0
+
+def discrete_gm_general(xi, init, drift, diffamp):
+    if _isscalar(drift):
+        drift = drift * jnp.ones((1, 1))
+    if _isscalar(diffamp):
+        diffamp = diffamp * jnp.ones((1, 1))
+
+    in_ax = (None if len(diffamp.shape)==2 else 0,0)
+    res = vmap(jnp.matmul, in_ax, 0)(diffamp, xi)
+
+    def loop(i, a):
+        d = drift[i] if len(drift.shape) > 2 else drift
+        return a.at[i+1].add(jnp.matmul(d, a[i]))
+
+    res = jnp.concatenate([init[jnp.newaxis,...], res], axis=0)
+    return fori_loop(0, res.size, loop, res)
+
+#def continuous_to_discrete(drift, amplitude):
+
+
+
+def scalar_gm(xi, init, drift, diffamp):
+    if not _isscalar(drift):
+        drift = drift[:, jnp.newaxis, jnp.newaxis]
+    if not _isscalar(diffamp):
+        diffamp = diffamp[:, jnp.newaxis, jnp.newaxis]
+    if isinstance(init, float):
+        init = jnp.array([init,])
+    return discrete_gm_general(xi[:, jnp.newaxis], init, drift, diffamp)[:, 0]
 
 def wiener_process(
         x0: float,
         xi: Array,
         sigma: Union[float, Array],
         dt: Union[float, Array]):
-    dts = np.sqrt(dt)
-    if isinstance(dts, float):
-        dts = np.ones(xi.size)*dts
-    else:
-        if dts.size != xi.size:
-            raise ValueError("Timesteps and excitations are incompatible")
-    res = jnp.cumsum(sigma*dts*xi) + x0
-    return jnp.concatenate((jnp.array([x0,]), res))
+    drift = 1.
+    amp = jnp.sqrt(dt)*sigma
+    return scalar_gm(xi, x0, drift, amp)
 
+def integrated_wiener_process(
+        x0: Array,
+        xi: Array,
+        sigma: Array,
+        dt: Array,
+        asperity: Union[float, Array] = None):
+    asperity = 0. if asperity is None else asperity
+    dt = jnp.ones(xi.shape[0])*dt if _isscalar(dt) else dt
+    def drift_amp(d, sig, asp):
+        drift = jnp.array([[1., d], [0., 1.]])
+        amp = jnp.array([[jnp.sqrt(d**2 / 12. + asp), d / 2.],
+                         [0., 1.]])
+        amp *= sig*jnp.sqrt(d)
+        return drift, amp
+    axs = (0,
+           None if _isscalar(sigma) else 0,
+           None if _isscalar(asperity) else 0)
+    drift, amp = vmap(drift_amp, axs, (0,0))(dt, sigma, asperity)
+    return discrete_gm_general(xi, x0, drift, amp)
 
 def ou_process(
         x0: float,
@@ -34,31 +79,17 @@ def ou_process(
         alpha: Union[float, Array],
         gamma: Union[float, Array],
         dt: Union[float, Array]):
-    if isinstance(dt, float):
-        dt = np.ones(xi.size)*dt
-    else:
-        if dt.size != xi.size:
-            raise ValueError("Timesteps and excitations are incompatible")
+    drift = jnp.exp(-gamma*dt)
+    amp = alpha * jnp.sqrt(1. - drift**2)
+    return scalar_gm(xi, x0, drift, amp)
 
-    bias = jnp.exp(-gamma*dt)
-    wgt = alpha * jnp.sqrt(1. - bias**2)
-
-    r = wgt * xi
-    res = jnp.concatenate((jnp.array([x0,]), jnp.zeros_like(r)))
-    def loop(i, a):
-        a = a.at[i+1].set(bias[i] * a[i] + r[i])
-        return a
-    return fori_loop(0, res.size, loop, res)
-
-
-def stationary_ou(
+def stationary_init_ou(
         xi: Array,
         alpha: Union[float, Array],
         gamma: Union[float, Array],
         dt: Union[float, Array]):
-    x0 = (alpha if isinstance(alpha, float) else alpha[0]) * xi[0]
-    xi = xi[1:]
-    return ou_process(x0, xi, alpha, gamma, dt)
+    x0 = (alpha if _isscalar(alpha) else alpha[0]) * xi[0]
+    return ou_process(x0, xi[1:], alpha, gamma, dt)
 
 
 class WienerProcess(Model):
@@ -68,7 +99,7 @@ class WienerProcess(Model):
                  dt: Union[float, Array],
                  name: str = 'wpxi',
                  N_steps: int = None):
-        if isinstance(dt, float):
+        if _isscalar(dt):
             dt = np.ones(N_steps) * dt
         shape = dt.shape
         domain = {name: ShapeWithDtype(shape, float)}
@@ -94,7 +125,7 @@ class OUProcess(Model):
                  dt: Union[float, Array],
                  name: str = 'ouxi',
                  N_steps: int = None):
-        if isinstance(dt, float):
+        if _isscalar(dt):
             dt = np.ones(N_steps) * dt
         shape = dt.shape
         domain = {name: ShapeWithDtype(shape, float)}
