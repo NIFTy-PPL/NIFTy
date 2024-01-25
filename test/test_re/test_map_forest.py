dc4d864c5f114526e4c44a90edcaf3f857f045ec
Harth-Kitzerow Johannes <jharthki@mpa-garching.mpg.de>
2024-01-25 18:03:36 +0100

    commented test and specified test function naming

--- test/test_re/test_map_forest.py
+++ test/test_re/test_map_forest.py
@@ -6,31 +6,35 @@ import nifty8.re as jft
 
 pmp = pytest.mark.parametrize
 
-def test_map_forest():
+def test_map_forest_out_axes():
     f = lambda x : x
     with pytest.raises(TypeError):
+        # out_axes != 0
         jft.map_forest(
 				f,
 				out_axes = 1
 		)
 
-def test_map_forest():
+def test_map_forest_in_axes():
     f = lambda x : x
     jft.map_forest(
 		f,
 		in_axes = 1
     )
     with pytest.raises(ValueError):
+        # len(in_axes) > 2
 	    jft.map_forest(
 	    	f,
 	    	in_axes = (1,2)
 	    )
     with pytest.raises(ValueError):
+        # in_axes shoult be int or list of ints
 	    jft.map_forest(
 	    	f,
 	    	in_axes = (None,)
 	    )
     with pytest.raises(TypeError):
+        # in_axes should not contain floats
 	    jft.map_forest(
 	    	f,
 	    	in_axes = (1.,)
