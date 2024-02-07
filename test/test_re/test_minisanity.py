20a22d63634988eb1590980bade01c3cee494946
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-02-06 19:00:20 -0800

    test_minisanity: Format

--- test/test_re/test_minisanity.py
+++ test/test_re/test_minisanity.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause
 
 import numpy as np
-from numpy.testing import assert_allclose, assert_array_equal
 import pytest
+from numpy.testing import assert_allclose, assert_array_equal
 
 from nifty8.re.minisanity import reduced_residual_stats
 
@@ -32,15 +32,16 @@ def test_reduced_residual_stats_normal_residuals(seed):
         rtol=rtol
     )
 
+
 @pmp("seed", (42, 43))
 @pmp("complex", (True, False))
 def test_reduced_chi_square_dtype(seed, complex):
     sseq = np.random.SeedSequence(seed)
     rng = np.random.default_rng(sseq)
-    n_npix =500
+    n_npix = 500
     norm_res = rng.normal(size=(n_npix), )
     if complex:
-        norm_res = norm_res + 1j*rng.normal(size=(n_npix), )
+        norm_res = norm_res + 1j * rng.normal(size=(n_npix), )
     rrs = reduced_residual_stats(norm_res)
     assert rrs.mean.dtype == norm_res.dtype
     # chi^2 should be real and positive
@@ -49,7 +50,6 @@ def test_reduced_chi_square_dtype(seed, complex):
     atol, rtol = 1e-1, 1e-1
     assert_allclose(rrs.reduced_chisq[0], 1., atol=atol, rtol=rtol)
     assert_allclose(rrs.mean, 0., atol=atol, rtol=rtol)
-
 
 
 if __name__ == "__main__":
