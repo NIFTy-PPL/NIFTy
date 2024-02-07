480927131f43ed0dd678b76681c356dd1eba70a5
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-02-06 20:54:00 -0800

    minimal_benchmark_vis: Compare multiple number of cores

--- paper/minimal_benchmark_vis.py
+++ paper/minimal_benchmark_vis.py
@@ -17,32 +17,58 @@ import plotly.graph_objects as go
 # ```
 
 # %%
-fn_benchmark = "benchmark_nthreads=1_devices=cpu+NVIDIA A100-SXM4-80GB.npy"
-savestate = np.load(fn_benchmark, allow_pickle=True).item()
+benchmark_files = {
+    "1": "benchmark_nthreads=1_devices=cpu+NVIDIA A100-SXM4-80GB.npy",
+    "8": "benchmark_nthreads=8_devices=NVIDIA A100-SXM4-80GB.npy",
+}
+savestate = {}
+for prefix, fn in benchmark_files.items():
+    savestate[prefix] = np.load(fn, allow_pickle=True).item()
 
+# Make the GPU timings the first entry in the dictionary as to automatically
+# order the graphs in the following figure nicely
+for p, svt in savestate.items():
+    for nm in svt["all_t_jft"].keys():
+        gpu_key = None
+        if nm.lower().startswith("nvidia"):
+            gpu_key = nm
+        gpu_key = set((gpu_key,)) if gpu_key is not None else set()
+    new_key_order = set(svt["all_t_jft"].keys()) - gpu_key
+    new_key_order = tuple(gpu_key) + tuple(new_key_order)
+    svt["all_t_jft"] = {k: svt["all_t_jft"][k] for k in new_key_order}
+
+# %%
 fig = go.Figure()
-for nm in savestate["all_t_jft"].keys():
-    pretty_name = nm if not nm.lower().startswith("nvidia") else "GPU"
+n_gpu_plots = 0
+for prefix, svt in savestate.items():
+    for nm in svt["all_t_jft"].keys():
+        pretty_name = nm.upper() + f" w/ {prefix} core(s)"
+        if nm.lower().startswith("nvidia"):
+            pretty_name = "GPU"
+            n_gpu_plots += 1
+            if n_gpu_plots > 1:
+                continue
+        fig.add_trace(
+            go.Scatter(
+                x=np.array([np.prod(d) for d in svt["all_dims"]]),
+                y=np.array([t.time for t in svt["all_t_jft"][nm]]),
+                mode="lines+markers",
+                name=f"NIFTy.re on {pretty_name}",
+            )
+        )
+    assert len(svt["all_t_nft"])
+    nm = list(svt["all_t_nft"].keys())[0]
     fig.add_trace(
         go.Scatter(
-            x=np.array([np.prod(d) for d in savestate["all_dims"]]),
-            y=np.array([t.time for t in savestate["all_t_jft"][nm]]),
+            x=np.array([np.prod(d) for d in svt["all_dims"]]),
+            y=np.array([t.time for t in svt["all_t_nft"][nm]]),
             mode="lines+markers",
-            name=f"{pretty_name.upper()} NIFTy.re",
+            name=f"NIFTy w/ {prefix} core(s)",
         )
     )
-assert len(savestate["all_t_nft"])
-nm = list(savestate["all_t_nft"].keys())[0]
-fig.add_trace(
-    go.Scatter(
-        x=np.array([np.prod(d) for d in savestate["all_dims"]]),
-        y=np.array([t.time for t in savestate["all_t_nft"][nm]]),
-        mode="lines+markers",
-        name=f"NIFTy",
-    )
-)
 
 fig.update_layout(
+    legend=dict(x=0.05, y=1),
     showlegend=True,
     template="plotly_white",
     xaxis_title="number of dimensions",
@@ -52,5 +78,6 @@ fig.update_layout(
 )
 fig.update_xaxes(type="log")
 fig.update_yaxes(type="log")
-fig.write_html(fn_benchmark.replace(".npy", ".html"))
-fig.write_image(fn_benchmark.replace(".npy", ".png"), scale=4)
+fn_out_stem = "benchmark_nthreads=1+8_devices=cpu+gpu"
+fig.write_html(fn_out_stem + ".html")
+fig.write_image(fn_out_stem + ".png", scale=4)
