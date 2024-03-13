f78abdccb7ffbf445e420a3a96243b5f87f006fa
Philipp Frank <philipp@mpa-garching.mpg.de>
2024-03-13 15:28:23 +0100

    indexing.py: Revise indexing logic
    Co-authored-by: Gordian Edenhofer <gordian.edenhofer@gmail.com>

--- src/re/multi_grid/indexing.py
+++ src/re/multi_grid/indexing.py
@@ -3,7 +3,7 @@
 import operator
 from dataclasses import dataclass
 from functools import reduce
-from typing import Optional, Union
+from typing import Any, Callable, Iterable, Optional, Union
 
 import numpy as np
 
@@ -11,45 +11,82 @@ _kind_of_int = Union[tuple[int], list[int], int]
 
 
 @dataclass(kw_only=True)
-class GridAxisAtLevel:
-    size: int
-    refinement_size: int
-    parent_refinement_size: int = None
+class GridAtLevel:
+    shape: tuple[int]
+    refinement_shape: tuple[int]
+    parent_refinement_shape: Optional[tuple[int]] = None
+
+    def __init__(self, shape, refinement_shape, parent_refinement_shape):
+        self.shape = np.asarray(shape)
+        self.refinement_shape = np.asarray(refinement_shape)
+        self.parent_refinement_shape = np.asarray(parent_refinement_shape)
 
     def _parse_index(self, index):
-        if np.any(np.abs(index) >= self.size):
+        index = np.asarray(index)
+        if np.any(np.any(np.abs(idx) >= s) for idx, s in zip(index, self.shape)):
             nm = self.__class__.__name__
-            ve = f"index {index} is out of bounds for {nm} with size {self.size}"
+            ve = f"index {index} is out of bounds for {nm} with shape {self.shape}"
             raise IndexError(ve)
-        return index % self.size
+        return index % self.shape[(slice(None),) + (np.newaxis,) * (index.ndim - 1)]
+
+    @property
+    def size(self):
+        return len(self.shape)
 
     def children(self, index) -> np.ndarray:
         index = self._parse_index(index)
         dtp = np.result_type(index)
-        return np.add.outer(
-            self.refinement_size * index, np.arange(self.refinement_size, dtype=dtp)
+        rs = self.refinement_shape[(slice(None),) + (None,) * (index.ndim - 1)]
+        rs_bc = (slice(None),) * index.ndim, (None,) * len(self.refinement_shape)
+        c = np.mgrid[tuple(slice(0, sz) for sz in self.refinement_shape)].astype(dtp)
+        c_bc = (
+            (slice(None),)
+            + (None,) * (index.ndim - 1)
+            + (slice(None),) * len(self.refinement_shape)
         )
+        return rs[rs_bc] + c[c_bc]
 
-    def neighborhood(self, index, window_size: int):
+    def neighborhood(self, index, window_size: int, ensemble_axis=None):
         index = self._parse_index(index)
         dtp = np.result_type(index)
+        # TODO
         return (
             np.add.outer(index, np.arange(window_size, dtype=dtp) - window_size // 2)
             % self.size
         )
 
     def parent(self, index):
-        if self.parent_refinement_size is None:
+        if self.parent_refinement_shape is None:
             raise IndexError("you are alone in this world")
-        return index // self.parent_refinement_size
+        index = self._parse_index(index)
+        bc = (slice(None),) + (np.newaxis,) * (index.ndim - 1)
+        return index // self.parent_refinement_shape[bc]
+
+    def index2coord(self, index, **kwargs):
+        return NotImplementedError()
+
+    def coord2index(self, coord, **kwargs):
+        return NotImplementedError()
+
+    def index2volume(self, index, **kwargs):
+        return NotImplementedError()
 
 
-class GridAxis:
+class Grid:
     """Dense (single) axis with periodic boundary conditions.
 
     Open boundary conditions can be emulated by leaving out indices."""
 
-    def __init__(self, *, size0: int, refinement_size: _kind_of_int):
+    atLevel: Callable
+
+    def __init__(
+        self,
+        *,
+        shape0: int,
+        refinement_shape: tuple[_kind_of_int],
+        atLevel=GridAtLevel,
+    ):
+        # TODO
         self.size0 = size0
         refinement_size = (
             (refinement_size,) if isinstance(refinement_size, int) else refinement_size
@@ -58,27 +95,38 @@ class GridAxis:
         self.depth = len(self.refinement_size)
 
     def _parse_level(self, level):
+        # TODO
         if np.abs(level) >= self.depth:
             raise IndexError(f"{self.__class__.__name__} does not have level {level}")
         return level % self.depth
 
     def amend(self, refinement_size: _kind_of_int):
+        # TODO
         refinement_size = (
             (refinement_size,) if isinstance(refinement_size, int) else refinement_size
         )
-        return GridAxis(
+        return self.__class__(
             size0=self.size0,
             refinement_size=self.refinement_size + tuple(refinement_size),
         )
 
-    def at(self, level: int) -> GridAxisAtLevel:
+    def at(self, level: int) -> GridAtLevel:
+        # TODO
         level = self._parse_level(level)
         size = self.size0 * reduce(operator.mul, self.refinement_size[:level], 1)
         rs = self.refinement_size[level]
         rs_p = self.refinement_size[level - 1] if level >= 1 else None
-        return GridAxisAtLevel(
-            size=size, refinement_size=rs, parent_refinement_size=rs_p
+        return self.atLevel(
+            size=size, refinement_shape=rs, parent_refinement_shape=rs_p
         )
+
+
+class RegularGridAxisAtLevel(GridAtLevel):
+    def index2coord():
+        pass
+
+    def index2volume():
+        pass
 
 
 def _fill_bad_healpix_neighbors(nside, neighbors, nest: bool = True):
@@ -123,7 +171,7 @@ def _fill_bad_healpix_neighbors(nside, neighbors, nest: bool = True):
     return neighbors
 
 
-class HEALPixAxisAtLevel(GridAxisAtLevel):
+class HEALPixGridAtLevel(GridAtLevel):
     nside: int
     nest: True
 
@@ -193,7 +241,7 @@ class HEALPixAxisAtLevel(GridAxisAtLevel):
         return neighbors.astype(dtp)
 
 
-class HEALPixAxis(GridAxis):
+class HEALPixGrid(Grid):
     def __init__(
         self,
         *,
@@ -224,13 +272,13 @@ class HEALPixAxis(GridAxis):
             assert refinement_size is not None
             rss = refinement_size
             rss = (rss,) if isinstance(rss, int) else rss
-        return HEALPixAxis(
+        return HEALPixGrid(
             nside0=self.nside0,
             size0=self.size0,
             refinement_size=self.refinement_size + rss,
         )
 
-    def at(self, level: int) -> HEALPixAxisAtLevel:
+    def at(self, level: int) -> HEALPixGridAtLevel:
         level = self._parse_level(level)
         assert all(rs % 2 == 0 or rs == 1 for rs in self.refinement_size)
         nside = self.nside0 * reduce(
@@ -240,7 +288,7 @@ class HEALPixAxis(GridAxis):
         )
         rs = self.refinement_size[level]
         rs_p = self.refinement_size[level - 1] if level >= 1 else None
-        return HEALPixAxisAtLevel(
+        return HEALPixGridAtLevel(
             nside=nside, refinement_size=rs, parent_refinement_size=rs_p
         )
 
@@ -251,8 +299,9 @@ def _stack_outer(*arrays, outer_axis=-1, stack_axis=0):
     outer_axis %= len(arrays[0].shape)
     window_shape = tuple(a.shape[outer_axis] for a in arrays)
     out_shp = (
-        arrays[0].shape[:outer_axis] + window_shape +
-        arrays[0].shape[(outer_axis+1):]
+        arrays[0].shape[:outer_axis]
+        + window_shape
+        + arrays[0].shape[(outer_axis + 1) :]
     )
     stack_axis %= len(out_shp)
     out_shp = out_shp[:stack_axis] + (ndim,) + out_shp[stack_axis:]
@@ -260,11 +309,13 @@ def _stack_outer(*arrays, outer_axis=-1, stack_axis=0):
     res = np.zeros(out_shp, dtype=arrays[0].dtype)
     for i, a in enumerate(arrays):
         a = a.reshape(
-                a.shape[:outer_axis] +
-                (1,) * i + (window_shape, ) + (1,) * (ndim-i-1) +
-                a.shape[(outer_axis+1):]
-            )
-        res[(slice(None), ) * stack_axis + (i,)] += a
+            a.shape[:outer_axis]
+            + (1,) * i
+            + (window_shape,)
+            + (1,) * (ndim - i - 1)
+            + a.shape[(outer_axis + 1) :]
+        )
+        res[(slice(None),) * stack_axis + (i,)] += a
     return res
 
 
@@ -277,6 +328,7 @@ def regularAxis_index2cart(index, size):
 
 def healpixAxis_index2cart(index, nside, nest):
     from healpy.pixelfunc import pix2vec
+
     shp = index.shape[1:]
     cc = pix2vec(nside, np.ravel(index), nest=nest)
     return np.stack(cc, axis=0).reshape((3,) + shp)
@@ -284,21 +336,22 @@ def healpixAxis_index2cart(index, nside, nest):
 
 def default_index2cart(index, grids):
     coords = tuple(
-        healpixAxis_index2cart(index[i], g.nside, g.nest)
-        if isinstance(g, HEALPixAxisAtLevel) else
-        regularAxis_index2cart(index[i], g.size) for i, g in enumerate(grids)
+        (
+            healpixAxis_index2cart(index[i], g.nside, g.nest)
+            if isinstance(g, HEALPixGridAtLevel)
+            else regularAxis_index2cart(index[i], g.size)
+        )
+        for i, g in enumerate(grids)
     )
-    return coords #TODO better concatenate instead of tuple?
+    return coords  # TODO better concatenate instead of tuple?
 
 
 @dataclass(kw_only=True)
-class OGridAtLevel:
-    grids: tuple[GridAxisAtLevel]
-    index2cart: callable
+class OGridAtLevel(GridAtLevel):
+    grids: tuple[GridAtLevel]
 
-    def __init__(self, *grids, index2cart=default_index2cart):
+    def __init__(self, *grids):
         self.grids = tuple(grids)
-        self.index2cart = index2cart
 
     @property
     def shape(self):
@@ -316,8 +369,8 @@ class OGridAtLevel:
 
     def neighborhood(self, index, window_size: tuple[int]):
         window = (
-            g.neighborhood(index[i], w) for i, (g, w) in
-            enumerate(zip(self.grids, window_size))
+            g.neighborhood(index[i], w)
+            for i, (g, w) in enumerate(zip(self.grids, window_size))
         )
         window = _stack_outer(window, outer_axis=-1, stack_axis=0)
         assert window.dtype == np.result_type(index)
@@ -331,41 +384,45 @@ class OGridAtLevel:
 
 
 @dataclass(kw_only=True)
-class OGrid:
-    grids: tuple[GridAxis]
-    index2cart: callable
+class OGrid(Grid):
+    grids: tuple[Grid]
     depth: int
 
-    def __init__(self, *grids, index2cart=default_index2cart):
+    def __init__(self, *grids):
         self.grids = tuple(grids)
         for g in grids:
-            if not isinstance(g, GridAxis):
+            if not isinstance(g, Grid):
                 raise ValueError(f"Grid {g.__name__} not of type `GridAxis`")
         self.depth = self.grids[0].depth
         for i, g in enumerate(grids):
             if g.depth != self.depth:
                 msg = f"Grid {g.__name__} at index {i} of incompatible depth {g.depth}"
                 raise ValueError(msg)
-        self.index2cart = index2cart
 
     def amend(self, *grid_kwargs: tuple[dict]):
         grids = (g.amend(*kwargs) for g, kwargs in zip(self.grids, grid_kwargs))
         return OGrid(*grids, index2cart=self.index2cart)
 
-    def at(self, level: int) -> HEALPixAxisAtLevel:
+    def at(self, level: int) -> HEALPixGridAtLevel:
         grids = (g.at(level) for g in self.grids)
         return OGridAtLevel(grids)
 
 
-class UniqueGrid:
-    """Same as :class:`OGrid` but with a single global integer index for each voxel."""
+class FlatGridAtLevel:
+    """Same as :class:`Grid` but with a single global integer index for each voxel."""
 
     ogrid: OGrid
 
 
-class RealizedGrid:
-    """Realized :class:`UniqueGrid` keeping track of the indices that are actually
+class SparseGridAtLevel(FlatGridAtLevel):
+    real2flat: np.ndarray
+    flat2real: dict
+
+
+class SparseGrid:
+    """Realized :class:`FlatGridAtLevel` keeping track of the indices that are actually
     being modeled at the end of the day. This class is especially convenient for
     open boundary conditions but works for arbitrarily sparsely resolved grids."""
 
-    raw_index_to_: tuple[np.ndarray]
+    real2flat: tuple[np.ndarray]
+    flat2real: dict
