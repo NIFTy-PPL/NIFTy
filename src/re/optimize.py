fecb75403bc76f07d149a02e6af14bde1c6b8ac5
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-06-04 11:14:08 +0200

    Revert "Merge branch '408-host_callback_depricated' into 'NIFTy_8'"
    This reverts commit 47b1fc3c33afbc1e40523a30fa5ffdc7b278d6ce, reversing
    changes made to c40172cbbf768954e352f034d54a8b3a3baea562.

--- src/re/optimize.py
+++ src/re/optimize.py
@@ -274,7 +274,7 @@ def _trust_ncg(
     subproblem_kwargs: Optional[Dict[str, Any]] = None,
     name: Optional[str] = None
 ) -> OptimizeResults:
-    from jax.debug import callback
+    from jax.experimental.host_callback import call
 
     maxiter = 200 if maxiter is None else maxiter
 
@@ -421,7 +421,7 @@ def _trust_ncg(
                 "nhev": params.nhev,
                 "hit": sub_result.hits_boundary
             }
-            callback(pp, printable_state, result_shape=None)
+            call(pp, printable_state, result_shape=None)
         return params
 
     def _trust_region_cond_f(params: _TrustRegionState) -> bool:
