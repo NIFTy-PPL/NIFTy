14b3d96b690dbd9a5937de0394542e97d32c2b56
matteani <matteani@mpa-garching.mpg.de>
2024-08-02 15:27:13 +0200

    frequency_model: aligned to correlated field model, removed unnecessary builders, cosmetics

--- src/re/library/mf_model.py
+++ src/re/library/mf_model.py
@@ -1,13 +1,14 @@
 from functools import reduce, partial
-from typing import Optional
+from typing import Optional, Union
 
 import jax.numpy as jnp
 from jax import vmap
+from numpy.typing import ArrayLike
 
 from .. import ShapeWithDtype
 from ..num.stats_distributions import lognormal_prior, normal_prior
-from ..model import Model
-from ..gauss_markov import NdWienerProcess
+from ..model import Model, LazyModel
+from ..gauss_markov import build_wiener_process
 from ..correlated_field import (
     matern_amplitude, _make_grid, hartley, RegularCartesianGrid,
     WrappedCall)
@@ -40,21 +41,17 @@ def _build_distribution(
         white_init=True)
 
 
-def build_zero_mode_model(
-    prefix: str,
-    zero_mode_settings: dict,
-) -> Model:
-
-    zm_name = f'{prefix}_zero_mode'
-    _check_demands(zm_name, zero_mode_settings, demands={'mean', 'deviations'})
-
-    zero_mode_mean = zero_mode_settings['mean']
-    zero_mode_deviations = _build_distribution(
-        zm_name, 'deviations', zero_mode_settings, default=normal_prior)
-
-    return Model(
-        lambda x: zero_mode_mean + zero_mode_deviations(x),
-        init=zero_mode_deviations.init)
+def _build_distribution_or_default(arg: Union[callable, tuple],
+                                   key: str,
+                                   default: callable,
+                                   shape: tuple = (),
+                                   dtype=None):
+    return WrappedCall(
+            arg if callable(arg) else default(*arg),
+            name=key,
+            shape=shape,
+            dtype=dtype,
+            white_init=True)
 
 
 def build_amplitude_model(
@@ -62,10 +59,8 @@ def build_amplitude_model(
     grid_2d: RegularCartesianGrid,
     amplitude_settings: dict,
 ) -> Model:
-
-    # FIXME: Need support for correlated_field
-
-    amplitude_name = f'{prefix}_amplitude'
+    # TODO: Need support for correlated_field
+    amplitude_name = f'{prefix}_amplitude_'
     amplitude_settings = {**amplitude_settings,
                           **dict(renormalize_amplitude=False)}
     _check_demands(amplitude_name,
@@ -84,32 +79,11 @@ def build_amplitude_model(
         prefix=amplitude_name)
 
 
-def build_frequency_slope_model(
-    prefix: str,
-    shape_2d: tuple[int],
-    slope_settings: dict,
-):
-    slope_name = f'{prefix}_slope'
-    _check_demands(slope_name, slope_settings,
-                   demands={'mean', 'fluctuations'})
-
-    slope_mean = _build_distribution(
-        slope_name, 'mean', slope_settings, default=normal_prior)
-    slope_fluctuations = _build_distribution(
-        slope_name, 'fluctuations', slope_settings, default=lognormal_prior)
-    slope_xi = _build_distribution(
-        slope_name, 'xi', dict(xi=(0.0, 1.0)), normal_prior, shape=shape_2d)
-
-    return Model(
-        lambda x: (slope_mean(x), slope_fluctuations(x)*slope_xi(x)),
-        domain=slope_mean.domain | slope_fluctuations.domain | slope_xi.domain)
-
-
 def build_deviations_model(
     prefix: str,
     shape_2d: tuple[int],
-    log_frequencies: tuple[float],
-    reference_frequency: int,
+    log_frequencies: Union[tuple[float], ArrayLike],
+    reference_frequency_index: int,
     deviations_settings: Optional[dict],
 ) -> FrequencyDeviations:
 
@@ -122,11 +96,12 @@ def build_deviations_model(
     # FIXME: Need more processes
     if process_name in {'wiener', 'wiener_process'}:
         _check_demands(dev_name, deviations_settings, demands={'sigma'})
-        sigma = _build_distribution(
-            dev_name, 'sigma', deviations_settings, default=lognormal_prior)
+        sigma = _build_distribution(dev_name, 'sigma',
+                                    deviations_settings,
+                                    default=lognormal_prior)
 
         domain_key = f'{dev_name}_wp'
-        process = NdWienerProcess(
+        process = build_wiener_process(
             x0=jnp.zeros(shape_2d),  # 0,
             sigma=sigma,
             dt=log_frequencies[1:]-log_frequencies[0],
@@ -135,54 +110,64 @@ def build_deviations_model(
     else:
         raise NotImplementedError(f'{process_name} not implemented.')
 
-    return FrequencyDeviations(process, log_frequencies, reference_frequency)
+    return FrequencyDeviations(process, log_frequencies, reference_frequency_index)
 
 
-class MfModel(Model):
+class CorrelatedMultiFrequencySky(Model):
     def __init__(
-        self,
-        prefix: str,
-        # FIXME: spatial grid (doesn't have to be 2D)
-        grid_2d: RegularCartesianGrid,
-        log_relative_frequencies: tuple[float],
-        zero_mode: Model,
-        spatial_amplitude: Model,
-        spectral_index_mean: Model,
-        spectral_index_deviations: Optional[FrequencyDeviations] = None,
+            self,
+            prefix: str,
+            grid_2d: RegularCartesianGrid,  # FIXME: spatial grid (doesn't have to be 2D)
+            log_relative_frequencies: tuple[float],
+            zero_mode: Union[tuple, LazyModel],
+            zero_mode_offset: float,
+            spatial_amplitude: LazyModel,
+            spectral_index_mean: Union[tuple, LazyModel],
+            spectral_index_fluctuations: Union[tuple, LazyModel],
+            spectral_amplitude: LazyModel = None,  # TODO: add option
+            spectral_index_deviations: Optional[LazyModel] = None,
+            dtype: type = jnp.float64,
     ):
         self.prefix = prefix
         self.hdvol = 1.0 / grid_2d.total_volume
         self.pd = grid_2d.harmonic_grid.power_distributor
         self.ht = partial(hartley, axes=(0, 1))
-        self.freqs = jnp.array(log_relative_frequencies)[:, None, None]
-        self.zero_mode = zero_mode
+        self.freqs = jnp.array(log_relative_frequencies)[:, None, None]  # FIXME: spatial grid doesn't have to be 2D
         self.spatial_amplitude = spatial_amplitude
-        self.spectral_index_mean = spectral_index_mean
+        self.zero_mode_offset = zero_mode_offset
+        self._zm = _build_distribution_or_default(
+            zero_mode,
+            f'{prefix}_zero_mode',
+            normal_prior
+        )
+        self.spectral_index_mean = _build_distribution_or_default(
+            spectral_index_mean,
+            f'{prefix}_spectral_index_mean',
+            normal_prior
+        )
+        self.spectral_index_fluctuations = _build_distribution_or_default(
+            spectral_index_fluctuations,
+            f'{prefix}_spectral_index_fluctuations',
+            lognormal_prior
+        )
         self.spectral_index_deviations = spectral_index_deviations
 
-        models = [zero_mode, spatial_amplitude,
-                  spectral_index_mean, spectral_index_deviations]
+        models = [self._zm,
+                  self.spatial_amplitude,
+                  self.spectral_index_mean,
+                  self.spectral_index_fluctuations,
+                  self.spectral_index_deviations]
+
         domain = reduce(
             lambda a, b: a | b, [m.domain for m in models if m is not None]
         )
+
         domain[f"{self.prefix}_spatial_xi"] = ShapeWithDtype(
-            grid_2d.shape, jnp.float64)
+            grid_2d.shape, dtype)
+        domain[f"{self.prefix}_spectral_index_xi"] = ShapeWithDtype(
+            grid_2d.shape, dtype)
 
         super().__init__(self._build_apply(), domain=domain)
-
-    def spectral_index(self, p):
-        """Convenience function to retrieve the spectral index."""
-        amplitude = self.spatial_amplitude(p)
-        amplitude = amplitude.at[0].set(0.0)
-        slope_mean, slope = self.spectral_index_mean(p)
-        return self.hdvol*self.ht(amplitude[self.pd]*slope) + slope_mean
-
-    def spatial(self, p):
-        """Convenience function to retrieve the spatial intensity of the model."""
-        amplitude = self.spatial_amplitude(p)
-        amplitude = amplitude.at[0].set(0.0)
-        spatial_xi = p[f"{self.prefix}_spatial_xi"]
-        return self.hdvol*self.ht(amplitude[self.pd]*spatial_xi)
 
     def _build_apply(self):
         if self.spectral_index_deviations is not None:
@@ -193,17 +178,16 @@ class MfModel(Model):
                 amplitude = self.spatial_amplitude(p)
                 amplitude = amplitude.at[0].set(0.0)
                 spatial_xi = p[f"{self.prefix}_spatial_xi"]
+                spec_idx_xis = p[f"{self.prefix}_spectral_index_xi"]
 
-                slope_mean, slope = self.spectral_index_mean(p)
+                spectral_index = self.spectral_index_fluctuations(p) * spec_idx_xis
+                spec_idx_mean = self.spectral_index_mean(p)
                 deviations = self.spectral_index_deviations(p)
                 distributed_amplitude = amplitude[self.pd]
 
-                terms = spatial_xi + slope * self.freqs + deviations
-                # TODO: Check that vmap is not recomputed all the time?
+                terms = (spectral_index * self.freqs + spatial_xi + deviations)
                 ht_values = vmap(self.ht)(distributed_amplitude * terms)
-                full_return = self.hdvol * ht_values + (
-                    slope_mean * self.freqs + zm)
-                return jnp.exp(full_return)
+                return jnp.exp(self.hdvol * ht_values + spec_idx_mean * self.freqs + zm)
 
             return apply_with_deviations
 
@@ -214,13 +198,36 @@ class MfModel(Model):
             amplitude = self.spatial_amplitude(p)
             amplitude = amplitude.at[0].set(0.0)
             spatial_xi = p[f"{self.prefix}_spatial_xi"]
-            slope_mean, slope = self.spectral_index_mean(p)
+            spec_idx_xis = p[f"{self.prefix}_spectral_index_xi"]
+            spectral_index = self.spectral_index_fluctuations(p) * spec_idx_xis
+            spec_idx_mean = self.spectral_index_mean(p)
 
-            cfio = self.hdvol*self.ht(amplitude[self.pd]*spatial_xi)
-            cfsl = self.hdvol*self.ht(amplitude[self.pd]*slope) + slope_mean
-            return jnp.exp(cfio+zm + cfsl*self.freqs)
+            spatial_offset = self.hdvol*self.ht(amplitude[self.pd]*spatial_xi)
+            spectral_index_spatial = (self.hdvol*self.ht(amplitude[self.pd]*spectral_index)
+                                      + spec_idx_mean)
+            return jnp.exp(spatial_offset + zm + spectral_index_spatial*self.freqs)
 
         return apply_without_deviations
+
+    def spectral_index(self, p):
+        """Convenience function to retrieve the model's spectral index."""
+        amplitude = self.spatial_amplitude(p)
+        amplitude = amplitude.at[0].set(0.0)
+        spec_idx_fluctuations = self.spectral_index_fluctuations(p)
+        spec_idx_xis = p[f"{self.prefix}_spectral_index_xi"]
+        return (self.hdvol*self.ht(amplitude[self.pd]*spec_idx_fluctuations*spec_idx_xis)
+                + self.spectral_index_mean(p))
+
+    def spatial_distribution(self, p):
+        """Convenience function to retrieve the model's spatial distribution."""
+        amplitude = self.spatial_amplitude(p)
+        amplitude = amplitude.at[0].set(0.0)
+        spatial_xi = p[f"{self.prefix}_spatial_xi"]
+        return self.hdvol*self.ht(amplitude[self.pd]*spatial_xi)
+
+    def zero_mode(self, p):
+        """Convenience function to retrieve the model's zero mode."""
+        return self._zm(p) + self.zero_mode_offset
 
 
 def build_default_mf_model(
@@ -229,17 +236,14 @@ def build_default_mf_model(
     distances_2d: tuple[float],
     log_frequencies: tuple[float],
     reference_frequency: int,
-
     zero_mode_settings: dict,
     amplitude_settings: dict,
     slope_settings: dict,
     deviations_settings: Optional[dict] = None,
-
     harmonic_type: str = 'fourier',
-
-
 ):
-    '''Build multi frequency model.
+    '''
+    Build multi frequency model.
         f = exp( F * A * (
               io(k, l0) +
               slope(k) * (l-l0) +
@@ -268,26 +272,27 @@ def build_default_mf_model(
         - fluctuations: callable or parameters for default (lognormal_prior)
 
     deviations_settings: If none deviations are not build.
-        - process: wiener (default),
+        - process: wiener (default)
             - sigma: callable or parameters for default (lognormal_prior)
+    harmonic_type: the type of the harmonic domain
     '''
 
     grid_2d = _make_grid(shape_2d, distances_2d, harmonic_type)
 
     zero_mode_model = build_zero_mode_model(prefix, zero_mode_settings)
     spatial_model = build_amplitude_model(prefix, grid_2d, amplitude_settings)
-    frequency_slope_model = build_frequency_slope_model(
-        prefix, shape_2d, slope_settings)
     deviations_model = build_deviations_model(
         prefix, shape_2d, log_frequencies, reference_frequency, deviations_settings)
 
-    return MfModel(
+    return CorrelatedMultiFrequencySky(
         prefix=prefix,
         grid_2d=grid_2d,
         log_relative_frequencies=jnp.array(
             log_frequencies) - log_frequencies[reference_frequency],
-        zero_mode=zero_mode_model,
+        zero_mode=zero_mode_settings['deviations'],
+        zero_mode_offset=zero_mode_settings['mean'],
         spatial_amplitude=spatial_model,
-        spectral_index_mean=frequency_slope_model,
+        spectral_index_mean=slope_settings['mean'],
+        spectral_index_fluctuations=slope_settings['fluctuations'],
         spectral_index_deviations=deviations_model
     )
