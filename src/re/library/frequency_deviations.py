e7399e31ef3beb96c854005fd3530b77a54db840
matteani <matteani@mpa-garching.mpg.de>
2024-08-28 15:05:13 +0200

    frequency_model: fixed slope removal

--- src/re/library/frequency_deviations.py
+++ src/re/library/frequency_deviations.py
@@ -22,7 +22,7 @@ class FrequencyDeviations(Model):
 
     Parameters
     ----------
-    frequency_deviations_model: GaussMarkovProcess
+    deviations_process: GaussMarkovProcess
         A Gauss-Markov process used to model frequency deviations.
         TODO: Extend to allow for correlated field.
     frequencies: Union[tuple[float], ArrayLike]
@@ -32,32 +32,32 @@ class FrequencyDeviations(Model):
     """
     def __init__(
         self,
-        frequency_deviations_model: GaussMarkovProcess,  # TODO: Allow for correlated field
+        deviations_process: GaussMarkovProcess,  # TODO: Allow for correlated field
         frequencies: Union[tuple[float], ArrayLike],
         reference_frequency_index: int
     ):
-        self.frequency_deviations = frequency_deviations_model
+        self.deviations_process = deviations_process
 
         slicing_tuple = (
             (slice(None),) +
-            (None,) * len(self.frequency_deviations.target.shape[1:])
+            (None,) * len(self.deviations_process.target.shape[1:])
         )
         relative_freqs = jnp.array(
             frequencies - frequencies[reference_frequency_index])[slicing_tuple]
         frequencies_denominator = 1 / jnp.sum(relative_freqs**2)
 
         def deviations_call(p):
-            dev = self.frequency_deviations(p)
+            dev = self.deviations_process(p)
             dev = dev - dev[reference_frequency_index]
 
-            # m = sum_l(gmp(l) * (l-l0)) / sum_l((l-l0)**2)
+            # m = sum_l(\delta gmp(l) * (l-l0)) / sum_l((l-l0)**2)
             dev_slope = (jnp.sum(dev*relative_freqs, axis=0)
                          * frequencies_denominator)
 
             return dev - dev_slope * relative_freqs
 
         super().__init__(
-            deviations_call, init=frequency_deviations_model.init)
+            deviations_call, init=deviations_process.init)
 
 
 def build_frequency_deviations_model(
