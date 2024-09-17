a9a0e28f1d8ec6b71537342ca087f87f32e6c00a
Jakob Roth <roth@mpa-garching.mpg.de>
2024-09-17 14:23:41 +0200

    test_evi: format with black

--- test/test_re/test_evi.py
+++ test/test_re/test_evi.py
@@ -33,9 +33,9 @@ def wiener_filter_posterior(data, response, noise_covariance, prior_covariance):
     $\blacksquare^{-1}$ the
     [matrix-inverse](https://en.wikipedia.org/wiki/Invertible_matrix).
     """
-    noise_covariance = np.diag(
-        noise_covariance
-    ) if noise_covariance.ndim == 1 else noise_covariance
+    noise_covariance = (
+        np.diag(noise_covariance) if noise_covariance.ndim == 1 else noise_covariance
+    )
     noise_cov_inv = np.linalg.inv(noise_covariance)
     post_cov_inv = response.T @ noise_cov_inv @ response + np.linalg.inv(
         prior_covariance
@@ -44,9 +44,8 @@ def wiener_filter_posterior(data, response, noise_covariance, prior_covariance):
     return post_cov @ response.T @ noise_cov_inv @ data, post_cov
 
 
-def los_response(end, shape, extent=(0, 1.), start=0.):
-    """Compute the line-of-sight response matrix for a given shape and extent.
-    """
+def los_response(end, shape, extent=(0, 1.0), start=0.0):
+    """Compute the line-of-sight response matrix for a given shape and extent."""
     if len(shape) > 1:
         raise ValueError("only 1D shapes are supported.")
     start, end = np.broadcast_arrays(start, end)
@@ -60,28 +59,26 @@ def los_response(end, shape, extent=(0, 1.), start=0.):
     for i in range(shape[0]):
         # print(i, start, (i + 0.5 - start).clip(0., 1.))
         # Assume the zeroth pixel is centered at 0.0
-        l = np.minimum(
-            (i + 0.5 - start).clip(0., 1.), (end - i + 0.5).clip(0., 1.)
-        )
+        l = np.minimum((i + 0.5 - start).clip(0.0, 1.0), (end - i + 0.5).clip(0.0, 1.0))
         response.append(l)
     return np.stack(response, axis=1)
 
 
-def signal_cov(shape, extent=(0, 1.), scale=1., lengthscale=0.1):
+def signal_cov(shape, extent=(0, 1.0), scale=1.0, lengthscale=0.1):
     from scipy.spatial import distance_matrix
 
     if len(shape) > 1:
         raise ValueError("only 1D shapes are supported.")
     p = np.linspace(*extent, num=shape[0], endpoint=True)
     d = distance_matrix(p.reshape(-1, 1), p.reshape(-1, 1))
-    return scale * (np.exp(-(d / lengthscale)**2) + 1e-6 * np.eye(shape[0]))
+    return scale * (np.exp(-((d / lengthscale) ** 2)) + 1e-6 * np.eye(shape[0]))
 
 
 @pmp("seed", (12, 42))
 def test_mgvi_wiener_filter_consistency(
     seed,
-    shape=(16, ),
-    extent=(0., 1.),
+    shape=(16,),
+    extent=(0.0, 1.0),
     n_los=14,
     n_mgvi_cov_samples=16_000,
     n_vi_samples=16,
@@ -94,23 +91,19 @@ def test_mgvi_wiener_filter_consistency(
     prior_lsm = np.linalg.cholesky(prior_cov)
     dust_synth = prior_lsm @ random.normal(k_p, shape=shape)
     dust_synth = np.exp(dust_synth)  # Fake positivity; THIS VOILATES THE PRIOR
-    endpoints = random.uniform(
-        k_r, minval=extent[0], maxval=extent[1], shape=(n_los, )
-    )
+    endpoints = random.uniform(k_r, minval=extent[0], maxval=extent[1], shape=(n_los,))
     response = los_response(end=endpoints, shape=shape, extent=extent)
     k_n1, k_n2 = random.split(k_n)
     noise_cov = np.mean(dust_synth) * (
-        1 + random.uniform(k_n1, minval=0.1, maxval=0.5, shape=(n_los, ))
+        1 + random.uniform(k_n1, minval=0.1, maxval=0.5, shape=(n_los,))
     )
     data = response @ dust_synth
-    data += np.sqrt(noise_cov) * random.normal(k_n2, shape=(n_los, ))
+    data += np.sqrt(noise_cov) * random.normal(k_n2, shape=(n_los,))
     # plt.scatter(endpoints, data); plt.show()
 
     # Retrieve Wiener Filter posterior (even though model draws data slightly
     # differently)
-    post_mean, post_cov = wiener_filter_posterior(
-        data, response, noise_cov, prior_cov
-    )
+    post_mean, post_cov = wiener_filter_posterior(data, response, noise_cov, prior_cov)
     # plt.plot(np.array((dust_synth, post_mean)).T); plt.show()
     # plt.matshow(post_cov); plt.colorbar(); plt.show()
 
@@ -132,24 +125,20 @@ def test_mgvi_wiener_filter_consistency(
     lh = lh.amend(partial(jnp.matmul, response)).amend(forward)
 
     draw_linear_kwargs = dict(cg_kwargs=dict(absdelta=1e-10, maxiter=size))
-    draw_linear_residual = partial(
-        jft.draw_linear_residual, lh, **draw_linear_kwargs
-    )
+    draw_linear_residual = partial(jft.draw_linear_residual, lh, **draw_linear_kwargs)
     draw_linear_residual = jft.smap(draw_linear_residual, in_axes=(None, 0))
     # Compare metric of Likelihood + Prior against true posterior covariance
     key, sk = random.split(key)
-    some_positions = (
-        random.normal(sk, shape=shape), jnp.ones(shape), jnp.zeros(shape)
-    )
+    some_positions = (random.normal(sk, shape=shape), jnp.ones(shape), jnp.zeros(shape))
     key, k_smpl = random.split(key)
     for pos in some_positions:
         probe = jnp.zeros(shape)
         lh_metric = jax.vmap(
-            lambda i: lh.metric(pos, probe.at[i].set(1.)), out_axes=1
+            lambda i: lh.metric(pos, probe.at[i].set(1.0)), out_axes=1
         )(jnp.arange(size))
-        lh_metric_diy = prior_lsm.T @ response.T @ np.diag(
-            1. / noise_cov
-        ) @ response @ prior_lsm
+        lh_metric_diy = (
+            prior_lsm.T @ response.T @ np.diag(1.0 / noise_cov) @ response @ prior_lsm
+        )
         assert_allclose(lh_metric, lh_metric_diy, atol=1e-10, rtol=1e-12)
         mgvi_post_cov = jnp.linalg.inv(lh_metric + jnp.eye(size))
         # Compare posterior cov in space of standardized prior
@@ -163,11 +152,9 @@ def test_mgvi_wiener_filter_consistency(
         assert_array_equal(info, 0)
         residual_draw = jax.vmap(forward)(residual_draw)
         tol = (np.diag(post_cov) / n_mgvi_cov_samples).sum()
-        assert_allclose(residual_draw.mean(axis=0)**2, 0., atol=tol)
+        assert_allclose(residual_draw.mean(axis=0) ** 2, 0.0, atol=tol)
         smpl_post_cov = np.cov(residual_draw, rowvar=False)
-        assert_allclose(
-            np.diag(smpl_post_cov), np.diag(post_cov), atol=tol**0.5
-        )
+        assert_allclose(np.diag(smpl_post_cov), np.diag(post_cov), atol=tol**0.5)
         assert_allclose(smpl_post_cov, post_cov, atol=tol**0.5)
 
     kl_kwargs = dict(minimize_kwargs=dict(name="M", xtol=1e-10, maxiter=35))
@@ -219,8 +206,8 @@ def test_mgvi_wiener_filter_consistency(
 if __name__ == "__main__":
     test_mgvi_wiener_filter_consistency(
         42,
-        shape=(16, ),
-        extent=(0., 1.),
+        shape=(16,),
+        extent=(0.0, 1.0),
         n_los=14,
         n_mgvi_cov_samples=16_000,
         n_vi_samples=16,
