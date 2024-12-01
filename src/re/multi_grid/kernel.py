cee853a1885b108dba20a9411a38f2d69d22c0d5
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-12-01 21:13:10 +0100

    kernel.py: Tweak method names

--- src/re/multi_grid/kernel.py
+++ src/re/multi_grid/kernel.py
@@ -6,8 +6,9 @@
 import operator
 from collections import namedtuple
 from functools import partial, reduce
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Tuple
 
+from numpy import typing as npt
 import jax.numpy as jnp
 import numpy as np
 from jax import eval_shape, jit, vmap
@@ -51,92 +52,21 @@ class KernelBase:
 
     def __getitem__(self, key):
         index, level = key
-        return self.get_kernel(index, level)
+        return self.get_matrices(index, level)
 
     @property
-    def grid(self):
+    def grid(self) -> Grid:
         return self._grid
 
-    def get_indices(self, index, level):
-        raise NotImplementedError
+    def get_output_input_indices(
+        self, index, level: int
+    ) -> Tuple[
+        Tuple[npt.NDArray[np.int_], int], Tuple[Tuple[npt.NDArray[np.int_], int], ...]
+    ]:
+        raise NotImplementedError()
 
-    def get_kernel(self, index, level):
-        raise NotImplementedError
-
-    def freeze(
-        self,
-        *,
-        rtol=1e-5,
-        atol=1e-10,
-        buffer_size=10000,
-        use_distances=True,
-        uindices=None,
-        invindices=None,
-        indexmaps=None,
-        distance_norm=partial(jnp.linalg.norm, axis=0),
-    ):
-        """Evaluate the kernel and store it in a `FrozenKernel`. Kernels may be
-        grouped by similarity and accessed via lookup"""
-        if uindices is not None and invindices is not None and indexmaps is not None:
-            _FrozenKernel(self, uindices, invindices, indexmaps)
-
-        def get_distance_kernel(index, level):
-            if level is None:
-                raise NotImplementedError
-            (out, olvl), ids = self.get_indices(index, level)
-            out = out.reshape(index.shape + (-1,))
-            out = self.grid.at(olvl).index2coord(out)
-            assert index.ndim == out.ndim - 1
-            ids = tuple(self.grid.at(ii[1]).index2coord(ii[0]) for ii in ids)
-            ids = jnp.concatenate(ids, axis=-1)
-            assert index.ndim == ids.ndim - 1
-            return (distance_norm(out[..., jnp.newaxis] - ids[..., jnp.newaxis, :]),)
-
-        gridf = self.grid if isinstance(self.grid, FlatGrid) else FlatGrid(self.grid)
-        uindices = []
-        invindices = []
-        indexmaps = []
-        for lvl in range(self.grid.depth):
-            grid_at = self.grid.at(lvl)
-            grid_at_f = gridf.at(lvl)
-
-            def get_kernel(id):
-                id = jnp.atleast_1d(id)
-                f = get_distance_kernel if use_distances else self.get_kernel
-                ker = f(grid_at_f.flatindex2index(id), lvl)
-                ker = jnp.concatenate(tuple(kk.ravel() for kk in ker))
-                return ker
-
-            @jit
-            def scanned_amend_unique(carry, idx, shift):
-                (u, inv) = carry
-                k = get_kernel(idx)
-                u, invid = amend_unique_(u, k, axis=0, atol=atol, rtol=rtol)
-                inv = inv.at[idx - shift].set(invid)
-                return (u, inv), invid
-
-            indices = grid_at.refined_indices()
-            indices = grid_at_f.index2flatindex(indices)[0].ravel()
-            shift = np.min(indices)
-            size = np.max(indices) - shift
-            inv = np.full(size, buffer_size + 1)
-
-            shp = eval_shape(get_kernel, indices[0]).shape
-            unique = jnp.full((buffer_size,) + shp, jnp.nan)
-
-            (unique, inv), invid = scan(
-                partial(scanned_amend_unique, shift=shift), (unique, inv), indices
-            )
-            _, idx = np.unique(invid, return_index=True)
-            n = idx.size
-            if n >= unique.shape[0] or not np.all(np.isnan(unique[n:])):
-                raise ValueError("`mat_buffer_size` too small")
-            uids = indices[idx]
-            uids = grid_at_f.flatindex2index(uids[np.newaxis, :])
-            uindices.append(uids)
-            invindices.append(inv)
-            indexmaps.append(_IdxMap(shift, grid_at_f.index2flatindex))
-        return _FrozenKernel(self, uindices, invindices, indexmaps)
+    def get_matrices(self, index, level):
+        raise NotImplementedError()
 
     def apply(self, x, copy=True):
         """Applies the kernel to values on an entire multigrid"""
@@ -151,8 +81,8 @@ class KernelBase:
 
         def apply_at(index, level, x):
             assert index.ndim == 1
-            iout, iin = self.get_indices(index, level)
-            kernels = self.get_kernel(index, level)
+            iout, iin = self.get_output_input_indices(index, level)
+            kernels = self.get_matrices(index, level)
             assert len(iin) == len(kernels)
             res = reduce(
                 operator.add,
@@ -174,25 +104,97 @@ class KernelBase:
             x[lvl_nxt] = self.grid.at(lvl_nxt).resort(res)
         return x
 
+    def freeze(
+        self,
+        *,
+        rtol=1e-5,
+        atol=1e-10,
+        buffer_size=10000,
+        use_distances=True,
+        uindices=None,
+        invindices=None,
+        indexmaps=None,
+        distance_norm=partial(jnp.linalg.norm, axis=0),
+    ):
+        """Evaluate the kernel and store it in a `FrozenKernel`. Kernels may be
+        grouped by similarity and accessed via lookup"""
+        if uindices is not None and invindices is not None and indexmaps is not None:
+            _FrozenKernel(self, uindices, invindices, indexmaps)
+
+        def get_distance_matrix(index, level):
+            if level is None:
+                raise NotImplementedError
+            (out, olvl), ids = self.get_output_input_indices(index, level)
+            out = out.reshape(index.shape + (-1,))
+            out = self.grid.at(olvl).index2coord(out)
+            assert index.ndim == out.ndim - 1
+            ids = tuple(self.grid.at(ii[1]).index2coord(ii[0]) for ii in ids)
+            ids = jnp.concatenate(ids, axis=-1)
+            assert index.ndim == ids.ndim - 1
+            return (distance_norm(out[..., jnp.newaxis] - ids[..., jnp.newaxis, :]),)
+
+        gridf = self.grid if isinstance(self.grid, FlatGrid) else FlatGrid(self.grid)
+        uindices = []
+        invindices = []
+        indexmaps = []
+        for lvl in range(self.grid.depth):
+            grid_at = self.grid.at(lvl)
+            grid_at_f = gridf.at(lvl)
+
+            def get_matrices(idx):
+                f = get_distance_matrix if use_distances else self.get_matrices
+                ker = f(grid_at_f.flatindex2index(jnp.atleast_1d(idx)), lvl)
+                ker = jnp.concatenate(tuple(kk.ravel() for kk in ker))
+                return ker
+
+            @jit
+            def scanned_amend_unique(carry, idx, shift):
+                (u, inv) = carry
+                k = get_matrices(idx)
+                u, invid = amend_unique_(u, k, axis=0, atol=atol, rtol=rtol)
+                inv = inv.at[idx - shift].set(invid)
+                return (u, inv), invid
+
+            indices = grid_at.refined_indices()
+            indices = grid_at_f.index2flatindex(indices)[0].ravel()
+            shift = np.min(indices)
+            size = np.max(indices) - shift
+            inv = np.full(size, buffer_size + 1)
+
+            shp = eval_shape(get_matrices, indices[0]).shape
+            unique = jnp.full((buffer_size,) + shp, jnp.nan)
+
+            (unique, inv), invid = scan(
+                partial(scanned_amend_unique, shift=shift), (unique, inv), indices
+            )
+            _, idx = np.unique(invid, return_index=True)
+            n = idx.size
+            if n >= unique.shape[0] or not np.all(np.isnan(unique[n:])):
+                raise ValueError("`mat_buffer_size` too small")
+            uids = indices[idx]
+            uids = grid_at_f.flatindex2index(uids[np.newaxis, :])
+            uindices.append(uids)
+            invindices.append(inv)
+            indexmaps.append(_IdxMap(shift, grid_at_f.index2flatindex))
+        return _FrozenKernel(self, uindices, invindices, indexmaps)
+
 
 class _FrozenKernel(KernelBase):
     def __init__(self, kernel: KernelBase, uindices, invindices, indexmaps):
-        # TODO: "frozenness" should be part of the kernel itself; if more than
-        # just `get_indices` is overwritten by `kernel`, we should honor that!
-        self._get_indices = kernel.get_indices
+        self._get_refinement_indices = kernel.get_output_input_indices
         self.uindices = uindices
         self.invindices = invindices
         self.indexmaps = indexmaps
         self._kernels = tuple(
-            kernel.get_kernel(ii, ll) for ll, ii in enumerate(uindices)
+            kernel.get_matrices(ii, ll) for ll, ii in enumerate(uindices)
         )
-        self._base_kernel = kernel.get_kernel(jnp.array([-1]), None)
+        self._base_kernel = kernel.get_matrices(jnp.array([-1]), None)
         super().__init__(grid=kernel.grid)
 
-    def get_indices(self, index, level):
-        return self._get_indices(index, level)
+    def get_output_input_indices(self, index, level):
+        return self._get_refinement_indices(index, level)
 
-    def get_kernel(self, index, level):
+    def get_matrices(self, index, level):
         if level is None:
             return self._base_kernel
 
@@ -223,7 +225,7 @@ class ICRefine(KernelBase):
     def window_size(self):
         return self._window_size
 
-    def get_indices(self, index, level):
+    def get_output_input_indices(self, index, level):
         if level is None:
             grid_at_lvl = self.grid.at(0)
             pixel_indices = np.mgrid[tuple(slice(0, sz) for sz in grid_at_lvl.shape)]
@@ -241,18 +243,18 @@ class ICRefine(KernelBase):
         gf = gout.reshape(index.shape + (-1,))
         return (gout, level + 1), ((gc, level), (gf, level + 1))
 
-    def get_kernel(self, index, level):
+    def get_matrices(self, index, level):
         from ..refine.util import sqrtm
 
         if level is None:
-            _, ((ids, _),) = self.get_indices(index, None)
+            _, ((ids, _),) = self.get_output_input_indices(index, None)
             gc = self.grid.at(0).index2coord(ids)
             assert gc.ndim == 2
             cov = self.covariance_outer(gc, gc)
             assert cov.shape == (gc.shape[1],) * 2
             return (sqrtm(cov),)
 
-        _, ((idc, _), (idf, _)) = self.get_indices(index, level)
+        _, ((idc, _), (idf, _)) = self.get_output_input_indices(index, level)
 
         def _get_kernel(gc, gf):
             gc = self.grid.at(level).index2coord(gc)
@@ -278,7 +280,7 @@ class FixedKernelFunctionBatch(KernelBase):
         self._periodicity = periodicity
         super().__init__(grid=grid)
 
-    def get_indices(self, index, level):
+    def get_output_input_indices(self, index, level):
         if level == 0:
             grid = np.mgrid[tuple(slice(0, s) for s in self.grid.shape)]
             grid = grid.reshape((grid.shape[0], -1))
@@ -306,9 +308,9 @@ class FixedKernelFunctionBatch(KernelBase):
         )
         return self._kernel(targets_pos, nbrs_delta)
 
-    def get_kernel(self, index, level):
+    def get_matrices(self, index, level):
         # FIXME shizophrenic behavior of level
-        (targets, _), ((nbrs, _),) = self.get_indices(index, level)
+        (targets, _), ((nbrs, _),) = self.get_output_input_indices(index, level)
         return (self.evaluate_kernel(targets, nbrs, level + 1),)
 
 
@@ -322,7 +324,7 @@ class IsoPowerInterpolationKernel(KernelBase):
         self._periodicity = periodicity
         super().__init__(grid=grid)
 
-    def get_indices(self, index, level):
+    def get_output_input_indices(self, index, level):
         if level == self.grid.depth:
             raise ValueError("Finest level has no children to interpolate to")
         g = self.grid.at(level)
@@ -331,9 +333,9 @@ class IsoPowerInterpolationKernel(KernelBase):
         source = source.reshape(index.shape + (-1,))
         return (target, level + 1), ((source, level),)
 
-    def get_kernel(self, index, level):
+    def get_matrices(self, index, level):
         """Get distances between neighbors and distances to children from an index."""
-        target, source = self.get_indices(index, level)
+        target, source = self.get_output_input_indices(index, level)
         coord_source = self.grid.at(level).index2coord(source)
         coord_target = self.grid.at(level + 1).index2coord(target)
 
@@ -377,7 +379,7 @@ class IsoPowerInterpolationKernel(KernelBase):
 
 
 class CombineKernel(KernelBase):
-    def get_indices(self, index, level):
+    def get_output_input_indices(self, index, level):
         if level == self.grid.depth:
             raise ValueError("Finest level has no children to combine")
         return (index, level), (
@@ -387,8 +389,8 @@ class CombineKernel(KernelBase):
             ),
         )
 
-    def get_kernel(self, index, level):
-        _, ((children, _),) = self.get_indices(index, level)
+    def get_matrices(self, index, level):
+        _, ((children, _),) = self.get_output_input_indices(index, level)
         return (jnp.ones(children.shape[1:-1] + (1, children.shape[-1])),)
 
 
@@ -405,31 +407,33 @@ class MSCRefine(KernelBase):
         assert self._convolution.grid == self._interpolation.grid
         super().__init__(grid=self._convolution.grid)
 
-    def get_indices(self, index, level):
-        target, cindex = self._convolution.get_indices(index, level)
+    def get_output_input_indices(self, index, level):
+        target, cindex = self._convolution.get_output_input_indices(index, level)
         if level == 0:
             ids = ()
         else:
-            itarget, iindex = self._interpolation.get_indices(index, level - 1)
+            itarget, iindex = self._interpolation.get_output_input_indices(
+                index, level - 1
+            )
             assert itarget == target
             ids = ((iindex, level - 1),)
         return (target, level), ids + ((cindex, level),)
 
-    def get_kernel(self, index, level):
+    def get_matrices(self, index, level):
         # FIXME shizophrenic behavior of level
-        ker = self._convolution.get_kernel(index, level)
+        ker = self._convolution.get_matrices(index, level)
         if level == 0:
             return (ker,)
-        iwgts = self._interpolation.get_kernel(index, level)
+        iwgts = self._interpolation.get_matrices(index, level)
 
-        _, iindex, cindex = self.get_indices(index, level)
+        _, iindex, cindex = self.get_output_input_indices(index, level)
 
         parent_kernel = self._convolution.evaluate_kernel(iindex, iindex, level)
         assert parent_kernel.shape[:-2] == iwgts.shape[:-2]
 
-        cparent, children = self._combine.get_indices(iindex, level)
+        cparent, children = self._combine.get_output_input_indices(iindex, level)
         assert np.all(cparent == iindex)
-        combine = self._combine.get_kernel(iindex, level)
+        combine = self._combine.get_matrices(iindex, level)
         combine = combine[..., 0, :]  # Remove single axis for output
         assert combine.shape[:-2] == iwgts.shape[:-2]
         # Rearange matrices `combine` to map from convolution nbrs to coarse points
