a1b1bda1b510691691ba380852794caccffd391d
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-12-09 00:56:01 +0100

    multi_grid: Rewrite a learnable ICRCorrelatedField

--- src/re/multi_grid/correlated_field.py
+++ src/re/multi_grid/correlated_field.py
@@ -1,262 +1,80 @@
 from dataclasses import field
-from typing import Callable, List, Mapping, Union
 
 import jax.numpy as jnp
-import numpy as np
-import numpy.typing as npt
-from scipy.special import j0, sici
 
 from ..model import Model
-from ..prior import LogNormalPrior, NormalPrior
-from ..tree_math import ShapeWithDtype, zeros_like
-from .grid import Grid
-from .kernel import ICRKernel
-from .correlated_field_util import j1
+from ..prior import NormalPrior
+from ..tree_math import ShapeWithDtype
+from .kernel import ICRKernel, apply_kernel
 
 
-def log_k_offset_dist(r_min, r_max, N):
-    km = 1.0 / r_max
-    kM = 1.0 / r_min
-    dlk = (np.log(kM) - np.log(km)) / N
-    return np.log(km), dlk
-
-
-def logdists(r_min, r_max, N):
-    return np.arange(N) * (np.log(r_max) - np.log(r_min)) / N + np.log(r_min)
-
-
-def dists(r_min, r_max, N):
-    ld = logdists(r_min, r_max, N)
-    return np.concatenate([np.array([0.0]), np.exp(ld)])
-
-
-def k_lengths(r_min, r_max, N):
-    lkmin, dlk = log_k_offset_dist(r_min, r_max, N)
-    lk = np.arange(N) * dlk + lkmin
-    return np.concatenate((np.array([0.0]), np.exp(lk)))
-
-
-def k_binbounds(r_min, r_max, N):
-    lk = np.log(k_lengths(r_min, r_max, N)[1:])
-    _, dlk = log_k_offset_dist(r_min, r_max, N)
-    lk = np.append(lk - 0.5 * dlk, lk[-1] + 0.5 * dlk)
-    return np.concatenate((np.array([0.0]), np.exp(lk)))
-
-
-def norm_weights(r_min, r_max, N, d):
-    k_bin = k_binbounds(r_min, r_max, N)
-    if d == 1:
-        fkr = sici(k_bin * r_max)[0]
-    elif d == 2:
-        fkr = 1.0 - j0(k_bin * r_max)
-    elif d == 3:
-        fkr = sici(k_bin * r_max)[0] - np.sin(k_bin * r_max)
-    else:
-        raise NotImplementedError
-    res = fkr[1:] - fkr[:-1]
-    if (d == 1) or (d == 3):
-        res *= 2.0 / np.pi
-    return res
-
-
-def distfunc_from_specfunk(r_min, r_max, N, d, normalize=True):
-    kl = k_lengths(r_min, r_max, N)
-    f = distfunc_from_spec(r_min, r_max, N, d, normalize)
-
-    def func(specfunc):
-        spec = specfunc(kl)
-        return f(spec)
-
-    return func
-
-
-def distfunc_from_spec(r_min, r_max, N, d, normalize=True):
-    k_bin = k_binbounds(r_min, r_max, N)
-    fct = [np.pi, 2.0 * np.pi, 2.0 * np.pi**2]
-    if normalize:
-        weights = norm_weights(r_min, r_max, N, d)
-
-    def func(spec):
-        if spec.size != N + 1:
-            raise ValueError
-
-        def distfunc(r):
-            k = jnp.expand_dims(k_bin, tuple(i for i in range(len(r.shape))))
-            r = r[..., jnp.newaxis]
-            kr = r * k
-            if d == 1:
-                fkr = jnp.sin(kr)
-            elif d == 2:
-                fkr = kr * j1(kr)
-            elif d == 3:
-                fkr = jnp.sin(kr) - kr * jnp.cos(kr)
-            else:
-                raise NotImplementedError
-            res0 = (k[..., 1:] ** d - k[..., :-1] ** d) / d
-            resn0 = (fkr[..., 1:] - fkr[..., :-1]) / r**d
-            res = jnp.where(r < 1e-10, res0, resn0) / fct[d - 1]
-            res = jnp.tensordot(res, spec, axes=(-1, 0))
-            if normalize:
-                res /= (weights * spec).sum()
-            return res
-
-        return distfunc
-
-    return func
-
-
-def icrmatern_amplitude(
-    rmin: float,
-    rmax: float,
-    Nbins: int,
-    cutoff: Union[tuple, Callable],
-    loglogslope: Union[tuple, Callable],
-    prefix: str = "",
-    kind: str = "amplitude",
-) -> Model:
-    """Constructs a function computing the amplitude of a Matérn-kernel
-    power spectrum.
-
-    See
-    :class:`nifty8.re.correlated_field.CorrelatedFieldMaker.add_fluctuations
-    _matern`
-    for more details on the parameters.
-
-    See also
-    --------
-    `Causal, Bayesian, & non-parametric modeling of the SARS-CoV-2 viral
-    load vs. patient's age`, Guardiani, Matteo and Frank, Philipp and Kostić,
-    Andrija and Edenhofer, Gordian and Roth, Jakob and Uhlmann, Berit and
-    Enßlin, Torsten, `<https://arxiv.org/abs/2105.13483>`_
-    `<https://doi.org/10.1371/journal.pone.0275011>`_
-    """
-    assert rmax > rmin
-    assert rmin > 0
-    if isinstance(cutoff, (tuple, list)):
-        cutoff = LogNormalPrior(*cutoff, name=prefix + "cutoff")
-    elif not callable(cutoff):
-        te = f"invalid `cutoff` specified; got '{type(cutoff)}'"
-        raise TypeError(te)
-    if isinstance(loglogslope, (tuple, list)):
-        loglogslope = NormalPrior(*loglogslope, name=prefix + "loglogslope")
-    elif not callable(loglogslope):
-        te = f"invalid `loglogslope` specified; got '{type(loglogslope)}'"
-        raise TypeError(te)
-    mode_lengths = k_lengths(rmin / rmax, 1.0, Nbins)
-
-    def correlate(primals: Mapping) -> jnp.ndarray:
-        ctf = cutoff(primals)
-        slp = loglogslope(primals)
-
-        ln_spectrum = 0.25 * slp * jnp.log1p((mode_lengths / ctf) ** 2)
-
-        spectrum = jnp.exp(ln_spectrum)
-        spectrum = spectrum.at[0].set(spectrum[1])
-
-        if kind.lower() == "amplitude":
-            spectrum = spectrum**2
-        elif kind.lower() != "spectrum":
-            raise ValueError(f"invalid kind specified {kind!r}")
-        return spectrum
-
-    model = Model(correlate, init=cutoff.init | loglogslope.init)
-    model.klengths = mode_lengths
-    return model
-
-
-class ICRSpectral(Model):
-    uindices: List[npt.NDArray] = field(metadata=dict(static=False))
-    invindices: List[npt.NDArray] = field(metadata=dict(static=False))
-    spectrum: Model = field(metadata=dict(static=False))
+class ICRCorrelatedField(Model):
+    offset: Model = field(metadata=dict(static=False))
+    covariance: Model = field(metadata=dict(static=False))
 
     def __init__(
         self,
-        grid: Grid,
-        spectrum: Model,
-        rmin: float,
-        rmax: float,
-        dimension: int,
-        offset,
-        scale,
-        window_size,
-        rtol=1e-5,
-        atol=1e-5,
-        buffer_size=1000,
-        use_distances=True,
-        prefix="icr",
+        grid,
+        *,
+        kernel=dict(kind="matern"),
+        offset=0.0,
+        window_size=None,
+        compress=dict(rtol=1e-5, atol=1e-10, buffer_size=10_000, use_distances=True),
+        prefix="mgcfm",
     ):
-        assert rmax > rmin
-        assert rmin > 0
-        _get_kernelfunc = distfunc_from_spec(
-            rmin / rmax, 1.0, spectrum.target.size - 1, dimension, normalize=False
-        )
+        from .matern import MaternCovariance
 
-        def get_normalized_kerfunc(spec):
-            func = _get_kernelfunc(spec)
-
-            def kerfunc(x, y):
-                r = jnp.linalg.norm(x - y, axis=0) / rmax
-                return func(r) / func(jnp.zeros((1,)))[0]
-
-            return kerfunc
-
-        prefix = str(prefix)
-        self._get_kernelfunc = get_normalized_kerfunc
-        self.spectrum = spectrum
         self.grid = grid
+        shapes = [
+            ShapeWithDtype(self.grid.at(lvl).shape, jnp.float_)
+            for lvl in range(grid.depth + 1)
+        ]
+        self._name_exc = str(prefix) + "excitations"
+        domain = {self._name_exc: shapes}
 
-        latent_kernel = self._get_kernelfunc(
-            self.spectrum(zeros_like(self.spectrum.domain))
-        )
-        self.optimized_kernel = ICRKernel(grid, latent_kernel, window_size).compress(
-            rtol=rtol, atol=atol, buffer_size=buffer_size, use_distances=use_distances
-        )
-
-        self.window_size = window_size
-        self.xikey = prefix + "xi"
-
-        if isinstance(scale, (tuple, list)):
-            scale = LogNormalPrior(*scale, name=prefix + "scale")
-        elif not isinstance(scale, Model) or not isinstance(scale, float):
-            raise ValueError
-        self.scale = scale
+        DEFAULT_KERNEL_KIND = "matern"
+        if not isinstance(kernel, dict):
+            raise TypeError(f"invalid kernel type; got {kernel!r}")
+        fixed_kernel = False
+        kernel = kernel.copy()
+        kernel_kind = kernel.pop("kind", DEFAULT_KERNEL_KIND).lower()
+        if kernel_kind == "matern":
+            covariance = MaternCovariance(**kernel, prefix=prefix)
+        elif kernel_kind == "fixed":
+            fixed_kernel = True
+            covariance = kernel["covariance"]
+        else:
+            raise ValueError(f"kernel {kernel_kind!r} not supported")
+        self.covariance = covariance
+        self.fixed_kernel = fixed_kernel
+        if not self.fixed_kernel:
+            domain |= self.covariance.domain
 
         if isinstance(offset, (tuple, list)):
             offset = NormalPrior(*offset, name=prefix + "offset")
-        elif not isinstance(offset, Model) or not isinstance(offset, float):
+        elif callable(offset) or not isinstance(offset, float):
             raise ValueError
         self.offset = offset
+        if callable(self.offset):
+            domain |= self.offset.domain
 
-        assert isinstance(spectrum.domain, dict)  # TODO use init
-        grid_domain = {
-            self.xikey + str(lvl): ShapeWithDtype(self.grid.at(lvl).shape, jnp.float_)
-            for lvl in range(grid.depth + 1)
-        }
-        domain = spectrum.domain | grid_domain
-        if isinstance(scale, Model):
-            domain = domain | scale.domain
-        if isinstance(offset, Model):
-            domain = domain | offset.domain
+        self.compress = isinstance(compress, dict) and len(compress) > 0
+        kernel = ICRKernel(self.grid, None, window_size=window_size)
+        if self.compress:
+            kernel = kernel.compress_indices(**compress)
+        if self.fixed_kernel:
+            kernel = kernel.replace(covariance=self.covariance)
+            if self.compress:
+                kernel = kernel.compress_matrices()
+        self.kernel = kernel
+
+        # TODO: join init instead of domain
         super().__init__(domain=domain, white_init=True)
 
-    def get_kernel_function(self, x):
-        spec = self.spectrum(x)
-        return self._get_kernelfunc(spec)
-
-    def get_kernel(self, x):
-        kerfunc = self.get_kernel_function(x)
-        kernel = ICRKernel(self.grid, kerfunc, self.window_size)
-        return self.optimized_kernel.replace_kernel(kernel)
-
     def __call__(self, x):
-        kernel = self.get_kernel(x)
-        scale = self.scale(x) if isinstance(self.scale, Model) else self.scale
-        offset = self.offset(x) if isinstance(self.offset, Model) else self.offset
-        xs = list(x[self.xikey + str(lvl)] for lvl in range(self.grid.depth + 1))
-        res = kernel(xs)[-1]
-        res = (res - jnp.mean(res)) / jnp.std(res)
-        res = (scale * res) + offset
-        return res
-
-    def apply(self, x):
-        return self(x)
+        if not self.fixed_kernel:
+            kernel = self.kernel.replace(covariance=self.covariance(x))
+            if self.compress:
+                kernel = kernel.compress_matrices()
+        off = self.offset(x) if callable(self.offset) else self.offset
+        return off + apply_kernel(x[self._name_exc], kernel=kernel)[-1]
---
+++ src/re/multi_grid/matern.py
@@ -0,0 +1,402 @@
+#!/usr/bin/env python3
+
+# Author: Laurin Soeding
+
+from dataclasses import field
+from typing import Callable, Union
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+from jax.tree_util import Partial
+
+from ..model import Model
+from ..prior import LogNormalPrior, NormalPrior
+
+
+def si(x):
+    """Calculates the integral of sin(t)/t from 0 to x.
+
+    Based on https://github.com/GalSim-developers/GalSim/blob/releases/2.6/src/math/Sinc.cpp"""
+    x2 = x * x
+
+    def g(_x):
+        """
+        Chebyshev-Pade approximation of 1/y g(1/sqrt(y)) from 0..1/4^2
+        leads to the following formula for g(x),
+        which is also accurate to better than 1.e-16 for x > 4.
+        """
+        _y = 1.0 / (_x * _x)
+        return (
+            _y
+            * (
+                1.0
+                + _y
+                * (
+                    8.1359520115168615e2
+                    + _y
+                    * (
+                        2.35239181626478200e5
+                        + _y
+                        * (
+                            3.12557570795778731e7
+                            + _y
+                            * (
+                                2.06297595146763354e9
+                                + _y
+                                * (
+                                    6.83052205423625007e10
+                                    + _y
+                                    * (
+                                        1.09049528450362786e12
+                                        + _y
+                                        * (
+                                            7.57664583257834349e12
+                                            + _y
+                                            * (
+                                                1.81004487464664575e13
+                                                + _y
+                                                * (
+                                                    6.43291613143049485e12
+                                                    + _y * (-1.36517137670871689e12)
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+            / (
+                1.0
+                + _y
+                * (
+                    8.19595201151451564e2
+                    + _y
+                    * (
+                        2.40036752835578777e5
+                        + _y
+                        * (
+                            3.26026661647090822e7
+                            + _y
+                            * (
+                                2.23355543278099360e9
+                                + _y
+                                * (
+                                    7.87465017341829930e10
+                                    + _y
+                                    * (
+                                        1.39866710696414565e12
+                                        + _y
+                                        * (
+                                            1.17164723371736605e13
+                                            + _y
+                                            * (
+                                                4.01839087307656620e13
+                                                + _y * (3.99653257887490811e13)
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+    def f(_x):
+        """
+        Chebyshev-Pade approximation of 1/sqrt(y) f(1/sqrt(y)) from 0..1/4^2
+        leads to the following formula for g(x),
+        which is also accurate to better than 1.e-16 for x > 4.
+        """
+        _y = 1.0 / (_x * _x)
+        return (
+            1.0
+            + _y
+            * (
+                7.44437068161936700618e2
+                + _y
+                * (
+                    1.96396372895146869801e5
+                    + _y
+                    * (
+                        2.37750310125431834034e7
+                        + _y
+                        * (
+                            1.43073403821274636888e9
+                            + _y
+                            * (
+                                4.33736238870432522765e10
+                                + _y
+                                * (
+                                    6.40533830574022022911e11
+                                    + _y
+                                    * (
+                                        4.20968180571076940208e12
+                                        + _y
+                                        * (
+                                            1.00795182980368574617e13
+                                            + _y
+                                            * (
+                                                4.94816688199951963482e12
+                                                + _y * (-4.94701168645415959931e11)
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        ) / (
+            _x
+            * (
+                1.0
+                + _y
+                * (
+                    7.46437068161927678031e2
+                    + _y
+                    * (
+                        1.97865247031583951450e5
+                        + _y
+                        * (
+                            2.41535670165126845144e7
+                            + _y
+                            * (
+                                1.47478952192985464958e9
+                                + _y
+                                * (
+                                    4.58595115847765779830e10
+                                    + _y
+                                    * (
+                                        7.08501308149515401563e11
+                                        + _y
+                                        * (
+                                            5.06084464593475076774e12
+                                            + _y
+                                            * (
+                                                1.43468549171581016479e13
+                                                + _y * (1.11535493509914254097e13)
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+    def large_x2_approx(_x):
+        """
+        For |x| > 4, we use the asymptotic formula:
+        si(x) = pi/2 - f(x) cos(x) - g(x) sin(x)
+        where f(x) = int(sin(t)/(x+t),t=0..inf)
+              g(x) = int(cos(t)/(x+t),t=0..inf)
+        (asymptotic: f and g approach 1/x and 1/x^2 respectively as x -> inf. The formula as given is exact.)
+        """
+        fx = f(_x)
+        gx = g(_x)
+
+        sinx = jnp.sin(_x)
+        cosx = jnp.cos(_x)
+        return jnp.where(_x > 0.0, jnp.pi / 2.0, -jnp.pi / 2.0) - fx * cosx - gx * sinx
+
+    def small_x2_approx(_x):
+        """
+        Here, Maple was used to calculate the Pade approximation for si(x), which is accurate
+        to better than 1.e-16 for x < 4.
+        """
+        _x2 = _x * _x
+        return (
+            _x
+            * (
+                1.0
+                + _x2
+                * (
+                    -4.54393409816329991e-2
+                    + _x2
+                    * (
+                        1.15457225751016682e-3
+                        + _x2
+                        * (
+                            -1.41018536821330254e-5
+                            + _x2
+                            * (
+                                9.43280809438713025e-8
+                                + _x2
+                                * (
+                                    -3.53201978997168357e-10
+                                    + _x2
+                                    * (
+                                        7.08240282274875911e-13
+                                        + _x2 * (-6.05338212010422477e-16)
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+            / (
+                1.0
+                + _x2
+                * (
+                    1.01162145739225565e-2
+                    + _x2
+                    * (
+                        4.99175116169755106e-5
+                        + _x2
+                        * (
+                            1.55654986308745614e-7
+                            + _x2
+                            * (
+                                3.28067571055789734e-10
+                                + _x2
+                                * (
+                                    4.5049097575386581e-13
+                                    + _x2 * (3.21107051193712168e-16)
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+    return jnp.where(x2 > 16.0, large_x2_approx(x), small_x2_approx(x))
+
+
+def sin_integral_antilinearly_exact(f, xs, k):
+    """Compute integral of f(x) * sin(kx) using antilinearly exact evaluation.
+    That is, $f(x) \approx c_1+\frac{c_2}{x}$ piecewise approximated and then analytically integrated.
+    """
+    x = xs
+    kx = k * x
+    f_values = f(x)
+
+    dxinv = jnp.diff(1.0 / x)
+    df = jnp.diff(f_values)
+
+    si_kx = si(kx)
+    cos_kx = jnp.cos(kx)
+    c2 = df / dxinv
+    c1 = f_values[:-1] - c2 / x[:-1]
+
+    int_1 = c1 / k * (-jnp.diff(cos_kx))
+    int_2 = c2 * jnp.diff(si_kx)
+    result = jnp.sum(int_1 + int_2)
+    return result
+
+
+class MaternCovariance(Model):
+    scale: Union[Model, float] = field(metadata=dict(static=False))
+    cutoff: Union[Model, float] = field(metadata=dict(static=False))
+    loglogslope: Union[Model, float] = field(metadata=dict(static=False))
+    _interpolation_log_dists: jnp.ndarray = field(metadata=dict(static=False))
+    _integration_log_dists: jnp.ndarray = field(metadata=dict(static=False))
+
+    def __init__(
+        self,
+        scale: Union[tuple, Callable, float],
+        cutoff: Union[tuple, Callable, float],
+        loglogslope: Union[tuple, Callable, float],
+        *,
+        n_integrate=2_000,
+        n_interpolate=128,
+        interpolation_dists_min_max=(1e-5, 1e2),
+        prefix: str = "",
+    ):
+        if isinstance(cutoff, (tuple, list)):
+            cutoff = LogNormalPrior(*cutoff, name=prefix + "cutoff")
+        elif not callable(cutoff) or not isinstance(cutoff, float):
+            raise TypeError(f"invalid `cutoff` specified; got '{cutoff!r}'")
+        self.cutoff = cutoff
+        if isinstance(loglogslope, (tuple, list)):
+            loglogslope = NormalPrior(*loglogslope, name=prefix + "loglogslope")
+        elif not callable(loglogslope) or not isinstance(loglogslope, float):
+            raise TypeError(f"invalid `loglogslope` specified; got '{loglogslope!r}'")
+        self.loglogslope = loglogslope
+        if isinstance(scale, (tuple, list)):
+            scale = LogNormalPrior(*scale, name=prefix + "scale")
+        elif not callable(scale) or not isinstance(scale, float):
+            raise TypeError(f"invalid `scale` specified; got '{scale!r}'")
+        self.scale = scale
+
+        self._interpolation_log_dists = jnp.logspace(
+            *tuple(np.log10(interpolation_dists_min_max)), n_interpolate
+        )
+        integration_dists_min_max = (1e-5, 1e4)
+        self._integration_log_dists = jnp.logspace(
+            *tuple(np.log10(integration_dists_min_max)), n_integrate, base=10.0
+        )
+
+        super().__init__(
+            domain=self.scale.domain | self.loglogslope.domain | self.cutoff.domain
+        )
+
+    @staticmethod
+    def _cov(x, y, *, scale, distances, logcorr):
+        r = jnp.linalg.norm(x - y, axis=0, ord=2)
+        cov = jnp.where(
+            r == 0.0,
+            jnp.ones_like(r) + 1.0e-5,
+            jnp.exp(
+                jnp.interp(
+                    r, distances, logcorr, left="extrapolate", right="extrapolate"
+                )
+            ),
+        )
+        return scale * cov
+
+    def __call__(self, x):
+        scale = self.scale(x) if callable(self.scale) else self.scale
+        cutoff = self.cutoff(x) if callable(self.cutoff) else self.cutoff
+        loglogslope = (
+            self.loglogslope(x) if callable(self.loglogslope) else self.loglogslope
+        )
+
+        def integral(r):
+            r = jnp.abs(r) + 1.0e-5  # avoid numerical nonsense
+
+            def f(k):
+                power = 1.0 / (1.0 + (k * cutoff) ** 2) ** (-loglogslope / 2.0)
+                return 1.0 / (2.0 * jnp.pi**2) * k / r * power
+
+            return sin_integral_antilinearly_exact(f, self._integration_log_dists, r)
+
+        correlations = jax.vmap(integral)(self._interpolation_log_dists) / integral(0.0)
+
+        # This could perhaps go even lower, but the accuaracy is already quite
+        # good and this is definitely numerically stable.
+        # Restrict domain to ensure good numerics
+        mask = correlations < 1.0e-5
+        maxidx = jnp.argmax(mask) - 1
+        # take abs to ensure good logarithm
+        correlations = jnp.abs(correlations)
+        logcorr = jnp.log(correlations)
+        # DIY interpolation that keeps the shapes static
+        slope_at_maxidx = (logcorr[maxidx - 1] - logcorr[maxidx]) / (
+            self._interpolation_log_dists[maxidx - 1]
+            - self._interpolation_log_dists[maxidx]
+        )
+        logcorr = jnp.where(
+            ~mask,
+            logcorr,
+            logcorr[maxidx]
+            + slope_at_maxidx
+            * (self._interpolation_log_dists - self._interpolation_log_dists[maxidx]),
+        )
+
+        return Partial(
+            self.__class__._cov,
+            scale=scale,
+            distances=self._interpolation_log_dists,
+            logcorr=logcorr,
+        )
