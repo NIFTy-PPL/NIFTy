8082743236bf6c318d14131fd482a321323e40b2
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-12-09 17:54:24 +0100

    multi_grid: Document (and type) exposed functionality

--- src/re/multi_grid/matern.py
+++ src/re/multi_grid/matern.py
@@ -274,8 +274,10 @@ def si(x):
 
 
 def sin_integral_antilinearly_exact(f, xs, k):
-    """Compute integral of f(x) * sin(kx) using antilinearly exact evaluation.
-    That is, $f(x) \approx c_1+\frac{c_2}{x}$ piecewise approximated and then analytically integrated.
+    """Compute integral of $f(x) \cdot sin(kx)$ using antilinearly exact evaluation.
+
+    That is, $f(x) \\approx c_1 + \\frac{c_2}{x}$ piecewise approximated and then
+    analytically integrated.
     """
     x = xs
     kx = k * x
