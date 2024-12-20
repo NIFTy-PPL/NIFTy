d314a2d473d8dc52894b9e2432716e34c0561dcf
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-12-20 18:11:16 +0100

    ICRCorrelatedField -> ICRField

--- src/re/multi_grid/__init__.py
+++ src/re/multi_grid/__init__.py
@@ -1,3 +1,3 @@
-from .correlated_field import ICRCorrelatedField
+from .correlated_field import ICRField
 from .grid import Grid, MGrid
 from .grid_impl import HEALPixGrid, HPLogRGrid, LogarithmicGrid, SimpleOpenGrid
