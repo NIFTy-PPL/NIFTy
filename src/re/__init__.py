b773872a333987db05cd1259f99a4695708169c1
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2024-12-23 22:53:47 +0100

    Fix multi_grid export in NIFTy.re

--- src/re/__init__.py
+++ src/re/__init__.py
@@ -35,7 +35,15 @@ from .logger import logger
 from .minisanity import minisanity, reduced_residual_stats
 from .misc import hvp, interpolate, wrap, wrap_left
 from .model import Initializer, Model, VModel, WrappedCall
-from .multi_grid import *
+from .multi_grid import (
+    Grid,
+    HEALPixGrid,
+    HPLogRGrid,
+    ICRField,
+    LogarithmicGrid,
+    MGrid,
+    SimpleOpenGrid,
+)
 from .num import *
 from .optimize import minimize, newton_cg, static_newton_cg, trust_ncg
 from .optimize_kl import OptimizeVI, OptimizeVIState, optimize_kl
