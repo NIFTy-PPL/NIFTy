8cb9ff1c5bebfc925f800573ac39d27805dc2028
Gordian Edenhofer <gordian.edenhofer@gmail.com>
2025-02-01 12:19:54 -0600

    Add missing SPDX license identifiers for NIFTy.re code

--- src/re/multi_grid/matern.py
+++ src/re/multi_grid/matern.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause
 # Author: Philipp Frank, Laurin Soeding, Gordian Edenhofer
 
 from dataclasses import dataclass, field
--- src/re/num/stats_distributions.py
+++ src/re/num/stats_distributions.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: GPL-2.0+ OR BSD-2-Clause
+
 from functools import partial
 from typing import Callable, Optional
 
