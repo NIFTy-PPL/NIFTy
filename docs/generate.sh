c03784551813d1ca46358bdcdff1b725398cf29d
Jakob Roth <roth@mpa-garching.mpg.de>
2025-06-18 16:56:13 +0200

    docs: rename doc files old_nifty -> nifty_cl

--- docs/generate.sh
+++ docs/generate.sh
@@ -4,13 +4,13 @@ set -e
 
 FOLDER=docs/source/user
 
-for FILE in ${FOLDER}/0_intro ${FOLDER}/old_nifty_getting_started_0 ${FOLDER}/old_nifty_getting_started_4_CorrelatedFields ${FOLDER}/old_nifty_custom_nonlinearities ${FOLDER}/a_correlated_field; do
+for FILE in ${FOLDER}/0_intro ${FOLDER}/nifty_cl_getting_started_0 ${FOLDER}/nifty_cl_getting_started_4_CorrelatedFields ${FOLDER}/nifty_cl_custom_nonlinearities ${FOLDER}/a_correlated_field; do
     if [ ! -f "${FILE}.md" ] || [ "${FILE}.ipynb" -nt "${FILE}.md" ]; then
         jupytext --to ipynb "${FILE}.py"
         jupyter-nbconvert --to markdown --execute --ExecutePreprocessor.timeout=None "${FILE}.ipynb"
     fi
 done
 
-EXCLUDE="nifty8/cl/logger.py"
+EXCLUDE="nifty8/cl/logger.py, nifty8/config.py"
 sphinx-apidoc -e -d 1 -o docs/source/mod nifty8 ${EXCLUDE}
 sphinx-build -b html docs/source/ docs/build/
