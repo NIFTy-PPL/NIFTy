0b25d65d5f1e083c8014614f173197bba60fee86
Philipp Arras <c@philipp-arras.de>
2025-07-10 11:45:36 +0200

    CI: use gitlab CI

--- test/test_cl/test_numpy_accuracy.py
+++ test/test_cl/test_numpy_accuracy.py
@@ -28,14 +28,14 @@ def _check(a, b):
     assert rel_error < 1e-6
 
 
-# When this is fixed in numpy, the warning in nifty/cl/ducc_dispath.py is no longer
-# necessary
-@pytest.mark.xfail(reason="np.vdot inaccurate for single precision", strict=True)
+@pytest.mark.xfail(reason="np.vdot inaccurate for single precision")
 def test_vdot():
     a = 100*np.ones((1000000,)).astype(dt)
     _check(a, a)
 
 
+# When this is fixed in numpy, the warning in nifty/cl/ducc_dispath.py is no longer
+# necessary
 @pytest.mark.xfail(reason="np.vdot inaccurate for single precision", strict=True)
 def test_vdot_extreme():
     a = np.array([1e8, 1, -1e8]).astype(dt)
