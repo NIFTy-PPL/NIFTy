d111c1ee45fa56528d220d649cd6910279b0e00f
Philipp Arras <c@philipp-arras.de>
2025-07-30 11:09:08 +0200

    flake.nix: simplify and unify

--- flake.nix
+++ flake.nix
@@ -8,152 +8,136 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        pkgs-cuda = import nixpkgs {
+        pkgsBase = import nixpkgs { inherit system; };
+        pkgsCuda = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
           config.cudaSupport = true;
         };
 
-        myPyPkgs = pkgs.python3Packages;
-        myPyPkgs-cuda = pkgs-cuda.python3Packages;
-
         version = "9.1.0";
 
-        req.minimal = with myPyPkgs; [ numpy scipy ducc0 ];
-        req.dev = with myPyPkgs; [ pytest pytest-cov pytest-xdist matplotlib ];
-        req.mpi = [ myPyPkgs.mpi4py pkgs.openmpi pkgs.openssh ];
-        # req.jax = with myPyPkgs; [ jax jaxlib ];
-        req.rest = with myPyPkgs; [ h5py ];
-        req.docs = with myPyPkgs; [
-          sphinx
-          pkgs.jupyter # python3Packages.jupyter is broken, see https://github.com/NixOS/nixpkgs/issues/299385
-          jupytext
-          pydata-sphinx-theme
-          sphinxcontrib-bibtex
-          myst-parser
-        ];
+        getDeps = pkgs: cudaSupport:
+          let
+            pyPkgs = pkgs.python3Packages;
+            main = with pyPkgs;
+              [ numpy scipy ducc0 ] ++ (if cudaSupport then [
+                cupy
+                pkgs.cudaPackages.cudatoolkit
+              ] else
+                [ ]) ++ [ pyPkgs.mpi4py pkgs.openmpi pkgs.openssh ];
+          in {
+            pyPkgs = pyPkgs;
+            main = main;
+            test = main ++ (with pyPkgs; [
+              pytestCheckHook
+              pytest
+              pytest-cov
+              pytest-xdist
+              matplotlib
+              h5py
+            ]);
+            docs = [
+              pkgs.jupyter # python3Packages.jupyter is broken, see https://github.com/NixOS/nixpkgs/issues/299385
+              pyPkgs.jupytext
+              pyPkgs.pydata-sphinx-theme
+              pyPkgs.sphinx
+              pyPkgs.sphinxcontrib-bibtex
+              pyPkgs.myst-parser
+              pyPkgs.jax
+              pyPkgs.matplotlib
+            ];
+          };
 
-        req-cuda.minimal = with myPyPkgs-cuda; [ numpy scipy ducc0 cupy pkgs-cuda.cudaPackages.cudatoolkit ];
-        req-cuda.dev = with myPyPkgs-cuda; [ pytest pytest-cov pytest-xdist matplotlib ];
-        req-cuda.mpi = [ myPyPkgs-cuda.mpi4py pkgs-cuda.openmpi pkgs-cuda.openssh ];
-        # req-cuda.jax = with myPyPkgs-cuda; [ jax jaxlib ];
-        req-cuda.rest = with myPyPkgs-cuda; [ h5py ];
-        req-cuda.docs = with myPyPkgs-cuda; [
-          sphinx
-          pkgs-cuda.jupyter # python3Packages.jupyter is broken, see https://github.com/NixOS/nixpkgs/issues/299385
-          jupytext
-          pydata-sphinx-theme
-          sphinxcontrib-bibtex
-          myst-parser
-        ];
+        mkNifty = { cudaSupport ? false, doCheck ? true }:
+          let
+            pkgs = if cudaSupport then pkgsCuda else pkgsBase;
+            deps = getDeps pkgs cudaSupport;
+          in pkgs.lib.makeOverridable (args:
+            deps.pyPkgs.buildPythonPackage rec {
+              pname = "nifty";
+              inherit version;
+              src = ./.;
+              pyproject = true;
+              build-system = with pkgs.python3.pkgs; [ setuptools ];
+              dependencies = deps.main;
+              doCheck = args.doCheck or true;
+              checkInputs = if doCheck then deps.test else [ ];
+              pytestFlagsArray = [ "--ignore=test/test_re" ];
+              postCheck = pkgs.lib.optionalString doCheck ''
+                ${
+                  pkgs.lib.getExe' pkgs.mpi "mpirun"
+                } -n 2 --bind-to none python3 -m pytest test/test_cl/test_mpi
+              '';
+              pythonImportsCheck = [ "nifty" ];
+            }) { # TODO: Is this really the way to go in nix? (see also below)
+              cudaSupport = cudaSupport;
+              doCheck = doCheck;
+            };
 
-        allreqs = pkgs.lib.attrValues req;
-        allreqs-cuda = pkgs.lib.attrValues req-cuda;
+        mkNiftyDocs = { cudaSupport ? false }:
+          let
+            pkgs = if cudaSupport then pkgsCuda else pkgsBase;
+            deps = getDeps pkgs cudaSupport;
+          in pkgs.lib.makeOverridable (args:
+            pkgs.stdenv.mkDerivation {
+              name = "nifty-docs";
+              inherit version;
+              src = ./.;
+              buildInputs = deps.docs
+                ++ (if cudaSupport then [ nifty-cuda ] else [ nifty ]);
+              buildPhase = "sh docs/generate.sh";
+              installPhase = ''
+                mkdir $out
+                mv docs/build/* $out
+              '';
+            }) { cudaSupport = cudaSupport; };
 
-        nifty = myPyPkgs.buildPythonPackage {
-          pname = "nifty";
-          inherit version;
-          src = ./.;
-          pyproject = true;
-          build-system = with pkgs.python3.pkgs; [ setuptools ];
-          dependencies = req.minimal ++ req.mpi;
-          checkInputs = with myPyPkgs; [ pytestCheckHook pytest-xdist ]
-            ++ allreqs;
-          postPatch = ''
-            rm -r nifty/re
-            rm -r test/test_re
-          '';
-          postCheck = ''
-            ${
-              pkgs.lib.getExe' pkgs.mpi "mpirun"
-            } -n 2 --bind-to none python3 -m pytest test/test_cl/test_mpi
-          '';
-          pythonImportsCheck = [ "nifty" ];
-        };
-        nifty-cuda = myPyPkgs-cuda.buildPythonPackage {
-          pname = "nifty";
-          inherit version;
-          src = ./.;
+        mkDevShell = { cudaSupport ? false }:
+          let
+            pkgs = if cudaSupport then pkgsCuda else pkgsBase;
+            deps = getDeps pkgs cudaSupport;
+            cudaEnv = if cudaSupport then ''
+              export CUDA_HOME=${toString pkgs.cudaPackages.cudatoolkit}
+              export LD_LIBRARY_PATH=${
+                toString pkgs.cudaPackages.cudatoolkit.lib
+              }/lib:$LD_LIBRARY_PATH
+            '' else
+              "";
+            #   CUDA_HOME = "/usr/local/cuda";
+            #   LD_LIBRARY_PATH =
+            #     "${pkgs-cuda.cudaPackages.cudatoolkit.lib}/lib:/usr/local/cuda/lib64";
+          in pkgs.mkShell {
+            buildInputs = deps.main ++ deps.test ++ (with deps.pyPkgs; [
+              pip
+              venvShellHook
+              python-lsp-server
+              python-lsp-ruff
+            ]) ++ [ pkgs.ruff ];
+            venvDir = ".nix-nifty-venv";
+            shellHook = ''
+              export PIP_PREFIX=$(pwd)/_build/pip_packages
+              export PYTHONPATH="$PIP_PREFIX/${deps.pyPkgs.python.sitePackages}:$PYTHONPATH"
+              export PATH="$PIP_PREFIX/bin:$PATH"
+              unset SOURCE_DATE_EPOCH
+              ${cudaEnv}
+            '';
+          };
 
-          pyproject = true;
-          build-system = with pkgs-cuda.python3.pkgs; [ setuptools ];
-          dependencies = req-cuda.minimal ++ req-cuda.mpi;
-          checkInputs = with myPyPkgs-cuda; [ pytestCheckHook pytest-xdist ]
-            ++ allreqs;
-          postPatch = ''
-            rm -r nifty/re
-            rm -r test/test_re
-          '';
-          postCheck = ''
-            ${
-              pkgs-cuda.lib.getExe' pkgs-cuda.mpi "mpirun"
-            } -n 2 --bind-to none python3 -m pytest test/test_mpi
-          '';
-          pythonImportsCheck = [ "nifty" ];
-        };
-
-        nifty-docs = pkgs.stdenv.mkDerivation {
-          name = "nifty-docs";
-          inherit version;
-          src = ./.;
-          buildInputs = allreqs ++ [ nifty ];
-          buildPhase = "sh docs/generate.sh";
-          installPhase = ''
-            mkdir $out
-            mv docs/build/* $out
-          '';
-        };
-
+        nifty = mkNifty { cudaSupport = false; };
+        nifty-cuda = mkNifty { cudaSupport = true; };
       in {
         # Standard nifty package
         packages.default = nifty;
         packages."cuda" = nifty-cuda;
 
         # Build nifty docs (`nix build .#docs`)
-        packages."docs" = nifty-docs;
+        packages."docs" = mkNiftyDocs { cudaSupport = false; };
 
         # Run `nix develop .` to enter the development shell including, e.g.,
         # python-lsp-server. Then compile nifty with, e.g., `pip3 install .`
-        devShells.default = pkgs.mkShell {
-          buildInputs = allreqs ++ (with myPyPkgs; [
-            pip
-            venvShellHook
-            python-lsp-server
-            python-lsp-ruff
-            pkgs.ruff
-          ]);
-          venvDir = ".nix-nifty-venv";
-
-          shellHook = ''
-            export PIP_PREFIX=$(pwd)/_build/pip_packages
-            export PYTHONPATH="$PIP_PREFIX/${myPyPkgs.python.sitePackages}:$PYTHONPATH"
-            export PATH="$PIP_PREFIX/bin:$PATH"
-            unset SOURCE_DATE_EPOCH
-          '';
-        };
-
-        devShells."cuda" = pkgs-cuda.mkShell {
-          buildInputs = allreqs-cuda ++ (with myPyPkgs-cuda; [
-            pip
-            venvShellHook
-            python-lsp-server
-            python-lsp-ruff
-            pkgs-cuda.ruff
-          ]);
-          venvDir = ".nix-nifty-cuda-venv";
-
-          CUDA_HOME = "/usr/local/cuda";
-          LD_LIBRARY_PATH =
-            "${pkgs-cuda.cudaPackages.cudatoolkit.lib}/lib:/usr/local/cuda/lib64";
-
-          shellHook = ''
-            export PIP_PREFIX=$(pwd)/_build/pip_packages
-            export PYTHONPATH="$PIP_PREFIX/${myPyPkgs-cuda.python.sitePackages}:$PYTHONPATH"
-            export PATH="$PIP_PREFIX/bin:$PATH"
-            unset SOURCE_DATE_EPOCH
-          '';
-        };
+        devShells.default = mkDevShell { cudaSupport = false; };
+        devShells.cuda = mkDevShell { cudaSupport = true; };
 
       });
 }
