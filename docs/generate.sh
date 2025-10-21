306b05b1b989273d8f5203bfc2f87ac3b1aa03aa
Jakob Roth <roth@mpa-garching.mpg.de>
2025-10-21 09:57:08 +0200

    simplify generation of docs

--- docs/generate.sh
+++ docs/generate.sh
@@ -3,19 +3,19 @@
 set -e
 
 FOLDER=docs/source/user
-FILES=(
-  "nifty_cl_getting_started_0"
-  "nifty_cl_getting_started_4_CorrelatedFields"
-  "nifty_cl_custom_nonlinearities"
-  "notebooks_re/0_models"
-  "notebooks_re/1_inference"
-  "notebooks_re/2_gaussian_processes"
-  "notebooks_re/3_wiener_filter"
-  "notebooks_re/4_correlated_field_model"
-  "notebooks_re/5_log_normal_poisson"
-)
+FILES=" \
+  nifty_cl_getting_started_0 \
+  nifty_cl_getting_started_4_CorrelatedFields \
+  nifty_cl_custom_nonlinearities \
+  notebooks_re/0_models \
+  notebooks_re/1_inference \
+  notebooks_re/2_gaussian_processes \
+  notebooks_re/3_wiener_filter \
+  notebooks_re/4_correlated_field_model \
+  notebooks_re/5_log_normal_poisson \
+"
 
-for FILE in "${FILES[@]}"; do
+for FILE in $FILES; do
     if [ ! -f "${FILE}.md" ] || [ "${FILE}.ipynb" -nt "${FILE}.md" ]; then
         jupytext --to ipynb "${FOLDER}/${FILE}.py"
         jupyter-nbconvert --to markdown --execute --ExecutePreprocessor.timeout=None "${FOLDER}/${FILE}.ipynb"
