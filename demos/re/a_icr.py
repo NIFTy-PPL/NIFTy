0d03eb3775ff04f4948d18b484e30a01cfdea981
Julian Ruestig <jruestig@posteo.de>
2026-02-16 13:52:10 +0100

    452-optional-save: remove new key from demo

--- demos/re/a_icr.py
+++ demos/re/a_icr.py
@@ -84,7 +84,6 @@ samples, state = jft.optimize_kl(
     ),
     sample_mode="linear_resample",
     odir="results_icr",
-    intermediate_samples_save="results_icr/intermediate.pkl",
     resume=False,
 )
 
